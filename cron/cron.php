2a1873a4f601e216585eda82119f9f731ee12877
Xander Jansen <xander.jansen@surfnet.nl>
2010-11-05 12:22:50 +0000

    Fixed include of config.php (was dev_config.php)
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/trunk@326 8555b757-d854-4b86-925a-82fd84c90ff4

--- cron/cron.php
+++ cron/cron.php
@@ -42,7 +42,7 @@ error_reporting(E_ALL);
 $filesenderbase = dirname(dirname(__FILE__));
 
 // include all required classes
-require_once("$filesenderbase/config/dev_config.php");
+require_once("$filesenderbase/config/config.php");
 require_once("$filesenderbase/classes/DB.php");
 require_once("$filesenderbase/classes/EN_AU.php");
 require_once("$filesenderbase/classes/Mail.php");
