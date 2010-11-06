4d2e2396645c40d134f51873f462a53b4415bb8f
Xander Jansen <xander.jansen@surfnet.nl>
2010-11-06 21:07:05 +0000

    DEBUG code for #325 (blåbærsyltetøy buggen).
    Lots of DEBUG statements using logEntry() to help debugging the filename problem in #325. Code should be removed before the RC. No actual codechanges in this commit.
    
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/trunk@330 8555b757-d854-4b86-925a-82fd84c90ff4

--- classes/Mail.php
+++ classes/Mail.php
@@ -66,6 +66,7 @@ public function sendemail($mailobject,$template){
 	$template = str_replace("{fileexpirydate}", date("d-M-Y",strtotime($mailobject["fileexpirydate"])), $template);
 	$template = str_replace("{filefrom}", $mailobject["filefrom"], $template);
 	// Convert (transliterate) the personal message to ISO-8859-1 when text is in UTF-8
+	logEntry("DEBUG sendemail: filemessage = " . $mailobject['filemessage'] . " - Detected encoding: " . mb_detect_encoding($mailobject['filemessage']) . ". ");
 	if ( mb_detect_encoding($mailobject['filemessage']) == 'UTF-8' )
 	{
 		$mailobject['filemessage'] = iconv("UTF-8", "ISO-8859-1//TRANSLIT", $mailobject['filemessage']);
@@ -95,6 +96,7 @@ public function sendemail($mailobject,$template){
 	$to = "<".$mailobject['fileto'].">".","."<".$mailobject['filefrom'].">";
 		if(isset($mailobject['filesubject']) && $mailobject['filesubject'] != ""){
 			// Properly encode the message subject when it contains UTF-8 characters
+			logEntry("DEBUG sendemail: filesubject = " . $mailobject['filesubject'] . " - Detected encoding: " . mb_detect_encoding($mailobject['filesubject']) . ". ");
 			if ( mb_detect_encoding($mailobject['filesubject']) == 'UTF-8' )
 			{
 				mb_internal_encoding("UTF-8");
