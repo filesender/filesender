9e3be6ca4ebb6c821de170ae13fbd80f0ee6565e
gijzelaerr <gijzelaerr@8555b757-d854-4b86-925a-82fd84c90ff4>
2010-11-11 15:18:13 +0000

    fixed unreadable indentation
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/trunk@339 8555b757-d854-4b86-925a-82fd84c90ff4

--- www/fs_main.php
+++ www/fs_main.php
@@ -28,13 +28,13 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
- 
- /*
-  * flex calls fs_main.php for all requests except the actual file uploading
-  * $_POST['call'] defines the functions triggere from this call
-  * flex uses HTTPService calls to talk to php and returns results as text or json text
-  * e.g. <mx:HTTPService id="init_main" url="../fs_main.php{URLvid}" useProxy="false" method="POST" resultFormat="text" result="resultInit(event)" fault="resultError(event)"/> 
-  */
+
+/*
+ * flex calls fs_main.php for all requests except the actual file uploading
+ * $_POST['call'] defines the functions triggere from this call
+ * flex uses HTTPService calls to talk to php and returns results as text or json text
+ * e.g. <mx:HTTPService id="init_main" url="../fs_main.php{URLvid}" useProxy="false" method="POST" resultFormat="text" result="resultInit(event)" fault="resultError(event)"/> 
+ */
 
 require_once('../classes/_includes.php');
 $authsaml = AuthSaml::getInstance();
@@ -52,189 +52,189 @@ $returnData = array(); // init return array
 date_default_timezone_set($config['Default_TimeZone']);
 
 if(session_id() == ""){
-	// start new session and mark it as valid because the system is a trusted source
-	session_start();
-	$_SESSION['validSession'] = true;
+    // start new session and mark it as valid because the system is a trusted source
+    session_start();
+    $_SESSION['validSession'] = true;
 } 
 // test if flex can continue to call this page
 // check if valid vourcher
 
 // get the function call from flex
-	if (isset($_POST['call'])) {
-		$call = htmlspecialchars($_POST['call']);
-		
-	// call logon function
-	if ($call == "logon" ) {
-		echo $authsaml->logonURL();
-		exit;
-	}
-	
-	// call logoff function
-	if ($call == "logoff" ) {
-		echo $authsaml->logoffURL();
-		exit;
-	}
-	
-	// return init/configuration data as array to flex
-	if($call == "getInit") {
-	
-	$returnData["lang"] = json_encode($lang->language());
-	$returnData["samlauth"] = $authsaml->isAuth();
-	$returnData["voucherauth"] = $authvoucher->aVoucher();
-	$returnData["splash"] = $functions->getSplash();
-	$returnData["statsdata"] = "";
-	$returnData["authIsAdmin"] = $authsaml->authIsAdmin();
-	if($authvoucher->aVoucher() || $authsaml->isAuth()) 
-	{
-		$returnData["statsdata"] = $functions->getStats();
-		$returnData["config"] = $functions->getConfig();
-		
-		if($authsaml->isAuth()) 
-		{
-			$returnData["authUser"]  =  json_encode($authsaml->sAuth());
-			
-		}
-	
-		if($authvoucher->aVoucher()) 
-		{
-			$returnData["voucher"]  =  $authvoucher->validVoucher();
-		}
-			
-		
- 	}
-		//$sendmail->sendemailAdmin(json_encode($returnData));
-		echo json_encode($returnData); 
-	}
-	
-	if(!$authvoucher->aVoucher() && !$authsaml->isAuth()) {
-		echo "notAuthenticated";
- 	}
-	else 
-	{
-	// process requests from flex
-	
-	switch ($call) {
-	
-	// log data from flex
-	case "log_process":
-    	echo $log->logProcess($_POST['client'],$_POST['message']);
-    	break;
-	
-	//  return authentication for flex
-	case "getAuth":
-        echo $authsaml->isAuth();
-        break;
-	
-	// return true if users is admin
-	case "getAuthAdmin":
-        echo $authsaml->authIsAdmin();
-        break;
-	
-	// request a file move from flex	
-	case "moveFile":
-        echo $functions->moveFile();
-        break;
-	
-	// return filesize for flex to calulcate gears or compare uploaded file	
-	case "getFileSize":
-        echo $functions->getFileSize($_POST['filename']);
-        break;
-	
-	// updates voucher/file data when there is a change in the voucher data
-	case "completeFile":
-        echo $functions->updateFile();
-        break;
-		
-	// email and log a file when downloaded
-	case "downloadedFile":
-        echo $functions->downloadedFile();
-        break;
-	
-	// resturn drivespace as array site_filestore_total; site_temp_filestore_total;	site_filestore_free; site_temp_filestore_free
-	case "driveSpace":
-        echo $functions->driveSpace();
-        break;
-		
-	// returns json array of admin log data from log table
-	case "getAdminLogs":
-        echo $functions->adminLogs();
-	    break;
-	
-	// returns json array of all uploaded files for admin
-	case "getAdminFiles":
-        echo $functions->adminFiles();
-		break;
-	
-	// returns  file information as json array
-	case "fileInfo":
-		logEntry("DEBUG fileInfo: jsonSendData = " . $_POST['jsonSendData'] );
-		$jsonString = rawurldecode($_POST['jsonSendData']);
-		logEntry("DEBUG fileInfo: \$jsonString = " . $jsonString . " - Detected encoding: " . mb_detect_encoding($jsonString) . ". ");
-		$jsonString = utf8_encode($jsonString);
-		logEntry("DEBUG fileInfo: \$jsonString(UTF8) = " . $jsonString );
-		$data = json_decode($jsonString, true);
-		logEntry("DEBUG fileInfo: \$data = " . print_r($data,true) );
-		echo $functions->getFile($data);
-		break;
-	
-	// returns voucher information as json array for flex
-	case "getFile":
-		echo $authvoucher->getVoucher();
-        break;
-	
-	// resturns all vouchers for a specific user as json array for flex	
-	case "getVouchers":
-		echo $functions->getVouchers();
-		break;
-		
-	// resurns current users file data as json array for flex
-	case "getUserFiles":
-		echo $functions->getUserFiles();
-		break;
-		
-	// deletes a specific voucher	
-	case "deleteVoucher": 
-		echo $functions->deleteVoucher($_POST['svid']);
-		break;
-	
-	// remove a specific file by voucherid
-	case "deleteFile": 
-		echo $functions->deleteFile($_POST['svid']);
-		break;
-	
-	// lcoses a specific voucher by voucherid
-	case "closeVoucher": 
-		echo $functions->closeVoucher($_POST['svid']);
-		break;
-	
-	// resends voucher as requested by flex in myFiles	
-	case "resendVoucher":
-		$fileArray =  $functions->getVoucher($_POST['svid']);
-		
-		echo $sendmail->sendEmail($fileArray[0],$config['voucherissuedemailbody']);
+if (isset($_POST['call'])) {
+    $call = htmlspecialchars($_POST['call']);
 
-		break;
-	
-	// resend file	as requested by flex in myFiles
-	case "resendFile":
-		$fileArray =  $functions->getVoucher($_POST['svid']);
-		
-		echo $sendmail->sendEmail($fileArray[0],$config['fileuploadedemailbody']);
+    // call logon function
+    if ($call == "logon" ) {
+        echo $authsaml->logonURL();
+        exit;
+    }
 
-		break;
-	
-	// insert a new file or voucher
-	case "insertFile":
-		
-		logEntry("INSERT:".rawurldecode($_POST['jsonSendData']));
-		echo $functions->insertFile();
-		break;
-		
-	}
+    // call logoff function
+    if ($call == "logoff" ) {
+        echo $authsaml->logoffURL();
+        exit;
+    }
 
-	}
-	}
+    // return init/configuration data as array to flex
+    if($call == "getInit") {
 
-	 
+        $returnData["lang"] = json_encode($lang->language());
+        $returnData["samlauth"] = $authsaml->isAuth();
+        $returnData["voucherauth"] = $authvoucher->aVoucher();
+        $returnData["splash"] = $functions->getSplash();
+        $returnData["statsdata"] = "";
+        $returnData["authIsAdmin"] = $authsaml->authIsAdmin();
+        if($authvoucher->aVoucher() || $authsaml->isAuth()) 
+        {
+            $returnData["statsdata"] = $functions->getStats();
+            $returnData["config"] = $functions->getConfig();
+
+            if($authsaml->isAuth()) 
+            {
+                $returnData["authUser"]  =  json_encode($authsaml->sAuth());
+
+            }
+
+            if($authvoucher->aVoucher()) 
+            {
+                $returnData["voucher"]  =  $authvoucher->validVoucher();
+            }
+
+
+        }
+        //$sendmail->sendemailAdmin(json_encode($returnData));
+        echo json_encode($returnData); 
+    }
+
+    if(!$authvoucher->aVoucher() && !$authsaml->isAuth()) {
+        echo "notAuthenticated";
+    }
+    else 
+    {
+        // process requests from flex
+
+        switch ($call) {
+
+            // log data from flex
+        case "log_process":
+            echo $log->logProcess($_POST['client'],$_POST['message']);
+            break;
+
+            //  return authentication for flex
+        case "getAuth":
+            echo $authsaml->isAuth();
+            break;
+
+            // return true if users is admin
+        case "getAuthAdmin":
+            echo $authsaml->authIsAdmin();
+            break;
+
+            // request a file move from flex	
+        case "moveFile":
+            echo $functions->moveFile();
+            break;
+
+            // return filesize for flex to calulcate gears or compare uploaded file	
+        case "getFileSize":
+            echo $functions->getFileSize($_POST['filename']);
+            break;
+
+            // updates voucher/file data when there is a change in the voucher data
+        case "completeFile":
+            echo $functions->updateFile();
+            break;
+
+            // email and log a file when downloaded
+        case "downloadedFile":
+            echo $functions->downloadedFile();
+            break;
+
+            // resturn drivespace as array site_filestore_total; site_temp_filestore_total;	site_filestore_free; site_temp_filestore_free
+        case "driveSpace":
+            echo $functions->driveSpace();
+            break;
+
+            // returns json array of admin log data from log table
+        case "getAdminLogs":
+            echo $functions->adminLogs();
+            break;
+
+            // returns json array of all uploaded files for admin
+        case "getAdminFiles":
+            echo $functions->adminFiles();
+            break;
+
+            // returns  file information as json array
+        case "fileInfo":
+            logEntry("DEBUG fileInfo: jsonSendData = " . $_POST['jsonSendData'] );
+            $jsonString = rawurldecode($_POST['jsonSendData']);
+            logEntry("DEBUG fileInfo: \$jsonString = " . $jsonString . " - Detected encoding: " . mb_detect_encoding($jsonString) . ". ");
+            $jsonString = utf8_encode($jsonString);
+            logEntry("DEBUG fileInfo: \$jsonString(UTF8) = " . $jsonString );
+            $data = json_decode($jsonString, true);
+            logEntry("DEBUG fileInfo: \$data = " . print_r($data,true) );
+            echo $functions->getFile($data);
+            break;
+
+            // returns voucher information as json array for flex
+        case "getFile":
+            echo $authvoucher->getVoucher();
+            break;
+
+            // resturns all vouchers for a specific user as json array for flex	
+        case "getVouchers":
+            echo $functions->getVouchers();
+            break;
+
+            // resurns current users file data as json array for flex
+        case "getUserFiles":
+            echo $functions->getUserFiles();
+            break;
+
+            // deletes a specific voucher	
+        case "deleteVoucher": 
+            echo $functions->deleteVoucher($_POST['svid']);
+            break;
+
+            // remove a specific file by voucherid
+        case "deleteFile": 
+            echo $functions->deleteFile($_POST['svid']);
+            break;
+
+            // lcoses a specific voucher by voucherid
+        case "closeVoucher": 
+            echo $functions->closeVoucher($_POST['svid']);
+            break;
+
+            // resends voucher as requested by flex in myFiles	
+        case "resendVoucher":
+            $fileArray =  $functions->getVoucher($_POST['svid']);
+
+            echo $sendmail->sendEmail($fileArray[0],$config['voucherissuedemailbody']);
+
+            break;
+
+            // resend file	as requested by flex in myFiles
+        case "resendFile":
+            $fileArray =  $functions->getVoucher($_POST['svid']);
+
+            echo $sendmail->sendEmail($fileArray[0],$config['fileuploadedemailbody']);
+
+            break;
+
+            // insert a new file or voucher
+        case "insertFile":
+
+            logEntry("INSERT:".rawurldecode($_POST['jsonSendData']));
+            echo $functions->insertFile();
+            break;
+
+        }
+
+    }
+}
+
+
 
 ?>
