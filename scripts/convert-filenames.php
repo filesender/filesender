98ae22338da520dc48b32286948a799e605e6379
Xander Jansen <xander.jansen@surfnet.nl>
2010-11-14 11:44:37 +0000

    [PROPOSED PATCHES] To handle new and more robust filestore naming scheme introduced in r334
    - add scripts/convert-filenames.php to convert to new scheme (or revert back when downgrading), should be manually invoked once when upgrading (or downgrading)
    - Added entry in CHANGELOG.txt regarding the upgrade/downgrade
    - made cron.php aware of the new naming scheme
    
    Commit should be reverted if better solutions are required.
    
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/trunk@351 8555b757-d854-4b86-925a-82fd84c90ff4

--- scripts/convert-filenames.php
+++ scripts/convert-filenames.php
@@ -29,11 +29,18 @@
  */
  
 //  --------------------------------
-// this file is called by CRON to remove files that have expired and close the expired files in the database
+// this file is manually invoked to rename all non-expired files in the filestore
+// to the new naming scheme introduced in beta 0.1.17. 
+// 
+// Usage: php convert-filenames.php [revert]
+//
+//  where 'revert' is an optional parameter to revert the filenames to the
+//  old naming scheme in case you want to switch back to a pre-0.1.17 release
+//  The new naming scheme was introduced in svn revision r334
 // ---------------------------------
 
  
-// required as this page is called from CRON not from a web browser
+// required as this page is called from CONVERT not from a web browser
 chdir(dirname(__FILE__));
 
 // force all error reporting
@@ -47,6 +54,7 @@ require_once("$filesenderbase/classes/DB.php");
 require_once("$filesenderbase/classes/EN_AU.php");
 require_once("$filesenderbase/classes/Mail.php");
 require_once("$filesenderbase/classes/DB_Input_Checks.php");
+require_once("$filesenderbase/classes/Functions.php");
 
 $lang = EN_AU::getInstance();
 $CFG = config::getInstance();
@@ -62,26 +70,33 @@ if(session_id() == ""){
 	session_start();
 	$_SESSION['validSession'] = true;
 } 
+// Check arguments passed
+if ($argc > 1 ) {
+    if ($argv[1] == "revert") {
+       $rename_to_old_scheme = TRUE ;
+    }
+} else {
+       $rename_to_old_scheme = FALSE ;
+}
 
-// log that cron has started running
-logProcess("CRON","Cron started");
-if (cleanUp())
+// log that conversion has started running
+logProcess("CONVERT","Conversion started");
+if (convertNames($rename_to_old_scheme))
 {
-	// cron completed - log
-	logProcess("CRON","Cron Complete");
+	// conversion completed - log
+	logProcess("CONVERT","Conversion Complete");
 } 
 	else 
 {
-	// email admin - error in Cron
-	logProcess("CRON","Cron Error - check error log");
+	// email admin - error in Conversion
+	logProcess("CONVERT","Conversion Error - check error log");
 }
 
 
 
 //---------------------------------------
-	// Clean up missing files
-	// Remove out of date files and vouchers
-function cleanUp() 
+// Rename stored files to new naming scheme (or back)
+function convertNames($rename_to_old_scheme) 
 	{
 	
 	global $config;
@@ -89,103 +104,25 @@ function cleanUp()
 	
 	// check log_location exists	
 	if (!file_exists($config["log_location"])) {
-	logProcess("CRON","Unable to find log_location location specified in config.php  :".$config["log_location"]);
+	logProcess("CONVERT","Unable to find log_location location specified in config.php  :".$config["log_location"]);
 	return false;
 	}
 	
 	// check site_filestore exists
 	if (!file_exists($config["site_filestore"])) {
-		logProcess("CRON","Unable to find site_filestore location specified in config.php  :".$config["site_filestore"]);
+		logProcess("CONVERT","Unable to find site_filestore location specified in config.php  :".$config["site_filestore"]);
 		return false;
 	}	
 	
 	// check site_temp_filestore exists
 	if (!file_exists($config["site_temp_filestore"])) {
-		logProcess("CRON","Unable to find site_temp_filestore location specified in config.php  :".$config["site_temp_filestore"]);
+		logProcess("CONVERT","Unable to find site_temp_filestore location specified in config.php  :".$config["site_temp_filestore"]);
 		return false;
 	}	
 	
-	// remove any files with no uid - leftover from bug earlier beta that save files without uid's
-	$sqlQuery = "
-					DELETE FROM					 
-						files 
-					WHERE 
-						fileuid IS NULL
-				";
-	
-	$db->fquery($sqlQuery);
-	
-	 
 	$FilestoreDirectory = $config["site_filestore"];
 
-	//
-	// check for any expired files first and close status in database
-	//
-	$today = date($config['postgresdateformat']); 
 	
-	// if file not closed and past expiry date then close the file
-	$searchquery = "SELECT * FROM files WHERE  fileexpirydate < '%s' AND (filestatus = 'Available' or filestatus = 'Voucher')";
-	$search = $db->fquery($searchquery, $today);
-		
-		// check for error in SQL
-	if (!$search) { 
-		logProcess("CRON","SQL Error on selecting files".pg_last_error());
-		return FALSE; 
-		}
-	
-	while($row = pg_fetch_assoc($search)) {
-		
-		// remove from database
-		$query = "UPDATE Files SET filestatus = 'Closed' WHERE fileid='%s'";
-		$result = $db->fquery($query, $row['fileid']);
-		
-		// check for error in SQL
-		if (!$result) { 
-			logProcess("CRON","SQL Error on updating files".pg_last_error());
-			return FALSE; 
-		}
-		
-	}
-
-	// remove files that do not have at least one Available file associated with it
-	// loop through directory and check file is Available
-	
-    // Open the folder
-    $dir_handle = @opendir($FilestoreDirectory) or die("Unable to open $FilestoreDirectory"); 
-
-    // 	- Loop through the files in  FilestoreDirectory 
-	//	- check in database if the file is closed
-	//	- if closed then delete the file
-	
-    while ($file = readdir($dir_handle)) {
-	
-	// skip . and ..
-	if($file == "." || $file == ".." || strpos($config['cron_exclude prefix'],substr($file,0,1)) === 0)
-	{
-		logProcess("CRON","Ignored file: ".$FilestoreDirectory.$file);
-		continue;
-	}
-
-	// check filename in database
-	$query = "SELECT * FROM files WHERE  fileuid = '%s' AND filestatus = 'Available'";
-	$result = $db->fquery($query, substr($file,0,36));
-	
-	$total_results = pg_num_rows($result);
-	if($total_results < 1) {
-	// no Files Available match this file so delete the file
-	
-		if (is_file($FilestoreDirectory.$file) && file_exists($FilestoreDirectory.$file)) {
-		unlink($FilestoreDirectory.$file);
-		// log removal
-		logProcess("CRON","File Removed (Expired)".$FilestoreDirectory.$file);	
-		}
-	}
-    }
-
-    // Close
-    closedir($dir_handle);
-	
-	//
 	// Final cleanup is to close any records in the database that do not have a physical file attached to them
 	// close all entries that do not have a pyhsical file in storage
 	
@@ -193,32 +130,28 @@ function cleanUp()
 		
 		// check for error in SQL
 		if (!$search) { 
-		logProcess("CRON","SQL Error on updating files".pg_last_error());
+		logProcess("CONVERT","SQL Error on updating files".pg_last_error());
 		return FALSE; 
 		}
 	
 	while($row = pg_fetch_assoc($search)) {
 
-		// we don't use ensureSaneFileUid()/sanitizeFilename() here because file_exists()
-		// is harmless, and the sanitized uid/filename might coincide with another file,
-		// in which case this file would never get status = Closed.
-		if (!file_exists($FilestoreDirectory."/".$row["fileuid"].".tmp")) {
-
-		// change status to closed in database
-		$query = "UPDATE Files SET filestatus = 'Closed' WHERE fileid='%s'";
-		
-		$result = $db->fquery($query, $row['fileid']);
-		
-		// check for error in SQL
-		if (!$result) { 
-			logProcess("CRON","SQL Error Updating files ".pg_last_error());
-			return FALSE; 
+		if ($rename_to_old_scheme) {
+		    $oldfile = $FilestoreDirectory."/".$row["fileuid"].".tmp";
+		    $newfile = $FilestoreDirectory."/".$row["fileuid"].sanitizeFilename($row["fileoriginalname"]);
+		} else {
+		    $oldfile = $FilestoreDirectory."/".$row["fileuid"].sanitizeFilename($row["fileoriginalname"]);
+		    $newfile = $FilestoreDirectory."/".$row["fileuid"].".tmp";
 		}
-		
-		logProcess("CRON","Removed (File not Available) ".$FilestoreDirectory."/".$row["fileuid"].".tmp");
-			
+		if (file_exists($oldfile)) {
+		    $result = rename($oldfile, $newfile);
+		    // check for error 
+		    if (!$result) { 
+		        logProcess("CONVERT","Error renaming file ". $oldfile . " to " . $newfile);
+		    } else {
+		        logProcess("CONVERT","Renamed " . $oldfile . " to " . $newfile);
+		    }
 		}
-		
 	}
 	return true;
 	}
