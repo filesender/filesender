5800c2a27ef27e806eea14c8083c0c70cf94d88d
Chris Richter <chris@ricoshae.com.au>
2011-04-10 13:19:34 +0000

    Fix #271 - Filesender 1.0 running on SAML 1.7.0
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/branches/filesender-1.0@523 8555b757-d854-4b86-925a-82fd84c90ff4

--- www/fs_uploadit.php
+++ www/fs_uploadit.php
@@ -40,9 +40,13 @@ function upperHexNumber($matches) {
     return '\u'.strtoupper($matches[1]);
 }
 
-require_once('../classes/_includes.php');
-
-// flash upoload creates a new session id https so we need to make sure we are using the same session  
+// use token if available for SIMPLESAML 1.7 or set session if earlier version of SIMPLESAML
+if (isset($_POST['token']) && $_POST['token'] != "") {
+	
+	$_COOKIE['SimpleSAMLAuthToken'] = $_POST['token'];
+	} else {
+	
+	// flash upoload creates a new session id https so we need to make sure we are using the same session  
 if(!empty($_REQUEST['s'])) { 
     session_id($_REQUEST['s']); 
     session_start();
@@ -56,10 +60,11 @@ if(!empty($_REQUEST['s'])) {
         $_SESSION['validSession'] = true;
         trigger_error("Invalid session supplied.", E_USER_ERROR);
     }
+	}	
 }
-
-$authvoucher = AuthVoucher::getInstance();
-
+	
+require_once('../classes/_includes.php');
+ logEntry($_COOKIE['SimpleSAMLAuthToken'] .":". $_POST['token']);
 // Check if there is a file supplied
 if(!isset($_FILES['Filedata'])) trigger_error("request without file upload", E_USER_ERROR);
 
@@ -70,6 +75,7 @@ $cofig = $CFG->loadConfig();
 date_default_timezone_set($config['Default_TimeZone']);
 
 if($authvoucher->aVoucher() || $authsaml->isAuth()) { 
+
     $uploadfolder =  $config["site_filestore"];
 
 
