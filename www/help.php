f829ea490bf1c49a464939be6c8073a296e62cb2
Xander Jansen <xander.jansen@surfnet.nl>
2011-04-27 06:44:57 +0000

    Updated help.php (added non-standard semi-colon)
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/branches/filesender-1.0@556 8555b757-d854-4b86-925a-82fd84c90ff4

--- www/help.php
+++ www/help.php
@@ -83,7 +83,7 @@ require_once('../classes/_includes.php');
 <h4>Limits</h4>
     <ul>
     <li><strong>
-      Maximum recipient  addresses per email:</strong>  <?php echo $config["max_email_recipients"]?> multiple email addresses can be  separated by a comma</li>
+      Maximum recipient  addresses per email:</strong>  <?php echo $config["max_email_recipients"]?> multiple email addresses (separated by comma or semi-colon)</li>
     <li><strong>Maximum number of files per  upload:</strong> one - to upload several files at once, zip them into a  single archive first</li>
     <li><strong>Maximum file size per upload, without  Gears: </strong> <?php echo formatBytes($config["max_flash_upload_size"])?></li>
     <li><strong>Maximum file size per upload, with Gears: </strong> <?php echo formatBytes($config["max_gears_upload_size"])?></li>
