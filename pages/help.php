225f986c2ae4b96ccca7ba7a1556f6ff44077860
Chris Richter <chris@ricoshae.com.au>
2011-05-17 04:32:44 +0000

    Removed references to GEARS
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/branches/filesender-1.5@603 8555b757-d854-4b86-925a-82fd84c90ff4

--- pages/help.php
+++ pages/help.php
@@ -53,8 +53,8 @@
   <ul>
     <li><strong> Maximum recipient  addresses per email:</strong> <?php echo $config["max_email_recipients"]?> multiple email addresses can be  separated by a comma</li>
     <li><strong>Maximum number of files per  upload:</strong> one - to upload several files at once, zip them into a  single archive first</li>
-    <li><strong>Maximum file size per upload, without  Gears: </strong> <?php echo formatBytes($config["max_flash_upload_size"])?></li>
-    <li><strong>Maximum file size per upload, with Gears: </strong> <?php echo formatBytes($config["max_gears_upload_size"])?></li>
+    <li><strong>Maximum file size per upload, without HTML 5: </strong> <?php echo formatBytes($config["max_flash_upload_size"])?></li>
+    <li><strong>Maximum file size per upload, with HTML 5: </strong> <?php echo formatBytes($config["max_gears_upload_size"])?></li>
     <li> <strong>Maximum  file / voucher expiry days: </strong><?php echo $config["default_daysvalid"]?> </li>
   </ul>
   <p>For more information please visit <a href="http://www.filesender.org/">www.filesender.org</a></p>
