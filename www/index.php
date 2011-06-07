6bc915d35fa8d354228506b7136fa495f213f750
Chris Richter <chris@ricoshae.com.au>
2011-06-07 12:46:53 +0000

    Remove Firebug (dev code)
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/branches/filesender-1.5@638 8555b757-d854-4b86-925a-82fd84c90ff4

--- www/index.php
+++ www/index.php
@@ -81,7 +81,6 @@ $s = "logon";
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <title>FileSender:</title>
-<script type="text/javascript" src="https://getfirebug.com/firebug-lite.js"></script>
 <link rel="stylesheet" type="text/css" href="css/default.css" />
 <link rel="stylesheet" type="text/css" href="css/progress.css" />
 <link rel="icon" href="favicon.ico"	type="image/x-icon">
