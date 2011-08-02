f2e00e3878b092760e317619d4deca80a2c89086
xjansen <xjansen@8555b757-d854-4b86-925a-82fd84c90ff4>
2011-08-02 19:17:35 +0000

    En omdat het niet over glazuur gaat wat streepjes erbij.
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/branches/filesender-1.5@743 8555b757-d854-4b86-925a-82fd84c90ff4

--- language/NL_nl.php
+++ language/NL_nl.php
@@ -82,8 +82,8 @@
 
 	// Vouchers
 	DEFINE ("_SEND_NEW_VOUCHER", "Met een Uitnodiging kunt u iemand een bestand laten sturen.<br>
-Om een Uitnodiging te maken voer een emailadres in en klik op Stuur Uitnodiging.<BR>
-Er wordt dan een email verstuurd naar de ontvanger met daarin een link om de uitnodiging te gebruiken.");
+Om een Uitnodiging te maken voer een e-mailadres in en klik op Stuur Uitnodiging.<BR>
+Er wordt dan een e-mail verstuurd naar de ontvanger met daarin een link om de uitnodiging te gebruiken.");
 	DEFINE ("_EMAIL_SEPARATOR_MSG", "(Meerdere e-mails gescheiden door, of ;)");
 
 	// Flash button menu
