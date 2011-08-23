815ac08e30901d2ad7f344e2ffd86d91c76cf430
Chris Richter <chris@ricoshae.com.au>
2011-08-23 22:57:41 +0000

    test #490 PHP session timeout with large (long) flash uploads
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/branches/filesender-1.5@783 8555b757-d854-4b86-925a-82fd84c90ff4

---
+++ www/keepalive.php
@@ -0,0 +1,17 @@
+<?php
+require_once('../classes/_includes.php');
+
+$authsaml = AuthSaml::getInstance();
+$authvoucher = AuthVoucher::getInstance();
+
+date_default_timezone_set($config['Default_TimeZone']);
+
+	if ($authvoucher->aVoucher()) {
+		logEntry("Voucher: * Keep alive *");
+	}
+	// else add SAML eduPersonTargetedID
+	else if( $authsaml->isAuth()) {
+		logEntry("Auth: * Keep alive *");
+	} 
+	
+?>
