14ce9b65b9bfb4552946d25f53ca6cf8887a308b
xjansen <xjansen@8555b757-d854-4b86-925a-82fd84c90ff4>
2011-08-31 20:52:56 +0000

    test #521 Use utf8 safe version of htmlentities (utf8tohtml from includes/UTF8.php)
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/branches/filesender-1.5@831 8555b757-d854-4b86-925a-82fd84c90ff4

--- pages/admin.php
+++ pages/admin.php
@@ -247,7 +247,7 @@ echo '<td><strong>'.lang("_EXPIRY").'</strong></td>';
         </tr>
         <?php 
 foreach($Available as $item) {
-echo "<tr><td class='HardBreak'>" .$item['fileto'] . "</td><td class='HardBreak'>" .$item['filefrom'] . "</td><td class='HardBreak'>" .$item['fileoriginalname']. "</td><td>" .formatBytes($item['filesize']). "</td><td class='HardBreak'>".htmlentities($item['filesubject']). "</td><td>" .date($config['datedisplayformat'],strtotime($item['filecreateddate'])) . "</td><td>" .date($config['datedisplayformat'],strtotime($item['fileexpirydate'])) . "</td></tr>"; //etc
+echo "<tr><td class='HardBreak'>" .$item['fileto'] . "</td><td class='HardBreak'>" .$item['filefrom'] . "</td><td class='HardBreak'>" .$item['fileoriginalname']. "</td><td>" .formatBytes($item['filesize']). "</td><td class='HardBreak'>".utf8tohtml($item['filesubject'],TRUE). "</td><td>" .date($config['datedisplayformat'],strtotime($item['filecreateddate'])) . "</td><td>" .date($config['datedisplayformat'],strtotime($item['fileexpirydate'])) . "</td></tr>"; //etc
 }
 
 ?>
@@ -282,7 +282,7 @@ echo '<td><strong>'.lang("_EXPIRY").'</strong></td>';
 ?>
             <?php 
 foreach($Voucher as $item) {
-echo "<tr><td class='HardBreak'>" .$item['fileto'] . "</td><td class='HardBreak'>" .$item['filefrom'] . "</td><td class='HardBreak'>".htmlentities($item['filesubject']). "</td><td>" .date($config['datedisplayformat'],strtotime($item['filecreateddate'])) . "</td><td>" .date($config['datedisplayformat'],strtotime($item['fileexpirydate'])) . "</td></tr>"; //etc
+echo "<tr><td class='HardBreak'>" .$item['fileto'] . "</td><td class='HardBreak'>" .$item['filefrom'] . "</td><td class='HardBreak'>".utf8tohtml($item['filesubject'],TRUE). "</td><td>" .date($config['datedisplayformat'],strtotime($item['filecreateddate'])) . "</td><td>" .date($config['datedisplayformat'],strtotime($item['fileexpirydate'])) . "</td></tr>"; //etc
 }
 
 ?>
