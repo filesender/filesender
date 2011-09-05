be420c7f1863fade1acc4ae574a439994b47bc49
Chris Richter <chris@ricoshae.com.au>
2011-09-05 21:44:43 +0000

    test #506 My Files localizations Added tooltip delete to Vouchers
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/branches/filesender-1.5@857 8555b757-d854-4b86-925a-82fd84c90ff4

--- pages/vouchers.php
+++ pages/vouchers.php
@@ -155,7 +155,7 @@ $json_o=json_decode($filedata,true);
     </tr>
     <?php 
 foreach($json_o as $item) {
-   echo "<tr><td>" .$item['fileto'] . "</td><td>" .date($config['datedisplayformat'],strtotime($item['filecreateddate'])) . "</td><td>" .date($config['datedisplayformat'],strtotime($item['fileexpirydate'])) . "</td><td><div  style='cursor:pointer;'><img src='images/shape_square_delete.png' onclick='confirmdelete(".'"' .$item['filevoucheruid'] . '"'. ")' border='0'></div></td></tr>"; //etc
+   echo "<tr><td>" .$item['fileto'] . "</td><td>" .date($config['datedisplayformat'],strtotime($item['filecreateddate'])) . "</td><td>" .date($config['datedisplayformat'],strtotime($item['fileexpirydate'])) . "</td><td><div  style='cursor:pointer;'><img src='images/shape_square_delete.png' title='".lang("_DELETE")."' onclick='confirmdelete(".'"' .$item['filevoucheruid'] . '"'. ")' border='0'></div></td></tr>"; //etc
 }
 ?>
   </table>
