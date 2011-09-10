11bec3d79c72576558d3e4472398ea2dc164117c
xjansen <xjansen@8555b757-d854-4b86-925a-82fd84c90ff4>
2011-09-10 06:57:27 +0000

    Converted custom NO_no.php to UTF-8 encoding
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/branches/filesender-1.5@868 8555b757-d854-4b86-925a-82fd84c90ff4

--- config/NO_no.php
+++ config/NO_no.php
@@ -1,17 +1,17 @@
 <?php
 
 $lang["_HELP_TEXT"] = '<div align="left" style="padding:5px">
-    <p> Ta kontakt med din lokale IT-avdeling hvis pålogging feiler eller du ikke finner din institusjon i listen av hjemmeinstitusjoner. </p>
+    <p> Ta kontakt med din lokale IT-avdeling hvis pÃ¥logging feiler eller du ikke finner din institusjon i listen av hjemmeinstitusjoner. </p>
     <p> <strong>Systemkrav</strong><br>
-      En nyere utgave av en populær nettleser</p>
+      En nyere utgave av en populÃ¦r nettleser</p>
     <p> <strong>Begrensninger</strong><br>
       <strong> Maks. antall mottakere per sending:</strong> '. $config["max_email_recipients"].' flere mottaker adresser skilles med komma eller semikolon (eks. ole@norge.no, per@uninettt.no)<br>
-      <strong>Antall filer per sending:</strong> 1 - for å sende flere filer samtidig kan du pakke dem i feks. en zip-fil først.<br>
-      <strong>Største filstørrelse per sending, uten HTML 5: </strong>'. formatBytes($config["max_flash_upload_size"]) .'<br>
-      <strong>Største filstørrelse per sending, med HTML 5: </strong>' .formatBytes($config["max_gears_upload_size"]).'<br>
-      <strong>Maksimum antall dager før utløp av sending: </strong>'. $config["default_daysvalid"].'<br>
+      <strong>Antall filer per sending:</strong> 1 - for Ã¥ sende flere filer samtidig kan du pakke dem i feks. en zip-fil fÃ¸rst.<br>
+      <strong>StÃ¸rste filstÃ¸rrelse per sending, uten HTML 5: </strong>'. formatBytes($config["max_flash_upload_size"]) .'<br>
+      <strong>StÃ¸rste filstÃ¸rrelse per sending, med HTML 5: </strong>' .formatBytes($config["max_gears_upload_size"]).'<br>
+      <strong>Maksimum antall dager fÃ¸r utlÃ¸p av sending: </strong>'. $config["default_daysvalid"].'<br>
     </p>
-    <p>For mer informasjon besøk <a href="http://www.filesender.org/">www.filesender.org</a></p>
+    <p>For mer informasjon besÃ¸k <a href="http://www.filesender.org/">www.filesender.org</a></p>
   </div>';
 
 $lang["_ABOUT_TEXT"] = ' <div align="left" style="padding:5px">'. htmlentities($config['site_name']) .' is an installation of FileSender (<a rel="nofollow" href="http://www.filesender.org/">www.filesender.org</a>), which is developed to the requirements of the higher education and research community.</div>';
