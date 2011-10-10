200344596094e57dbc04645e1ebefe4d105edad1
maarten.koopmans <maarten.koopmans@8555b757-d854-4b86-925a-82fd84c90ff4>
2011-10-10 11:36:01 +0000

    emoved old, postgres only DB layer now that DBAL finally seems to work withou log errors.
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/branches/filesender-1.5@940 8555b757-d854-4b86-925a-82fd84c90ff4

--- classes/_includes.php
+++ classes/_includes.php
@@ -46,7 +46,6 @@ require_once("$filesenderbase/includes/UTF8.php");
 require_once("$filesenderbase/classes/Version.php");
 require_once("$filesenderbase/classes/Functions.php");
 require_once("$filesenderbase/classes/LanguageSelection.php");
-require_once("$filesenderbase/classes/DB.php");
 require_once("$filesenderbase/classes/DBAL.php");
 require_once("$filesenderbase/classes/AuthSaml.php");
 require_once("$filesenderbase/classes/AuthVoucher.php");
