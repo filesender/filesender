568cdeff6b6af83b68a5b477a14ceeebfb279587
Jan Meijer <jan.meijer@uninett.no>
2011-10-11 17:59:07 +0000

    #573, <title> hard configured: modified <title> so that it reads site_name from config
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/branches/filesender-1.5@952 8555b757-d854-4b86-925a-82fd84c90ff4

--- www/index.php
+++ www/index.php
@@ -79,7 +79,7 @@ $s = "logon";
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>FileSender:</title>
+<title><?php echo $config['site_name']; ?></title>
 <link rel="icon" href="favicon.ico"	type="image/x-icon">
 <link rel="shortcut icon" href="favicon.ico"type="image/x-icon">
 <link type="text/css" href="css/smoothness/jquery-ui-1.8.2.custom.css" rel="Stylesheet" />
