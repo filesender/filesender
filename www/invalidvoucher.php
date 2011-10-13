b6706d9aab97c493a072fae0798949777aac53c3
ricoshae <ricoshae@8555b757-d854-4b86-925a-82fd84c90ff4>
2011-10-13 06:36:35 +0000

    fixed #573 updated title for invalidvoucher.php and vouchererror.php
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/branches/filesender-1.5@956 8555b757-d854-4b86-925a-82fd84c90ff4

--- www/invalidvoucher.php
+++ www/invalidvoucher.php
@@ -42,7 +42,7 @@ require_once('../classes/_includes.php');
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title>FileSender:</title>
+<title><?php echo $config['site_name']; ?></title>
 <link rel="stylesheet" type="text/css" href="css/default.css" />
 <style type="text/css">
 <!--
