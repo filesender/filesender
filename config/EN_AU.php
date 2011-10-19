1fbf3fc1acd812a79e2f6633cc43830306ced265
meijer <meijer@8555b757-d854-4b86-925a-82fd84c90ff4>
2011-10-19 19:25:24 +0000

    Modified EN_AU help text with changes applied today to version 1.1 help, but removed non-needed flash references.
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/branches/filesender-1.5@981 8555b757-d854-4b86-925a-82fd84c90ff4

--- config/EN_AU.php
+++ config/EN_AU.php
@@ -1,18 +1,56 @@
 <?php
 
-$lang["_HELP_TEXT"] = '<div align="left" style="padding:5px">
-    <p> If you dont see your institution in the list of Identity Providers (IdPs), or your institutional login fails, please contact your local IT support </p>
-    <p> <strong>Requirements</strong><br>
-      A modern, current release of most popular browsers </p>
-    <p> <strong>Limits</strong><br>
-      <strong> Maximum recipient  addresses per email:</strong> '. $config["max_email_recipients"].' multiple email addresses can be  separated by a comma<br>
-      <strong>Maximum number of files per  upload:</strong> one - to upload several files at once, zip them into a  single archive first<br>
-      <strong>Maximum file size per upload, without HTML 5: </strong>'. formatBytes($config["max_flash_upload_size"]) .'<br>
-      <strong>Maximum file size per upload, with HTML 5: </strong>' .formatBytes($config["max_gears_upload_size"]).'<br>
-      <strong>Maximum  file / voucher expiry days: </strong>'. $config["default_daysvalid"].'<br>
-    </p>
-    <p>For more information please visit <a href="http://www.filesender.org/" target="_new tab">www.filesender.org</a></p>
-  </div>';
+$lang["_HELP_TEXT"] = '
+
+<div>
+<style type="text/css">
+.textmiddle {
+        vertical-align:middle;
+        padding-right: 10px;
+        padding-top: 5px;
+        padding-bottom: 5px;
+}
+</style>
+
+<div align="left" style="padding:5px">
+
+<h4>Login</h4> 
+<ul>
+    <li>If you do not see your institution in the list of Identity Providers (IdPs), or your institutional login fails, please contact your local IT support</li>
+</ul>
+
+<h4>Uploads smaller than 2 Gigabytes (2GB) with Adobe Flash</h4>
+<ul>
+	<li>If you can watch YouTube videos this method should work for you</li>
+	<li>You need a modern browser running version 10 (or higher) of <a target="_blank" href="http://www.adobe.com/software/flash/about/">Adobe Flash.</a></li>
+	<li>FileSender will warn you should you try to upload a file that is too big for this method</li>
+</ul>
+
+<h4>Uploads of <i>any size</i> with HTML5</h4>
+<ul>
+        <li>If you see <img src="images/html5_installed.png" alt="green HTML5 tick" class="textmiddle" style="display:inline" /> next to FileSender\'s Log Off button this method works for you</li>
+	<li>You need a very recent browser supporting HTML5, the latest version of the "language of the web".</li>
+	<li>Currently Firefox4 (and higher) and Chrome on both Windows, Mac OSX and Linux are known to work.</li>
+</ul>
+
+<h4>Downloads of any size</h4>
+<ul>
+        <li>You need a modern browser, Adobe Flash or HTML5 are <b>not</b> required for downloads</li>
+</ul>
+
+
+<h4>Limits of this FileSender installation</h4>
+<ul>
+    <li><strong>
+      Maximum recipient  addresses per email: </strong>'. $config["max_email_recipients"].' multiple email addresses (separated by comma or semi-colon)</li>
+    <li><strong>Maximum number of files per  upload:</strong> one - to upload several files at once, zip them into a  single archive first</li>
+    <li><strong>Maximum file size per upload, with Adobe Flash only: </strong>'. formatBytes($config["max_flash_upload_size"]).' </li>
+    <li><strong>Maximum file size per upload, with HTML5: </strong>'. formatBytes($config["max_gears_upload_size"]).'</li>
+    <li>      <strong>Maximum  file / voucher expiry days: </strong>'. $config["default_daysvalid"].' </li>
+</ul>
+<p>For more information please visit <a href="http://www.filesender.org/">www.filesender.org</a></p>
+</div>
+</div>';
 
 $lang["_ABOUT_TEXT"] = ' <div align="left" style="padding:5px">'. htmlentities($config['site_name']) .' is an installation of FileSender (<a rel="nofollow" href="http://www.filesender.org/" target="_new tab">www.filesender.org</a>), which is developed to the requirements of the higher education and research community.</div>';
 
