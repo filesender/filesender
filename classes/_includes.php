347a04bbdd4cee4afbd5a0e95f1e06a2f090ef67
ricoshae <ricoshae@8555b757-d854-4b86-925a-82fd84c90ff4>
2011-11-11 06:51:27 +0000

    Remove my dev settings
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/branches/filesender-1.5@1024 8555b757-d854-4b86-925a-82fd84c90ff4

--- classes/_includes.php
+++ classes/_includes.php
@@ -33,7 +33,7 @@
 
 $filesenderbase = dirname(dirname(__FILE__));
 
-require_once("$filesenderbase/config/config_dev.php");
+require_once("$filesenderbase/config/config.php");
 
 $CFG = config::getInstance();
 global $config;
@@ -41,7 +41,7 @@ $config = $CFG->loadConfig();  // use _global $config in all functions
 
 require_once("$filesenderbase/classes/unstrip.php");
 require_once("$filesenderbase/includes/ErrorHandler.php");
-//require_once("$filesenderbase/includes/EnsureHTTPS.php");
+require_once("$filesenderbase/includes/EnsureHTTPS.php");
 require_once("$filesenderbase/includes/UTF8.php");
 require_once("$filesenderbase/classes/Version.php");
 require_once("$filesenderbase/classes/Functions.php");
