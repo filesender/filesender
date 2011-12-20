74059c7add12ab145225b23ccc273aceb97ce374
xjansen <xjansen@8555b757-d854-4b86-925a-82fd84c90ff4>
2011-12-20 11:02:11 +0000

    test #605, re #615 : also protect filefrom to allow for mail addresses with quotes and the like.
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/branches/filesender-1.5@1097 8555b757-d854-4b86-925a-82fd84c90ff4

--- pages/files.php
+++ pages/files.php
@@ -200,7 +200,7 @@ $json_o=json_decode($filedata,true);
 		// populate form and open add-recipient modal form
 		$("#form1").attr("action", "index.php?s=files&a=add&id=" + vid );
 		$("#filevoucheruid").val(vid);
-		$("#filefrom").html(from);
+		$("#filefrom").html(decodeURIComponent(from));
 		$("#filename").html(filename);
 		$("#filesubject").val(decodeURIComponent(subject));
 		$("#filemessage").val(decodeURIComponent(message));
@@ -231,7 +231,7 @@ $json_o=json_decode($filedata,true);
 if(sizeof($json_o) > 0)
 {
 foreach($json_o as $item) {
-   echo '<tr><td valign="top"> <a href="index.php?s=files&a=resend&id=' .$item['filevoucheruid'] . '"><img src="images/email_go.png" title="'.lang("_RE_SEND_EMAIL").'"></a></td><td valign="top"><img src="images/email_add.png" title="'.lang("_NEW_RECIPIENT").'" onclick="openAddRecipient('."'".$item['filevoucheruid']."','".$item['fileoriginalname'] ."','".$item['filesize'] ."','".$item['filefrom']."','".rawurlencode($item['filesubject'])."','".rawurlencode($item['filemessage'])."'" .');"  style="cursor:pointer;"></td>';
+   echo '<tr><td valign="top"> <a href="index.php?s=files&a=resend&id=' .$item['filevoucheruid'] . '"><img src="images/email_go.png" title="'.lang("_RE_SEND_EMAIL").'"></a></td><td valign="top"><img src="images/email_add.png" title="'.lang("_NEW_RECIPIENT").'" onclick="openAddRecipient('."'".$item['filevoucheruid']."','".$item['fileoriginalname'] ."','".$item['filesize'] ."','".rawurlencode($item['filefrom'])."','".rawurlencode($item['filesubject'])."','".rawurlencode($item['filemessage'])."'" .');"  style="cursor:pointer;"></td>';
    if($item['fileto'] == $attributes["email"])
    {
    echo "<td class='HardBreak' valign='top'>".lang("_ME")."</td>";
