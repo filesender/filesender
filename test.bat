c72234bcb8d7153fc0a78102a6e5b1f16b47b794
xjansen <xjansen@8555b757-d854-4b86-925a-82fd84c90ff4>
2011-12-21 08:34:37 +0000

    Test OK
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/branches/filesender-1.5@1105 8555b757-d854-4b86-925a-82fd84c90ff4

--- test.bat
+++
@@ -1,2 +0,0 @@
-"D:\Program Files\Adobe\Adobe Flash Builder 4\sdks\4.0.0\bin\mxmlc" -output www/swf/filesenderup.swf flex/src/filesenderup.mxml  -theme="D:\Program Files\Adobe\Adobe Flash Builder 4\sdks\4.0.0\frameworks\themes\Halo\halo.swc" -compatibility-version=3.0 -static-link-runtime-shared-libraries=true 
-pause
