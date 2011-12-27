b13ca523d95f79110424465b9ce50b85afe286c8
xjansen <xjansen@8555b757-d854-4b86-925a-82fd84c90ff4>
2011-12-27 13:38:00 +0000

    Another round of minor w3c validator nitpicks, mainly <br>, self-closing tags and missing alt.
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/branches/filesender-1.5@1118 8555b757-d854-4b86-925a-82fd84c90ff4

--- config/NO_no.php
+++ config/NO_no.php
@@ -2,14 +2,14 @@
 
 $lang["_HELP_TEXT"] = '<div align="left" style="padding:5px">
     <p> Ta kontakt med din lokale IT-avdeling hvis pålogging feiler eller du ikke finner din institusjon i listen av hjemmeinstitusjoner. </p>
-    <p> <strong>Systemkrav</strong><br>
+    <p> <strong>Systemkrav</strong><br />
       En nyere utgave av en populær nettleser</p>
-    <p> <strong>Begrensninger</strong><br>
-      <strong> Maks. antall mottakere per sending:</strong> '. $config["max_email_recipients"].' flere mottaker adresser skilles med komma eller semikolon (eks. ole@norge.no, per@uninettt.no)<br>
-      <strong>Antall filer per sending:</strong> 1 - for å sende flere filer samtidig kan du pakke dem i feks. en zip-fil først.<br>
-      <strong>Største filstørrelse per sending, uten HTML 5: </strong>'. formatBytes($config["max_flash_upload_size"]) .'<br>
-      <strong>Største filstørrelse per sending, med HTML 5: </strong>' .formatBytes($config["max_html5_upload_size"]).'<br>
-      <strong>Maksimum antall dager før utløp av sending: </strong>'. $config["default_daysvalid"].'<br>
+    <p> <strong>Begrensninger</strong><br />
+      <strong> Maks. antall mottakere per sending:</strong> '. $config["max_email_recipients"].' flere mottaker adresser skilles med komma eller semikolon (eks. ole@norge.no, per@uninettt.no)<br />
+      <strong>Antall filer per sending:</strong> 1 - for å sende flere filer samtidig kan du pakke dem i feks. en zip-fil først.<br />
+      <strong>Største filstørrelse per sending, uten HTML 5: </strong>'. formatBytes($config["max_flash_upload_size"]) .'<br />
+      <strong>Største filstørrelse per sending, med HTML 5: </strong>' .formatBytes($config["max_html5_upload_size"]).'<br />
+      <strong>Maksimum antall dager før utløp av sending: </strong>'. $config["default_daysvalid"].'<br />
     </p>
     <p>For mer informasjon besøk <a href="http://www.filesender.org/">www.filesender.org</a></p>
   </div>';
