ab0ae5514ade8a46fc4dab216f4c851840a44f26
Xander Jansen <xander.jansen@surfnet.nl>
2012-04-22 18:05:54 +0000

    Merge r1336,1354 test #696 Change fileto/logto to text in SQL scripts
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/branches/filesender-1.5@1355 8555b757-d854-4b86-925a-82fd84c90ff4

--- scripts/mysql_filesender_db.sql
+++ scripts/mysql_filesender_db.sql
@@ -11,7 +11,7 @@ USE `filesender`;
 --
 
 CREATE TABLE `files` (
-  `fileto` varchar(250) DEFAULT NULL,
+  `fileto` text DEFAULT NULL,
   `filesubject` varchar(250) DEFAULT NULL,
   `filevoucheruid` varchar(60) DEFAULT NULL,
   `filemessage` text,
@@ -45,7 +45,7 @@ CREATE TABLE `logs` (
   `logfileuid` varchar(60) DEFAULT NULL,
   `logtype` varchar(60) DEFAULT NULL,
   `logfrom` varchar(250) DEFAULT NULL,
-  `logto` varchar(250) DEFAULT NULL,
+  `logto` text DEFAULT NULL,
   `logdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `logtime` time DEFAULT NULL,
   `logfilesize` bigint(20) DEFAULT NULL,
