9e887b201eed372d87feb2ad829a4498b1d8e5e2
Xander Jansen <xander.jansen@surfnet.nl>
2012-05-18 17:20:53 +0000

    1.1 test #739  Change in database column type for fileto/logto
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/branches/filesender-1.1@1528 8555b757-d854-4b86-925a-82fd84c90ff4

--- scripts/filesender_db.sql
+++ scripts/filesender_db.sql
@@ -1,6 +1,6 @@
 CREATE TABLE files
 ( 
-  fileto character varying(250),
+  fileto text,
   filesubject character varying(250),
   filevoucheruid character varying(60),
   filemessage text,
@@ -37,7 +37,7 @@ CREATE TABLE logs
   logfileuid character varying(60),
   logtype character varying(60),
   logfrom character varying(250),
-  logto character varying(250),
+  logto text,
   logdate timestamp without time zone,
   logtime time with time zone,
   logfilesize bigint,
