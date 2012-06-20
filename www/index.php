bf5b076ad943376b5175fdd5fff98f69261e06d9
Chris Richter <chris@ricoshae.com.au>
2012-06-20 13:20:57 +0000

    test #762 added force no cache
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/trunk@1559 8555b757-d854-4b86-925a-82fd84c90ff4

--- www/index.php
+++ www/index.php
@@ -85,7 +85,10 @@ if($isAuth )
 		$useremail = $userdata["email"];
 	}
 } 
-
+//Set no caching
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header('Pragma: no-cache');
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
