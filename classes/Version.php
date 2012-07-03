2851b867bcbd970b643f789f26ede16fdd7188e4
Xander Jansen <xander.jansen@surfnet.nl>
2012-07-03 15:58:42 +0000

    Mark 1.5 branch for beta4
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/branches/filesender-1.5@1585 8555b757-d854-4b86-925a-82fd84c90ff4

--- classes/Version.php
+++ classes/Version.php
@@ -32,7 +32,7 @@
 
 class FileSender_Version
 {
-	const VERSION = '1.5-beta3';
+	const VERSION = '1.5-beta4';
 
 	//-------------------------------------------------------
 	// Compare the specified version with the current version.
