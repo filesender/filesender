f36e41c257ee628ecce11e7b60af09651fa95137
Jan Meijer <jan.meijer@uninett.no>
2012-08-30 17:37:48 +0000

    code done, language needs "a lick"
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/trunk@1839 8555b757-d854-4b86-925a-82fd84c90ff4

--- language/no_NO.php
+++ language/no_NO.php
@@ -218,18 +218,54 @@ $lang["_SITE_SPLASHHEAD"] = "Velkommen til ". htmlspecialchars($config['site_nam
 $lang["_SITE_SPLASHTEXT"] = htmlspecialchars($config['site_name']) ." tilbyr en sikker og enkel måte for å sende store filer til alle dine samarbeidspartnere!  Logg på for å overføre filer eller for å invitere andre til å sende deg en fil.";
 
 // site help
-$lang["_HELP_TEXT"] = '<div align="left" style="padding:5px">
-    <p> Ta kontakt med din lokale IT-avdeling hvis pålogging feiler eller du ikke finner din institusjon i listen av hjemmeinstitusjoner. </p>
-    <p> <strong>Systemkrav</strong><br />
-      En nyere utgave av en populær nettleser</p>
-    <p> <strong>Begrensninger</strong><br />
-      <strong> Maks. antall mottakere per sending:</strong> '. $config["max_email_recipients"].' flere mottaker adresser skilles med komma eller semikolon (eks. ole@norge.no, per@uninettt.no)<br />
-      <strong>Antall filer per sending:</strong> 1 - for å sende flere filer samtidig kan du pakke dem i feks. en zip-fil først.<br />
-      <strong>Største filstørrelse per sending, uten HTML 5: </strong>'. formatBytes($config["max_flash_upload_size"]) .'<br />
-      <strong>Største filstørrelse per sending, med HTML 5: </strong>' .formatBytes($config["max_html5_upload_size"]).'<br />
-      <strong>Maksimum antall dager før utløp av sending: </strong>'. $config["default_daysvalid"].'<br />
-    </p>
-    <p>For mer informasjon besøk <a href="http://www.filesender.org/" target="_blank">www.filesender.org</a></p>
+$lang["_HELP_TEXT"] = '
+<div>
+<div align="left" style="padding:5px">
+<h4>Pålogging</h4>
+<ul>
+	<li>Du logger på med vanlige brukerkontoen du bruker hos din institusjon.  Ta kontakt med din lokale IT-helpdesk hvis pålogging feiler eller du ikke finner din institusjon i listen.</li>
+</ul>
+
+<h4>Opplastninger av <i>enhver størrelse</i> med HTML5</h4>
+<ul>
+	<li>Du kan bruke denne metoden hvis dette symbolet vises: <img src="images/html5_installed.png" alt="green HTML5 tick" class="textmiddle" style="display:inline" /></li>
+	<li>For å få <img src="images/html5_installed.png" alt="green HTML5 tick" class="textmiddle" style="display:inline" /> symbolet, er alt du trenger en modern nettleser som støtter HTML5, siste versjonen av webbens språk.</li>
+	<li>Nyere versjoner av Firefox og Chrome, på Windows, Mac OS X og Linux er kjent for å fungere</li>
+	<li>Du kan <b><i>gjenoppta</i></b> en avbrudd eller innstilt opplastning.  Du trenger bare å sende nøyaktig samme filen på nytt; så lenge filen har akkurat samme navn som sist vil <i>'. htmlspecialchars($config['site_name']) .'</i> gjenkjenne at det er en omstart.  Når opplastningen starter, burde du se fremdriftsindikatoren hoppe til hvor den var blitt sist, og fortsette derfra.<br /><br />
+	Har du <b><i>endret filen</i></b> mellom første og andre forsøk, bør du omnevne filen først.  Dermed forsikres det at det startes en ny, fersk opplastning og at al dine endringer vil bli overført uten problemer.</li>
+</ul>
+
+<h4>Nedlastninger av enhver størrelse</h4>
+<ul>
+	<li>Alle nyere populære nettlesere vil fungere helt fint når det gjelder nedlastninger av vilkårlig store filer.  Ikke bekymre deg om Adobe Flash eller HTML5 - disse er bare relevant for opplastninger; inte noe spesielt kreves for nedlastninger.</li>
+</ul>
+
+<h4>Opplastninger opp til 2 Gigabytes (2GB) med Adobe Flash</h4>
+<ul>
+	<li>Hvis du kan se på YouTube filmer burde denne metoden fungere for deg.</li>
+	<li>Du trenger en nyere nettleser med versjon 10 (eller høgere) av <a target="_blank" href="http://www.adobe.com/software/flash/about/">Adobe Flash</a> tillegsprogramvare.</li>
+	<li>Ved bruk av Adobe Flash kan du laste opp filer opp til 2 Gigabytes (2GB).  Du vil bli varslet skulle du prøve å laste opp en fil som er for stor.</li>
+	<li>Gjennoptagelse av opplastninger er ikke støttet med denne metoden.</li>
+
+</ul>
+
+<h4>Konfigurerte begrensninger til tjenesten</h4>
+<ul>
+<li><b>Maks. antall mottakere per sending: </b>'. $config["max_email_recipients"].' flere mottaker adresser skilles med komma eller semikolon (f.eks. ole@norge.no, per@uninettt.no).</li>
+      <li><b>Antall filer per sending: </b> 1 - for å sende flere filer samtidig kan du pakke dem i f.eks. en zip-fil først.</li>
+      <li><b>Største mulige filstørrelse, per sending, uten HTML 5: </b>'. formatBytes($config["max_flash_upload_size"]) .'</li>
+      <li><b>Største mulige filstørrelse, per sending, med HTML 5: </b>' .formatBytes($config["max_html5_upload_size"]).'</li>
+      <li><b>Maksimum antall dager før utløp av sending: </b>'. $config["default_daysvalid"].'</li>
+</ul>
+
+<h4>Tekniske detaljer</h4>
+<ul>
+<li>
+<i>'. htmlspecialchars($config['site_name']) .'</i> bruker <a href="http://www.filesender.org/" target="_blank">FileSender programvaren</a>.  FileSender antyder om HTML5 opplastning er støttet i en bestemt nettleser, eller ikke.  Dette er primært avhengig av avansert nettleserfunksjonalitet, primært støtte for HTML5 File APIen.  På <a href="http://caniuse.com/fileapi" target="_blank">"When can I use..."</a> nettsiden kan implementasjonsframgang av HTML5-støtten i alle populære nettlesere følges.  Spesielt støtte av <a href="http://caniuse.com/filereader" target="_blank">FileReader API</a> og <a href="http://caniuse.com/bloburls" target="_blank">Blob URL</a> trenger å være lyse-grønnt (=støttet) for at opplastninger større enn 2GB er støttet.  Vær obs på at selv om at det står at Opera 12 støtter HTML5 FileAPI, er det i skrivende stund fortsatt mangler i Operas implementasjon som fører til at HTML5 opplastning ikke fungerer med Opera.
+</ul>
+
+    <p>For mer informasjon henvises det til <a href="http://www.filesender.org/" target="_blank">www.filesender.org</a></p>
+</div>
   </div>';
 
 // site about
