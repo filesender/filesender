6603a0788fb72e7b0f3ca0a447e3d2b70cabcfaa
Xander Jansen <xander.jansen@surfnet.nl>
2012-08-30 15:24:51 +0000

    Merge combined updates to en_AU.php (HELP tag)
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/branches/filesender-1.5@1835 8555b757-d854-4b86-925a-82fd84c90ff4

--- language/en_AU.php
+++ language/en_AU.php
@@ -213,27 +213,29 @@ $lang["_HELP_TEXT"] = '
 
 <h4>Uploads of <i>any size</i> with HTML5</h4>
 <ul>
-        <li>If you see <img src="images/html5_installed.png" alt="green HTML5 tick" class="textmiddle" style="display:inline" /> this method works for you</li>
-	<li>You need a very recent browser supporting HTML5, the latest version of the "language of the web".</li>
-	<li>Currently Firefox 4 (and higher) and Chrome on Windows, Mac OS X and Linux are known to work.</li>
-	<li>You can <b><i>resume</i></b> an interrupted or cancelled upload.  Select the exact same file (the file must not be modified!), enter the recipients and click send.  You will notice the progress bar will hop to where the upload was stopped, and continue from there.  If you modified the file, please rename it first to make sure the upload starts with the beginning of the file again.</li>   
+        <li>You\'ll be able to use this method if this sign is displayed: <img src="images/html5_installed.png" alt="green HTML5 tick" class="textmiddle" style="display:inline" /></li>
+	<li>To get the <img src="images/html5_installed.png" alt="green HTML5 tick" class="textmiddle" style="display:inline" /> sign, simply use an up to date browser that supports HTML5, the latest version of the "language of the web".</li>
+	<li>Up to date versions of Firefox and Chrome on Windows, Mac OS X and Linux are known to work.</li>
+	<li>You can <b><i>resume</i></b> an interrupted or cancelled upload.  To resume an upload, simply send the exact same file again!  Make sure the file has the same name as before and <i>'. htmlspecialchars($config['site_name']) .'</i> will recognise it.  When your upload starts, you should notice the progress bar jump to where the upload was halted, and continue from there.<br /><br />
+If you <b><i>modified the file</i></b> between the first and second attempt, please rename the file first.  This ensures a new, fresh upload is started and all your changes are properly transferred.</li>
 </ul>
 
 <h4>Downloads of any size</h4>
 <ul>
-        <li>You need a modern browser, Adobe Flash or HTML5 are <b>not</b> required for downloads</li>
+        <li>Any modern browser will do just fine.  Don\'t worry about Adobe Flash or HTML5 - these only matter for uploads; nothing special is required for downloads</li>
 </ul>
 
 <h4>Uploads smaller than 2 Gigabytes (2GB) with Adobe Flash</h4>
 <ul>
 	<li>If you can watch YouTube videos this method should work for you</li>
-	<li>You need a modern browser running version 10 (or higher) of <a target="_blank" href="http://www.adobe.com/software/flash/about/">Adobe Flash.</a></li>
-	<li>FileSender will warn you should you try to upload a file that is too big for this method</li>
+	<li>You need a modern browser with version 10 (or higher) of the <a target="_blank" href="http://www.adobe.com/software/flash/about/">Adobe Flash</a> plugin.</li>
+	<li>Using Adobe Flash you can upload file sizes of up to 2 Gigabytes (2GB).  <i>'. htmlspecialchars($config['site_name']) .'</i> will warn you should you try to upload a file that is too big for this method</li>
+	<li>Resuming uploads is not supported with this method</li>
 </ul>
 
 
 
-<h4>Limits of this FileSender installation</h4>
+<h4>Configured service constraints</h4>
 <ul>
     <li><strong>
       Maximum recipient  addresses per email:</strong> Up to '. $config["max_email_recipients"].' email addresses separated by  a comma or semi-colon</li>
@@ -243,9 +245,9 @@ $lang["_HELP_TEXT"] = '
     <li><strong>Maximum file / voucher expiry days: </strong>'. $config["default_daysvalid"].' </li>
 </ul>
 
-<h4>For your IT support</h4>
+<h4>Technical details</h4>
 <ul>
-	<li>FileSender indicates whether or not the HTML5 upload method is supported for a particular browser.  This depends mainly on the availability of advanced browser functionality, in particular the HTML5 FileAPI.  Please use the <a href="http://caniuse.com/#feat=fileapi" target="_blank">"When can I use..."</a> website to monitor implementation progress of the HTML5 FileAPI for all major browsers.  In particular support for <a href="http://caniuse.com/#feat=filereader" target="_blank">FileReader API</a> and <a href="http://caniuse.com/#feat=bloburls" target="_blank">Blob URLs</a> needs to be light green (=supported) for a browser to support uploads larger then 2GB.  Please note that although Opera 12 is listed to support the HTML5 FileAPI, it currently does not support all that is needed to support use of the HTML5 upload method in FileSender.</li>
+	<li><i>'. htmlspecialchars($config['site_name']) .'</i> uses the <a href="http://www.filesender.org/" target="_blank">FileSender software</a>. FileSender indicates whether or not the HTML5 upload method is supported for a particular browser.  This depends mainly on the availability of advanced browser functionality, in particular the HTML5 FileAPI.  Please use the <a href="http://caniuse.com/fileapi" target="_blank">"When can I use..."</a> website to monitor implementation progress of the HTML5 FileAPI for all major browsers.  In particular support for <a href="http://caniuse.com/filereader" target="_blank">FileReader API</a> and <a href="http://caniuse.com/bloburls" target="_blank">Blob URLs</a> needs to be light green (=supported) for a browser to support uploads larger then 2GB.  Please note that although Opera 12 is listed to support the HTML5 FileAPI, it currently does not support all that is needed to support use of the HTML5 upload method in FileSender.</li>
 </ul>
 
 <p>For more information please visit <a href="http://www.filesender.org/" target="_blank">www.filesender.org</a></p>
