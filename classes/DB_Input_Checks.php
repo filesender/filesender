9a4624c4a3aae2a528d343d9d0b907de114f16a3
xjansen <xjansen@8555b757-d854-4b86-925a-82fd84c90ff4>
2012-09-09 11:10:10 +0000

    test #813 Simplify checkIp6() function by using filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6) - removes dependency on php-gmp - clean up of code - unused and unfinished checkdateformat() removed - patch contributed by Thijs and slightly simplified by using filter_var instead of inet_pton()
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/trunk@1896 8555b757-d854-4b86-925a-82fd84c90ff4

--- classes/DB_Input_Checks.php
+++ classes/DB_Input_Checks.php
@@ -86,11 +86,10 @@ class DB_Input_Checks {
     }
 
     public function checkIp6($ip) {
-        if (strpos($ip, ':') !== FALSE) {
-            return long2ip6(ip2long6($ip));
-        } else {
-            return "::";
+        if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)) {
+            return $ip;
         }
+        return "::";
     }
 
     /*
@@ -105,52 +104,3 @@ class DB_Input_Checks {
 
 }
 
-/*
- * Ip6 check functions. Makes a ip2long and long2ip for ipv6.
- */
-
-function ip2long6($ipv6) { 
-    $ipv6long="";
-    if (strpos($ipv6, ':') !== FALSE) {
-       $ip_n = inet_pton($ipv6); 
-       $bits = 15; // 16 x 8 bit = 128bit 
-
-       while ($bits >= 0) { 
-           $bin = sprintf("%08b",(ord($ip_n[$bits]))); 
-           $ipv6long = $bin.$ipv6long; 
-           $bits--; 
-       } 
-       return gmp_strval(gmp_init($ipv6long,2),10); 
-    } else {
-       return FALSE ;
-    }
-} 
-
-function long2ip6($ipv6long) { 
-    $ipv6="";
-    $bin = gmp_strval(gmp_init($ipv6long,10),2); 
-    if (strlen($bin) < 128) { 
-        $pad = 128 - strlen($bin); 
-        for ($i = 1; $i <= $pad; $i++) { 
-            $bin = "0".$bin; 
-        } 
-    } 
-
-    $bits = 0; 
-    while ($bits <= 7) { 
-        $bin_part = substr($bin,($bits*16),16); 
-        $ipv6 .= dechex(bindec($bin_part)).":"; 
-        $bits++; 
-    } 
-    // compress 
-
-    return inet_ntop(inet_pton(substr($ipv6,0,-1))); 
-} 
-
-function checkdateformat($date,$dateformat) { 
-
-    return $date; 
-} 
-
-
-?>
