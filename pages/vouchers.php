84fc7295d2486a33be7ae1df740ba8375484ab94
Xander Jansen <xander.jansen@surfnet.nl>
2012-11-28 09:27:09 +0000

    re #637 Removed spurious <tr> to keep the w3c validator happy
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/trunk@2033 8555b757-d854-4b86-925a-82fd84c90ff4

--- pages/vouchers.php
+++ pages/vouchers.php
@@ -225,7 +225,6 @@ $json_o=json_decode($filedata,true);
         <td colspan="2"><textarea name="vouchermessage" cols="57" rows="4" id="vouchermessage"></textarea></td>
       </tr>
       <tr>
-      <tr>
         <td class="mandatory" id="vouchers_expirydate"><?php echo lang("_EXPIRY_DATE"); ?>:</td>
         <td><input id="datepicker" onchange="validate_expiry()" title="<?php echo lang('_DP_dateFormat'); ?>" />
         <div id="expiry_msg" class="validation_msg" style="display:none"><?php echo lang("_INVALID_EXPIRY_DATE"); ?></div>
