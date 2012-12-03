49ee04ad17b9a6117d1c78c4d7e77f854605dec7
Xander Jansen <xander.jansen@surfnet.nl>
2012-12-03 20:08:02 +0000

    re #142 encode and nl2br voucher message in voucher listing
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/trunk@2052 8555b757-d854-4b86-925a-82fd84c90ff4

--- pages/vouchers.php
+++ pages/vouchers.php
@@ -253,7 +253,7 @@ $json_o=json_decode($filedata,true);
 	$i = 0; 
 	foreach($json_o as $item) {
 		$i += 1; // counter for file id's
-		echo "<tr><td>" .$item['fileto'] . "</td><td class='HardBreak'>" .$item['filemessage'] . "</td><td>" .date($lang['datedisplayformat'],strtotime($item['filecreateddate'])) . "</td><td>" .date($lang['datedisplayformat'],strtotime($item['fileexpirydate'])) . "</td><td><div  style='cursor:pointer;'><img id='btn_deletevoucher_".$i."' src='images/shape_square_delete.png' alt='' title='".lang("_DELETE")."' onclick='confirmdelete(".'"' .$item['filevoucheruid'] . '"'. ")' border='0' /></div></td></tr>"; //etc
+		echo "<tr><td>" .$item['fileto'] . "</td><td class='HardBreak'>" . nl2br(utf8tohtml($item["filemessage"],TRUE)) . "</td><td>" .date($lang['datedisplayformat'],strtotime($item['filecreateddate'])) . "</td><td>" .date($lang['datedisplayformat'],strtotime($item['fileexpirydate'])) . "</td><td><div  style='cursor:pointer;'><img id='btn_deletevoucher_".$i."' src='images/shape_square_delete.png' alt='' title='".lang("_DELETE")."' onclick='confirmdelete(".'"' .$item['filevoucheruid'] . '"'. ")' border='0' /></div></td></tr>"; //etc
 	}
 ?>
   </table>
