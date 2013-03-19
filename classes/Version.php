b44125bd24a127147825e3a0a2cad47abee35b5a
Chris Richter <chris@ricoshae.com.au>
2013-03-19 20:47:49 +0000

    test #385 Terasender code in trunk
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/trunk@2121 8555b757-d854-4b86-925a-82fd84c90ff4

--- classes/Version.php
+++ classes/Version.php
@@ -32,7 +32,7 @@
 
 class FileSender_Version
 {
-	const VERSION = '1.5-trunk';
+	const VERSION = '1.5-trunk-terasender';
 	const DBVERSION  = '1.5'; 
 	//-------------------------------------------------------
 	// Compare the specified version with the current version.
