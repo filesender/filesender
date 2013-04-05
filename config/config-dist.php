d91b4c476a188c6284233ced9b85f5ceeb49da0f
ricoshae <ricoshae@8555b757-d854-4b86-925a-82fd84c90ff4>
2013-04-05 22:41:27 +0000

    test #889 temp folder for terasender Cron clean for temp folder after x days
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/trunk@2125 8555b757-d854-4b86-925a-82fd84c90ff4

--- config/config-dist.php
+++ config/config-dist.php
@@ -148,7 +148,7 @@ public function loadConfig() {
 	$config['cron_exclude prefix'] = '_'; // exclude deletion of files with the prefix character listed (can use multiple characters eg '._' will ignore .xxxx and _xxxx
 	$config['cron_shred'] = false; // instead of simply unlinking, overwrite expired files so they are hard to recover
 	$config['cron_shred_command'] = '/usr/bin/shred -f -u -n 1 -z'; // overwrite once (-n 1) with random data, once with zeros (-z), then remove (-u)
-
+	$config["cron_cleanuptempdays"] = 7;
 	// email templates section
 	$config['default_emailsubject'] = "{siteName}: {filename}";
 	$config['filedownloadedemailbody'] = '{CRLF}--simple_mime_boundary{CRLF}Content-type:text/plain; charset={charset}{CRLF}{CRLF}
