d55fdce8823bc2e4be515fc932b9f66959c9d0d0
Xander Jansen <xander.jansen@surfnet.nl>
2013-04-13 15:25:07 +0000

    re #819 Update jquery for www/logout.php (untested)
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/trunk@2139 8555b757-d854-4b86-925a-82fd84c90ff4

--- www/logout.php
+++ www/logout.php
@@ -53,11 +53,11 @@ session_destroy();
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <title><?php echo htmlspecialchars($config['site_name']); ?></title>
 <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
-<link type="text/css" href="css/smoothness/jquery-ui-1.8.2.custom.css" rel="Stylesheet" />
+<link type="text/css" href="css/smoothness/jquery-ui-1.10.2.custom.css" rel="Stylesheet" />
 <link rel="stylesheet" type="text/css" href="css/default.css?<?php echo FileSender_Version::VERSION; ?>" />
 <script type="text/javascript" src="js/common.js" ></script>
-<script type="text/javascript" src="js/jquery-1.7.min.js" ></script>
-<script type="text/javascript" src="js/jquery-ui-1.8.1.custom.min.js"></script>
+<script type="text/javascript" src="js/jquery-1.9.1.min.js" ></script>
+<script type="text/javascript" src="js/jquery-ui-1.10.2.custom.min.js"></script>
 <script type="text/javascript">
 $(function() {
 $( "a", ".menu" ).button();
