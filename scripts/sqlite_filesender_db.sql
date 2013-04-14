8c8340c23731ff2a8d755207a449d3935e1284b2
Xander Jansen <xander.jansen@surfnet.nl>
2013-04-14 09:31:03 +0000

    Qtest 1.6 re #888 re #888 Add sqlite init-script and 'how to' Patch contributed by Dick Visser
    Merge from trunk/r2157
    
    
    
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/qtest/filesender-1.6@2159 8555b757-d854-4b86-925a-82fd84c90ff4

---
+++ scripts/sqlite_filesender_db.sql
@@ -0,0 +1,60 @@
+-- 
+-- Use this code to generate an SQLite database.
+--
+-- NOTE
+-- Because SQLite does dynamic typing, almost all columns
+-- are TEXT types. See http://www.sqlite.org/datatype3.html.
+-- 
+CREATE TABLE files (
+	fileto TEXT,
+	filesubject TEXT,
+	filevoucheruid TEXT,
+	filemessage TEXT,
+	filefrom TEXT,
+	filesize INTEGER,
+	fileoriginalname TEXT,
+	filestatus TEXT,
+	fileip4address TEXT,
+	fileip6address TEXT,
+	filesendersname TEXT,
+	filereceiversname TEXT,
+	filevouchertype TEXT,
+	fileuid TEXT,
+	fileid INTEGER PRIMARY KEY AUTOINCREMENT,
+	fileexpirydate TEXT,
+	fileactivitydate TEXT,
+	fileauthuseruid TEXT,
+	filecreateddate TEXT,
+	fileauthurl TEXT,
+	fileauthuseremail TEXT
+);
+
+CREATE TRIGGER UPDATE_FILEEXPIRYDATE AFTER UPDATE ON files
+	BEGIN
+		UPDATE files SET fileexpirydate = datetime('now')
+		WHERE rowid = new.rowid;
+	END
+;
+
+CREATE TABLE logs (
+	logid INTEGER PRIMARY KEY AUTOINCREMENT,
+	logfileuid TEXT,
+	logtype TEXT,
+	logfrom TEXT,
+	logto TEXT,
+	logdate TEXT,
+	logtime TEXT,
+	logfilesize INTEGER,
+	logfilename TEXT,
+	logsessionid TEXT,
+	logmessage TEXT,
+	logvoucheruid TEXT,
+	logauthuseruid TEXT
+);
+
+CREATE TRIGGER UPDATE_LOGDATE AFTER UPDATE ON logs
+	BEGIN
+		UPDATE logs SET logdate = datetime('now')
+		WHERE rowid = new.rowid;
+	END
+;
