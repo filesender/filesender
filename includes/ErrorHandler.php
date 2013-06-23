3e2fed16939496b60869c51e14e5571ca084ae15
Xander Jansen <xander.jansen@surfnet.nl>
2013-06-23 17:41:11 +0000

    Qtest 1.6 test #897 replace $_REQUEST['PHPSESSID'] with session_id() Merged /filesender/trunk:r2200
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/qtest/filesender-1.6@2280 8555b757-d854-4b86-925a-82fd84c90ff4

--- includes/ErrorHandler.php
+++ includes/ErrorHandler.php
@@ -103,8 +103,8 @@ function logEntry($message, $type = "E_NOTICE"){
 	$myFile = $config['log_location'].$dateref.$logext;
 	$fh = fopen($myFile, 'a') or die("can't open file");
 	// don't print errors on screen when there is no session.
-	if(isset($_REQUEST['PHPSESSID'])){
-		$sessionId = $_REQUEST['PHPSESSID'];
+	if(session_id()){
+		$sessionId = session_id();
 	} else {
 		$sessionId = "none";
 	}
