ad76c944669de4b7096554f32e0e24350d5309b4
Jack Kittridge <jackKittridge92@gmail.com>
2013-07-08 13:36:17 +0000

    Re #948 Added 'upload_html5_js.php' and moved functions 'validate_file(n), validateForm, constrainNumWebWorkers & checkFilesSelected into this new file (from multiupload.php)
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/playground/filesender-1.5-multiupload-3@2359 8555b757-d854-4b86-925a-82fd84c90ff4

--- pages/multiupload.php
+++ pages/multiupload.php
@@ -44,6 +44,7 @@
 // --------------------------------------------------------
 
 require_once('../www/upload_common_js.php');
+require_once('../www/upload_html5_js.php');
 ?>
 
 <script type='text/javascript' src='lib/js/AC_OETags.js'></script>
@@ -117,61 +118,6 @@ $(function () {
 
     autoCompleteEmails();
 });
-
-// --------------------------
-// Validation functions
-// --------------------------
-
-// Validate FILE (with embedded calls to check filename and file-extension)
-function validate_file(id) {
-    fileMsg('');
-
-    if (!fileData[id]) {
-        // display message if a user enters all form details and selects upload without selecting a file
-        // in theory this error should not appear as a browse button should not be visible without a file first being selected
-        fileMsg('<?php echo lang('_SELECT_FILE') ?>');
-        return false;
-    } else {
-        var file = fileData[id];
-
-        if (!validateFileName(file.name)) {
-            return false;
-        } else if (file.size < 1) {
-            fileMsg('<?php echo lang('_INVALID_FILESIZE_ZERO') ?>');
-            return false;
-        } else if (file.size > maxHTML5UploadSize) {
-            fileMsg('<?php echo lang('_INVALID_TOO_LARGE_1') ?> ' + readablizebytes(maxHTML5UploadSize) + '. <?php echo lang('_SELECT_ANOTHER_FILE') ?> ');
-            return false;
-        }
-    }
-    return true;
-}
-
-// HTML5 form Validation
-function validateForm() {
-    // remove messages from any previous attempt
-    hideMessages();
-    return (validate_fileto() && validate_file(n) && validate_expiry() && validate_aup());
-}
-
-function constrainNumWebWorkers() {
-    <?php
-        $limit = 'undefined';
-        if (isset($config['webWorkersLimit'])) {
-            $limit = $config['webWorkersLimit'];
-        }
-    ?>
-
-    var maxLimitWebWorkers = <?php echo $limit; ?>;
-    var workerCount = $('#workerCount');
-    if (maxLimitWebWorkers != 'undefined' && parseInt(workerCount.val()) > maxLimitWebWorkers) {
-        workerCount.val(maxLimitWebWorkers);
-    }
-}
-
-function checkFilesSelected() {
-    return document.getElementById('fileToUpload').files[0] != null || document.getElementById('file_0') != null;
-}
 
 // special fix for esc key on firefox stopping xhr
 window.addEventListener('keydown', function (e) {
@@ -345,7 +291,6 @@ window.addEventListener('keydown', function (e) {
                 <a href="#" onclick="validate()"><?php echo lang('_SEND'); ?></a>
             </div>
         </div>
-
     </div>
     <div class="colmask threecol" id="dragfilestoupload"></div>
 </form>
