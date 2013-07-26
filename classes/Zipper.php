2a47eb7c9286b55afdf677d76c5cc6ab514cab10
vpolden <vpolden@8555b757-d854-4b86-925a-82fd84c90ff4>
2013-07-26 10:54:51 +0000

    Test #935: Added support for the ZIP64 format, enabling files larger than 4GB to work. Needs compatibility testing.
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/playground/filesender-1.5-multiupload-3@2417 8555b757-d854-4b86-925a-82fd84c90ff4

--- classes/Zipper.php
+++ classes/Zipper.php
@@ -41,6 +41,7 @@ class Zipper
 {
     private $files;
     private $centralDirectory;
+    private $useZip64 = false;
 
     public function __construct()
     {
@@ -65,6 +66,9 @@ class Zipper
     {
         global $functions, $config;
 
+        // Enable the ZIP64 format if total file size exceeds 4 GiB.
+        $this->useZip64 = $this->calculateTotalFileSize() >= 4 * 1024 * 1024 * 1024;
+
         $this->sendHttpHeaders();
         $offset = 0;
 
@@ -72,7 +76,7 @@ class Zipper
             // Set up metadata and send the local header.
             $name = preg_replace('/^\\/+/', '', $file["fileoriginalname"]); // Strip leading slashes from filename.
             $timestamp = $functions->unixToDosTime(strtotime($file["filecreateddate"]));
-            $localHeaderLength = $this->sendLocalFileHeader($name, $timestamp);
+            $localHeaderLength = $this->sendLocalFileHeader($name, $timestamp, $file["filesize"]);
 
             // File may be too large to hash all at once, so create a hash context and update it as the file is read.
             $path = $config['site_filestore'] . $file['fileuid'] . '.tmp';
@@ -125,14 +129,34 @@ class Zipper
         foreach ($this->files as $file) {
             $fileSize += 92 + strlen($file["fileoriginalname"]) * 2; // Size of the local file header, descriptor and per-file CDR entry.
             $fileSize += $file["filesize"]; // File data size.
+
+            if ($this->useZip64) {
+                $fileSize += 48; // Extra file data for ZIP64 format.
+            }
+        }
+
+        if ($this->useZip64) {
+            $fileSize += 76; // Extra end-of-file ZIP64 information.
         }
 
         return $fileSize;
     }
 
-    private function sendLocalFileHeader($filename, $timestamp)
+    private function pack64($value) {
+        // Takes an integer and returns it as a 64-bit unsigned little-endian binary string.
+        $left = ($value & 0xFFFFFFFF00000000) >> 32;
+        $right = $value & 0x00000000FFFFFFFF;
+
+        return pack('VV', $right, $left);
+    }
+
+    private function sendLocalFileHeader($filename, $timestamp, $fileSize)
     {
         $nameLength = strlen($filename);
+
+        // If using ZIP64, we need an extra field at the end of the header with 64-bit file sizes.
+        $extraFieldLength = $this->useZip64 ?  20 : 0;
+        $dataLength = $this->useZip64 ? 0xFFFFFFFF : 0x00;
 
         $header = pack('V', 0x04034b50) // Local file header signature.
             . pack('v', 0x000A) // Version needed to extract.
@@ -140,14 +164,23 @@ class Zipper
             . pack('v', 0x00) // Compression method (0x00 means no compression).
             . pack('V', $timestamp) // Last modified time and date (as a DOS timestamp).
             . pack('V', 0x00) // CRC-32 hash of file contents. 0x00 because it is specified in the file descriptor.
-            . pack('V', 0x00) // Compressed data length. 0x00 because it is specified in the file descriptor.
-            . pack('V', 0x00) // Uncompressed data length. 0x00 because it is specified in the file descriptor.
+            . pack('V', $dataLength) // Compressed data length. 0x00 because it is specified in the file descriptor.
+            . pack('V', $dataLength) // Uncompressed data length. 0x00 because it is specified in the file descriptor.
             . pack('v', $nameLength) // Length of the file name.
-            . pack('v', 0); // Length of the extra data field.
+            . pack('v', $extraFieldLength) // Length of the extra data field.
+            . $filename;
 
-        echo $header . $filename;
+        if ($this->useZip64) {
+            // Add ZIP64 extended information.
+            $header .= pack('v', 0x0001)
+                . pack('v', 16)
+                . $this->pack64($fileSize)
+                . $this->pack64($fileSize);
+        }
 
-        return strlen($header) + $nameLength; // Return the byte size of the header.
+        echo $header;
+
+        return strlen($header); // Return the byte size of the header.
     }
 
     private function sendFileData($path, $hashContext)
@@ -174,10 +207,13 @@ class Zipper
     private function sendFileDescriptor($fileSize, $crc32)
     {
         // Send the file descriptor that goes at the end of each file entry.
+
+        $packedFileSize = $this->useZip64 ? $this->pack64($fileSize) : pack('V', $fileSize);
+
         $descriptor = pack('V', 0x08074b50)
             . pack('V', $crc32)
-            . pack('V', $fileSize)
-            . pack('V', $fileSize);
+            . $packedFileSize
+            . $packedFileSize;
 
         echo $descriptor;
 
@@ -194,11 +230,15 @@ class Zipper
         }
 
         // Send the final end-of-file central directory record and return # bytes sent.
-        return $this->sendFinalCDR($cdrOffset, $offset) + $cdrOffset;
+        $bytesSent = $this->sendZip64CDR($cdrOffset, $offset);
+        return $bytesSent + $this->sendFinalCDR($cdrOffset, $offset) + $cdrOffset;
     }
 
     private function sendFileCDR($file)
     {
+        $fileSize = $this->useZip64 ? 0xFFFFFFFF : $file["size"];
+        $extraFieldLength = $this->useZip64 ? 20 : 0;
+
         // Send the central directory record belonging to a file.
         $record = pack('V', 0x02014b50) // Central file header signature.
             . pack('v', 0) // Made by version.
@@ -207,10 +247,10 @@ class Zipper
             . pack('v', 0x00) // Compression method (0x00 means no compression).
             . pack('V', $file["timestamp"]) // Last modified time and date (as a DOS timestamp).
             . pack('V', $file["crc"]) // CRC-32 hash of file contents.
-            . pack('V', $file["size"]) // Compressed data length. Equal to file size because of no compression.
-            . pack('V', $file["size"]) // Uncompressed data length.
+            . pack('V', $fileSize) // Compressed data length. Equal to file size because of no compression.
+            . pack('V', $fileSize) // Uncompressed data length.
             . pack('v', strlen($file["name"])) // Length of the file name.
-            . pack('v', 0) // Length of the extra data field.
+            . pack('v', $extraFieldLength) // Length of the extra data field.
             . pack('v', 0) // Length of the commend field.
             . pack('v', 0) // Disk number start.
             . pack('v', 0) // Internal file attributes.
@@ -218,7 +258,41 @@ class Zipper
             . pack('V', $file["offset"]) // Relative offset of local header.
             . $file["name"];
 
+
+        if ($this->useZip64) {
+            $record .= pack('v', 0x0001) // Zip64 extra block signature.
+                . pack('v', 16) // Size of the following extra block.
+                . $this->pack64($file["size"]) // Uncompressed file size.
+                . $this->pack64($file["size"]); // Compressed file size.
+        }
+
         echo $record;
+        return strlen($record);
+    }
+
+    private function sendZip64CDR($cdrOffset, $offset) {
+        $numEntries = count($this->centralDirectory);
+        $cdrLength = $cdrOffset;
+        $cdrOffset = $offset;
+
+        $record = pack('V', 0x06064b50) // ZIP64 end of central directory signature.
+            . $this->pack64(44) // Size of the rest of this record.
+            . pack('v', 0) // Made by version.
+            . pack('v', 0x000A) // Version needed to extract.
+            . pack('V', 0x0000) // Number of this disk.
+            . pack('V', 0x0000) // Number of the disk with the start of the central directory.
+            . $this->pack64($numEntries) // Number of CDR entries on this disk.
+            . $this->pack64($numEntries) // Total number of CDR entries.
+            . $this->pack64($cdrLength) // Size of central directory.
+            . $this->pack64($cdrOffset); // Start of central directory.
+
+        $record .= pack('V', 0x07064b50) // ZIP64 end of central directory locator signature.
+            . pack('V', 0x0000) // Number of the disk with the start of the Zip64 CDR.
+            . $this->pack64($cdrLength + $cdrOffset) // Start of zip64 cdr.
+            . pack('V', 0x0001); // Number of disks.
+
+        echo $record;
+
         return strlen($record);
     }
 
@@ -227,6 +301,8 @@ class Zipper
         $num = count($this->centralDirectory);
         $cdrLength = $cdrOffset;
         $cdrOffset = $offset;
+
+        if ($this->useZip64) $cdrOffset = 0xFFFFFFFF;
 
         // Send the final central directory section which goes at the end of the ZIP archive.
         $record = pack('V', 0x06054b50) // End of central directory record signature.
