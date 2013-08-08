e177343d6e09d8ae7263e1c4f9a633bfb7715900
Jack Kittridge <jackKittridge92@gmail.com>
2013-08-08 13:59:34 +0000

    Re: #967 Some refactoring / tidying to common.js
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/playground/filesender-1.5-multiupload-3@2509 8555b757-d854-4b86-925a-82fd84c90ff4

--- www/js/common.js
+++ www/js/common.js
@@ -38,16 +38,20 @@
 // display bytes in readable format
 // Used in upload
 function readablizebytes(bytes)
- {
-	if (bytes > 1024*1024*1024)
-		bytesdisplay = (Math.round(bytes * 100/(1024*1024*1024))/100).toString() + " GB";
-	else if (bytes > 1024*1024)
-		bytesdisplay = (Math.round(bytes * 100/(1024*1024))/100).toString() + " MB";
-	else if (bytes > 1024)
-		bytesdisplay = (Math.round(bytes * 100/1024)/100).toString() + " kB";
-	else
-		bytesdisplay = (Math.round(bytes * 100)/100).toString() + " Bytes";
-	return bytesdisplay;
+{
+    var bytesDisplay;
+
+	if (bytes > 1024*1024*1024) {
+		bytesDisplay = (Math.round(bytes * 100/(1024*1024*1024))/100).toString() + ' GB';
+    } else if (bytes > 1024*1024) {
+		bytesDisplay = (Math.round(bytes * 100/(1024*1024))/100).toString() + ' MB';
+    } else if (bytes > 1024) {
+		bytesDisplay = (Math.round(bytes * 100/1024)/100).toString() + ' kB';
+    } else {
+		bytesDisplay = (Math.round(bytes * 100)/100).toString() + ' Bytes';
+    }
+
+	return bytesDisplay;
 }	
 	
 // -------------------
@@ -61,9 +65,11 @@ function validate_fileto()
 
 	$("#fileto_msg").hide();
 	$("#maxemails_msg").hide();
+
 	// remove white spaces 
 	var email = $("#fileto").val();
 	email = email.split(" ").join("");
+
     if(email != "") {
         // if not empty - check and remove trailing , added by autocomplete
         email = email.replace(/,$/, "");
@@ -74,11 +80,10 @@ function validate_fileto()
         if(email.length>maxEmailRecipients) {
             $("#maxemails_msg").show();
             isValid = false;
-
         }
 
         for (var i = 0; i < email.length; i++) {
-            if (!echeck(email[i], 1, 0)) {
+            if (!echeck(email[i])) {
                 $("#fileto_msg").show();
                 isValid = false;
             }
@@ -100,31 +105,17 @@ function validate_fileto()
 // Validate EXPIRY - upload, voucher and files
 function validate_expiry()
 {
-	//var validformat=/^\d{2}\-\d{2}\-\d{4}$/ //Basic check for format validity
-	var today = new Date();
-    var maxDate = new Date(today.getFullYear(), today.getMonth(), today.getDate()+parseInt(maximumDate));
-	var returnval=false
-	
-	//var monthfield=$("#datepicker").val().split("-")[1]
-	//var dayfield=$("#datepicker").val().split("-")[0]
-	//var yearfield=$("#datepicker").val().split("-")[2]
-	//var dayobj = new Date(yearfield, monthfield-1, dayfield)
 	var selectedDate = $("#datepicker").val();
 	try {
-           $.datepicker.parseDate(datepickerDateFormat,selectedDate);
-        } catch (e) {
-            $("#expiry_msg").show();
+        $.datepicker.parseDate(datepickerDateFormat,selectedDate);
+    } catch (e) {
+        $("#expiry_msg").show();
 		return false;
-        };
+    }
 
-	//if(dayobj < today || dayobj > maxDate)
-	//{
-	//	$("#expiry_msg").show();
-	//	return false;	
-	//}
 	if($("#datepicker").datepicker("getDate") == null)
 	{
-		$("#expiry_msg").show();
+        $("#expiry_msg").show();
 		return false;
 	}
 	$("#expiry_msg").hide();
@@ -132,88 +123,67 @@ function validate_expiry()
 }
 
 //  validate single email	- upload, voucher and files
-function echeck(str) {
-
-	var at="@"
-	var dot="."
-	var lat=str.indexOf(at)
-	var lstr=str.length
-	var ldot=str.indexOf(dot)
+function echeck(str)
+{
+	var at='@';
+	var dot='.';
+	var lat=str.indexOf(at);
+	var lstr=str.length;
+	var ldot=str.indexOf(dot);
 		
-	if (str.indexOf(at)==-1){	return false; }
-	if (str.indexOf(at)==-1 || str.indexOf(at)==0 || str.indexOf(at)==lstr){  return false;	}
-	if (str.indexOf(dot)==-1 || str.indexOf(dot)==0 || str.indexOf(dot)==lstr){ return false; }
-	if (str.indexOf(at,(lat+1))!=-1){	return false;}
-	if (str.substring(lat-1,lat)==dot || str.substring(lat+1,lat+2)==dot){ return false; }
-	if (str.indexOf(dot,(lat+2))==-1){ 	return false; }
-	if (str.indexOf(" ")!=-1){			return false; }
-	return true					
+	if (str.indexOf(at) == -1 || str.indexOf(at) == 0 || str.indexOf(at) == lstr){
+        return false;
+    } else if (str.indexOf(dot) == -1 || str.indexOf(dot) == 0 || str.indexOf(dot) == lstr) {
+        return false;
+    } else if (str.indexOf(at,(lat+1))!=-1){
+        return false;
+    } else if (str.substring(lat-1,lat)==dot || str.substring(lat+1,lat+2)==dot){
+        return false;
+    } else if (str.indexOf(dot,(lat+2))==-1){
+        return false;
+    } else if (str.indexOf(" ") != -1){
+        return false;
+    } else return true;
 }
 
 	// display msg - upload, voucher and files
 function fileMsg(msg)
 {
-	$("#file_msg").html(msg);
-	$("#file_msg").show();
-}
-
-// validate date format
-// use: if (!isValidDate(myDateString, "DMY")) { alert("The date is not in the correct format."); }
-function isValidDate(dateStr, format) {
-   if (format == null) { format = "MDY"; }
-   format = format.toUpperCase();
-   if (format.length != 3) { format = "MDY"; }
-   if ( (format.indexOf("M") == -1) || (format.indexOf("D") == -1) || (format.indexOf("Y") == -1) ) { format = "MDY"; }
-   if (format.substring(0, 1) == "Y") { // If the year is first
-      var reg1 = /^\d{2}(\-|\/|\.)\d{1,2}\1\d{1,2}$/
-      var reg2 = /^\d{4}(\-|\/|\.)\d{1,2}\1\d{1,2}$/
-   } else if (format.substring(1, 2) == "Y") { // If the year is second
-      var reg1 = /^\d{1,2}(\-|\/|\.)\d{2}\1\d{1,2}$/
-      var reg2 = /^\d{1,2}(\-|\/|\.)\d{4}\1\d{1,2}$/
-   } else { // The year must be third
-      var reg1 = /^\d{1,2}(\-|\/|\.)\d{1,2}\1\d{2}$/
-      var reg2 = /^\d{1,2}(\-|\/|\.)\d{1,2}\1\d{4}$/
-   }
-   // If it doesn't conform to the right format (with either a 2 digit year or 4 digit year), fail
-   if ( (reg1.test(dateStr) == false) && (reg2.test(dateStr) == false) ) { return false; }
-   var parts = dateStr.split(RegExp.$1); // Split into 3 parts based on what the divider was
-   // Check to see if the 3 parts end up making a valid date
-   if (format.substring(0, 1) == "M") { var mm = parts[0]; } else 
-      if (format.substring(1, 2) == "M") { var mm = parts[1]; } else { var mm = parts[2]; }
-   if (format.substring(0, 1) == "D") { var dd = parts[0]; } else 
-      if (format.substring(1, 2) == "D") { var dd = parts[1]; } else { var dd = parts[2]; }
-   if (format.substring(0, 1) == "Y") { var yy = parts[0]; } else 
-      if (format.substring(1, 2) == "Y") { var yy = parts[1]; } else { var yy = parts[2]; }
-   if (parseFloat(yy) <= 50) { yy = (parseFloat(yy) + 2000).toString(); }
-   if (parseFloat(yy) <= 99) { yy = (parseFloat(yy) + 1900).toString(); }
-   var dt = new Date(parseFloat(yy), parseFloat(mm)-1, parseFloat(dd), 0, 0, 0, 0);
-   if (parseFloat(dd) != dt.getDate()) { return false; }
-   if (parseFloat(mm)-1 != dt.getMonth()) { return false; }
-   return true;
+    $("#file_msg").html(msg);
+    $("#file_msg").show();
 }
 
 // validate ajax return code to make sure it is parsable JSON
 function parseJSON(json)
 {
-	try {
-          var data = JSON.parse(json);
-		  return data;
-        } catch (e) {
-			var msg = "Error accessing server. ";
-			if(debug) { msg += "Fatal error (" + e + ")";};
-			$("#scratch").html('<div id="message">'+msg+'</>');	
-     	};
+    try {
+        var data = JSON.parse(json);
+        return data;
+    } catch (e) {
+        var msg = "Error accessing server. ";
+
+        if(debug) {
+            msg += "Fatal error (" + e + ")";
+        }
+
+        $("#scratch").html('<div id="message">'+msg+'</>');
+    }
 }
 
 // display erro state details if debug is on
 function ajaxerror(readyState,status,responseText)
 {
 	var msg = "Error accessing server. ";
-	if(debug) { msg += " Ready State:"+readyState + ", status:" + status + ",responseText" + responseText + "";};
+
+	if(debug) {
+        msg += " Ready State:"+readyState + ", status:" + status + ",responseText" + responseText + "";
+    }
+
 	$("#scratch").html('<div id="message">'+msg+'</>');						   
 }
 
-function hideMessages() {
+function hideMessages()
+{
     $("#fileto_msg").hide();
     $("#expiry_msg").hide();
     $("#maxemails_msg").hide();
@@ -221,7 +191,8 @@ function hideMessages() {
     $("#aup_msg").hide();
 }
 
-function validate_aup() {
+function validate_aup()
+{
     if ($("#aup").is(":checked")) {
         $('#aup').removeClass('errorglow');
         return true;
@@ -231,30 +202,27 @@ function validate_aup() {
     }
 }
 
-function statusMessage(msg, color) {
-    if(msg == null) { $('#statusmessage').html(''); }
-        $('#statusmessage').html(msg);
-        $('#statusmessage').attr('class', color);
+function statusMessage(msg, color)
+{
+    var statusMsg = $('#statusmessage');
+    if(msg == null) {
+        statusMsg.html('');
+    }
+
+    statusMsg.html(msg);
+    statusMsg.attr('class', color);
 }
 
 
 var totalBytesLoaded = 0;
 
-function updateProgressBar(bytesloaded, totalbytes, amountUploaded) {
-
+function updateProgressBar(bytesloaded, totalbytes, amountUploaded)
+{
     var percentComplete = Math.round(bytesloaded * 100 / totalbytes);
     var bytesTransfered = '';
     var bytesRemaining;
     var uploadSpeed;
     var timeRemaining;
-
-    if (bytesloaded > 1024 * 1024) {
-        bytesTransfered = (Math.round(bytesloaded * 100 / (1024 * 1024)) / 100).toString() + 'MB';
-    } else if (bytesloaded > 1024) {
-        bytesTransfered = (Math.round(bytesloaded * 100 / 1024) / 100).toString() + 'kB';
-    } else {
-        bytesTransfered = (Math.round(bytesloaded * 100) / 100).toString() + 'Bytes';
-    }
 
     // use time elapsed from start to calculate averages
     var now = new Date().getTime();
@@ -285,7 +253,6 @@ function updateProgressBar(bytesloaded, totalbytes, amountUploaded) {
         $('#totalUploaded').html('Total uploaded: ' + readablizebytes(bytesloaded) + '/' + readablizebytes(totalbytes));
         $('#averageUploadSpeed').html('Average upload Speed: ' + uploadSpeed.toFixed(2) * 8 + 'MBit/s');
         $('#timeRemaining').html('Approx time remaining: ' + secondsToString(timeRemaining));
-
     }
 
 }
