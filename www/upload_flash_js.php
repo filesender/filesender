a5a2ca0b6794db9417fd68e6d0ffac037d9d6606
Jack Kittridge <jackKittridge92@gmail.com>
2013-08-09 09:46:42 +0000

    Re: #948 Removed the code that appears in upload_flash_js from upload.php, removed code from upload.php that appears in upload_common_js. upload.php now only contains a doc ready and html code.
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/playground/filesender-1.5-multiupload-3@2513 8555b757-d854-4b86-925a-82fd84c90ff4

--- www/upload_flash_js.php
+++ www/upload_flash_js.php
@@ -1,121 +1,105 @@
 <?php
 ?>
 <script type="text/javascript">
-    function validateforflash(fname, fsize)
+    function validateforflash(fname,fsize)
     {
-    // remove previous validation messages
-        hidemessages();
+        // remove previous validation messages
+        hideMessages();
 
         var validate = true;
 
-        if (!validate_fileto()) {
-            validate = false;
-        }	// validate emails
-        if (aup == '1') // check if AUP is required
+        if(!validate_fileto() ){validate = false;}	// validate emails
+        if(aup == '1') // check if AUP is required
         {
-            if (!validate_aup()) {
-                validate = false;
-            }		// check AUP is selected
+            if(!validate_aup() ){validate = false;}		// check AUP is selected
         }
-        if (!validate_expiry()) {
-            validate = false;
-        }	// check date
+        if(!validate_expiry() ){validate = false;}	// check date
         // validate with server
-        if (validate) {
+        if(validate) {
             $("#uploadbutton").find("a").attr("onclick", ""); // prevent double clicks to start extra uploads
             var query = $("#form1").serializeArray(), json = {};
-            for ( var i in query) {
-                json[query[i].name] = query[i].value;
-            }
+            for (i in query) { json[query[i].name] = query[i].value; }
             // add file information fields
             json["fileoriginalname"] = fname;
             json["filesize"] = parseInt(fsize);
             json["vid"] = vid;
+            json["filetrackingcode"] = trackingCode;
+            json["filegroupid"] = groupid;
 
             $.ajax({
                 type: "POST",
-                url: "fs_upload.php?type=validateupload&vid=" + vid,
-                data: {myJson: JSON.stringify(json)}, success: function (data) {
-                    if (data == "") {
+                url: "fs_upload.php?type=validateupload&vid="+vid,
+                data: {myJson:  JSON.stringify(json)}
+                ,success:function( data ) {
+                    if(data == "") {
                         alert("No response from server");
                         return;
                     }
-                    if (data == "ErrorAuth") {
+                    if(data == "ErrorAuth")
+                    {
                         $("#dialog-autherror").dialog("open");
                         return;
                     }
-                    var jsonData = parseJSON(data);
-                    if (jsonData.errors) {
-                        $.each(jsonData.errors, function (i, result) {
-                            if (result == "err_token") {
-                                $("#dialog-tokenerror").dialog("open");
-                            } // token missing or error
-                            if (result == "err_tomissing") {
-                                $("#fileto_msg").show();
-                            } // missing email data
-                            if (result == "err_expmissing") {
-                                $("#expiry_msg").show();
-                            } // missing expiry date
-                            if (result == "err_exoutofrange") {
-                                $("#expiry_msg").show();
-                            } // expiry date out of range
-                            if (result == "err_invalidemail") {
-                                $("#fileto_msg").show();
-                            } // 1 or more emails invalid
-                            if (result == "err_invalidfilename") {
-                                $("#file_msg").show();
-                            } //  invalid filename
-                            if (result == "err_invalidextension") {
-                                $("#extension_msg").show();
-                            } //  invalid extension
-                            if (result == "err_nodiskspace") {
-                                errorDialog(errmsg_disk_space);
-                            }
+                    var data =  parseJSON(data);
+                    if(data.errors)
+                    {
+                        $.each(data.errors, function(i,result){
+                            if(result == "err_token") {$("#dialog-tokenerror").dialog("open");} // token missing or error
+                            if(result == "err_tomissing") { $("#fileto_msg").show();} // missing email data
+                            if(result == "err_expmissing") { $("#expiry_msg").show();} // missing expiry date
+                            if(result == "err_exoutofrange") { $("#expiry_msg").show();} // expiry date out of range
+                            if(result == "err_invalidemail") { $("#fileto_msg").show();} // 1 or more emails invalid
+                            if(result == "err_invalidfilename") { $("#file_msg").show();} //  invalid filename
+                            if(result == "err_invalidextension") { $("#extension_msg").show();} //  invalid extension
+                            if(result == "err_nodiskspace") { errorDialog(errmsg_disk_space);}
                         });
                         $("#uploadbutton").find("a").attr("onclick", "validate()"); // re-activate upload button
                     }
-                    if (jsonData.status && jsonData.status == "complete") {
+                    if(data.status && data.status == "complete")
+                    {
                         $("#fileToUpload").hide();// hide Browse
                         $("#selectfile").hide();// hide Browse message
                         $("#uploadbutton").hide(); // hide upload
                         $("#cancelbutton").show(); // show cancel
-
                         // show upload progress dialog
+
                         startTime = new Date().getTime();
-
                         // no error so use reuslt as current bytes uploaded for file resume
-                        vid = jsonData.vid;
-
+                        vid = data.vid;
                         // hide upload button
                         openProgressBar(fname);
                         getFlexApp("filesenderup").returnVoucher(vid)
                     } else {
-                        getFlexApp("filesenderup").returnMsg(false)
+                        getFlexApp("filesenderup").returnM^sg(false)
                     }
-                }, error: function (xhr, err) {
-                    alert("readyState: " + xhr.readyState + "\nstatus: " + xhr.status);
-                    alert("responseText: " + xhr.responseText);
+                },error:function(xhr,err){
+                    alert("readyState: "+xhr.readyState+"\nstatus: "+xhr.status);
+                    alert("responseText: "+xhr.responseText);
                 }
             })
         }
     }
 
+    // flex file information check
     function fileInfo(name,size)
     {
         $("#uploadbutton").hide();
         fileMsg("");
-        if(size < 1) {
+        if(size < 1)
+        {
             getFlexApp("filesenderup").returnMsg("hideupload");
             $("#fileInfoView").hide();
             fileMsg("<?php echo lang("_INVALID_FILESIZE_ZERO") ?>");
             return false;
         }
-        if(size > maxFLASHuploadsize) {
+        if(size > maxFLASHuploadsize)
+        {
             fileMsg("<?php echo lang("_INVALID_TOO_LARGE_1") ?> " + readablizebytes(maxFLASHuploadsize) + ". <?php echo lang("_INVALID_SIZE_USEHTML5") ?> ");
             $("#fileInfoView").hide();
             return false;
         }
-        if (validatefilename(name)) {
+        if (validateFileName(name))
+        {
             $("#fileInfoView").show();
             $("#n").val(name);
             $("#total").val(size);
@@ -129,6 +113,7 @@
         }
     }
 
+
     function uploadcomplete(name,size)
     {
         $("#fileName").val(name);
@@ -141,7 +126,8 @@
 
                 var data =  parseJSON(data);
 
-                if(data.errors) {
+                if(data.errors)
+                {
                     $.each(data.errors, function(i,result){
                         if(result == "err_token") { $("#dialog-tokenerror").dialog("open");} // token missing or error
                         if(result == "err_cannotrenamefile") { window.location.href="index.php?s=uploaderror";} //
@@ -159,17 +145,22 @@
         });
     }
 
+    // check browser type
     function getFlexApp(appName)
     {
-        if (navigator.appName.indexOf ("Microsoft") !=-1) {
-            if(window[appName] == undefined) {
+        if (navigator.appName.indexOf ("Microsoft") !=-1)
+        {
+            if(window[appName] == undefined)
+            {
                 return document[appName];
             } else {
                 return window[appName];
             }
         }
-        else {
+        else
+        {
             return document[appName];
         }
     }
+
 </script>
