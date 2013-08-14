728e260a95f18bdeececf3812da534edd1d24ced
Vegard Polden <vpolden@gmail.com>
2013-08-14 11:17:10 +0000

    Test #926: Added statistics for the upload to result.php.
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/playground/filesender-1.5-multiupload-3@2523 8555b757-d854-4b86-925a-82fd84c90ff4

--- pages/result.php
+++ pages/result.php
@@ -6,15 +6,30 @@ if ($s == "complete" || $s == "completev") {
         $groupId = $_REQUEST['gid'];
         $files = $functions->getMultiFileData($groupId);
 
-        echo lang("_TRACKING_CODE") . ': <a href="?gid=' . $groupId . '">' . $files[0]['filetrackingcode'] . '</a><br /><br />';
-        echo lang("_UPLOADED_LIST");
-        echo '<ul>';
+        $list = "";
+        $totalSize = 0;
 
         foreach ($files as $file) {
-            echo '<li>' . $file['fileoriginalname'] . ' (' . formatBytes($file['filesize']) . ')</li>';
+            $totalSize += $file['filesize'];
+            $list .= '<li>' . $file['fileoriginalname'] . ' (' . formatBytes($file['filesize']) . ')</li>';
         }
 
-        echo '</ul>';
+        echo lang("_TRACKING_CODE") . ': ' . $files[0]['filetrackingcode'] . '<br /><br />';
+
+        if (isset($_REQUEST['start']) && isset($_REQUEST['end'])) {
+            $start = intval($_REQUEST['start']);
+            $end = intval($_REQUEST['end']);
+
+            echo 'Upload start time: ' . date('H:i:s', $start) . '<br />';
+            echo 'Upload end time: ' . date('H:i:s', $end) . '<br />';
+
+            $uploadSpeed = $totalSize / ($end - $start); // Bytes per second.
+
+            echo 'Average upload speed: ' . formatBytes($uploadSpeed) . '/s. <br /><br />';
+        }
+
+        echo lang("_UPLOADED_LIST");
+        echo '<ul>' . $list . '</ul>';
 
         if ($s != "completev") {
             echo lang("_REFER_TO_MY_FILES");
