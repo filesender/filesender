04ef346e0426d7813ba63c5e9e8dfc4e3aec95dd
Chris Richter <chris@ricoshae.com.au>
2013-09-18 03:20:00 +0000

    Unit Tests for Multi 3 in development
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/playground/filesender-1.5-multiupload-3@2584 8555b757-d854-4b86-925a-82fd84c90ff4

--- unittest/configUT.php
+++ unittest/configUT.php
@@ -7,21 +7,20 @@ class testFileSender_configUT extends UnitTestCase {
 	 
 	function test_configUT()  
     {
-		$CFG = new Config();
-	 $config = $CFG->loadConfig();
+	
 	// check all required options exist in config file
-	$this->assertTrue(isset($config["customCSS"]));
-	$this->assertTrue(isset($config["admin"]));
-	$this->assertTrue(isset($config["adminEmail"]));
-	$this->assertTrue(isset($config["Default_TimeZone"]));
-	$this->assertTrue(isset($config["site_name"]));
-    $this->assertTrue(isset($config["customCSS"]));
+	$this->assertTrue(set("customCSS"));
+	$this->assertTrue(set("admin"));
+	$this->assertTrue(set("adminEmail"));
+	$this->assertTrue(set("Default_TimeZone"));
+	$this->assertTrue(set("site_name"));
+    $this->assertTrue(set("customCSS"));
 
-	$this->assertTrue(isset($config["terasender"])); // true/false - terasender (really fast uploads)uses html5 web workers to speed up file upload - effectively provding multi thread faster uploads
-	$this->assertTrue(isset($config["terasenderadvanced"])); // true/false - terasender advanced - show advanced settings
-	$this->assertTrue(isset($config["terasender_chunksize"]));
-	$this->assertTrue(isset($config["terasender_workerCount"]));
-	$this->assertTrue(isset($config["terasender_jobsPerWorker"]));
+	$this->assertTrue(set("terasender")); // true/false - terasender (really fast uploads)uses html5 web workers to speed up file upload - effectively provding multi thread faster uploads
+	$this->assertTrue(set("terasenderadvanced")); // true/false - terasender advanced - show advanced settings
+	$this->assertTrue(set("terasender_chunksize"));
+	$this->assertTrue(set("terasender_workerCount"));
+	$this->assertTrue(set("terasender_jobsPerWorker"));
 
 
  // Email flow settings
@@ -30,135 +29,141 @@ class testFileSender_configUT extends UnitTestCase {
     // settings marked as 'hidden' will be contained in 'More options' in the right hand column of the upload page
     // settings marked as 'off' are completely disabled and are not displayed anywhere
     // true/false on default fields specify whether or not boxes are checked on page load
-   $this->assertTrue( isset($config["email_me_copies_display"]));
-   $this->assertTrue( isset($config["email_me_copies_default"]));
+   $this->assertTrue( set("email_me_copies_display"));
+   $this->assertTrue( set("email_me_copies_default"));
 
-   $this->assertTrue( isset($config["upload_complete_email_display"]));
-   $this->assertTrue( isset($config["upload_complete_email_default"]));
+   $this->assertTrue( set("upload_complete_email_display"));
+   $this->assertTrue( set("upload_complete_email_default"));
 
-    $this->assertTrue(isset($config["inform_download_email_display"]));
-   $this->assertTrue( isset($config["inform_download_email_default"]));
+    $this->assertTrue(set("inform_download_email_display"));
+   $this->assertTrue( set("inform_download_email_default"));
 
-    $this->assertTrue(isset($config["email_me_daily_statistics_display"]));
-    $this->assertTrue(isset($config["email_me_daily_statistics_default"]));
+    $this->assertTrue(set("email_me_daily_statistics_display"));
+    $this->assertTrue(set("email_me_daily_statistics_default"));
 
-   $this->assertTrue( isset($config["download_confirmation_enabled_display"]));
-   $this->assertTrue( isset($config["download_confirmation_enabled_default"]));
+   $this->assertTrue( set("download_confirmation_enabled_display"));
+   $this->assertTrue( set("download_confirmation_enabled_default"));
 
-   $this->assertTrue( isset($config["email_only_me_display"]));
-   $this->assertTrue( isset($config["email_only_me_default"]));
+   $this->assertTrue( set("email_only_me_display"));
+   $this->assertTrue( set("email_only_me_default"));
 
-	$this->assertTrue(isset($config["openSSLKeyLength"]));
-	$this->assertTrue(isset($config["emailRegEx"]));
-   $this->assertTrue( isset($config["webWorkersLimit"]));
+	$this->assertTrue(set("openSSLKeyLength"));
+	$this->assertTrue(set("emailRegEx"));
+   $this->assertTrue( set("webWorkersLimit"));
 
 	// UI Settings
-	$this->assertTrue(isset($config["datedisplayformat"]));
-	$this->assertTrue(isset($config["versionNumber"])); // Show version number (true/false)
-	$this->assertTrue(isset($config["site_showStats"])); // Show site upload/download stats (true/false)
-	$this->assertTrue(isset($config["displayUserName"])); // Show 'Welcome user' (true/false)
+	$this->assertTrue(set("datedisplayformat"));
+	$this->assertTrue(set("versionNumber")); // Show version number (true/false)
+	$this->assertTrue(set("site_showStats")); // Show site upload/download stats (true/false)
+	$this->assertTrue(set("displayUserName")); // Show 'Welcome user' (true/false)
 	
 	// multi file option
-	$this->assertTrue(isset($config["multiupload"]));
+	$this->assertTrue(set("multiupload"));
     
     // auto complete - provides auto complete in input field for emails
-    $this->assertTrue(isset($config["autocomplete"]));
-   $this->assertTrue( isset($config["autocompleteHistoryMax"]));
+    $this->assertTrue(set("autocomplete"));
+   $this->assertTrue( set("autocompleteHistoryMax"));
 
  // Mac OS X alternative unzip application (for multiple file downloads where the default Archive Utility will not work).
-   $this->assertTrue( isset($config["mac_unzip_name"]));
-    $this->assertTrue(isset($config["mac_unzip_link"]));
+   $this->assertTrue( set("mac_unzip_name"));
+    $this->assertTrue(set("mac_unzip_link"));
 	
 	// advanced options
 	// voucherreturnlock - vouchers can only be used to upload files back to the voucher creator
-	$this->assertTrue(isset($config["voucherreturnlock"]));
-	$this->assertTrue(isset($config["displaydownloadsummary"]));
+	$this->assertTrue(set("voucherreturnlock"));
+	$this->assertTrue(set("displaydownloadsummary"));
 	
 	// options - stored as array in fileoptions 
 	// Voucher Locked To Sender
-	$this->assertTrue(isset($config["vlts"]));
+	$this->assertTrue(set("vlts"));
 
 	// debug settings
-	$this->assertTrue(isset($config["debug"]));
-	$this->assertTrue(isset($config["dnslookup"]));
-	$this->assertTrue(isset($config["client_specific_logging"]));
-	$this->assertTrue(isset($config["client_specific_logging_uids"]));
+	$this->assertTrue(set("debug"));
+	$this->assertTrue(set("dnslookup"));
+	$this->assertTrue(set("client_specific_logging"));
+	$this->assertTrue(set("client_specific_logging_uids"));
 
 	// saml settings
-	$this->assertTrue(isset($config["saml_email_attribute"]));
-	$this->assertTrue(isset($config["saml_name_attribute"]));
-	$this->assertTrue(isset($config["saml_uid_attribute"]));
+	$this->assertTrue(set("saml_email_attribute"));
+	$this->assertTrue(set("saml_name_attribute"));
+	$this->assertTrue(set("saml_uid_attribute"));
 
 	// AuP settings
-	$this->assertTrue(isset($config["AuP_default"]));
-	$this->assertTrue(isset($config["AuP"]));
-	//isset($config["AuP_label"] = "I accept the terms and conditions of this service"; // moved AUP to language files
-	//isset($config["AuP_terms"] = "AuP Terms and conditions"; // moved AUP to language files
+	$this->assertTrue(set("AuP_default"));
+	$this->assertTrue(set("AuP"));
+	//set("AuP_label"] = "I accept the terms and conditions of this service"; // moved AUP to language files
+	//set("AuP_terms"] = "AuP Terms and conditions"; // moved AUP to language files
 
 	// Server settings
-	$this->assertTrue(isset($config["default_daysvalid"]));
-	$this->assertTrue(isset($config["ban_extension"]));
-	$this->assertTrue(isset($config["max_email_recipients"]));
+	$this->assertTrue(set("default_daysvalid"));
+	$this->assertTrue(set("ban_extension"));
+	$this->assertTrue(set("max_email_recipients"));
 
-	$this->assertTrue(isset($config["max_flash_upload_size"]));
-	$this->assertTrue(isset($config["max_gears_upload_size"]));
-	$this->assertTrue(isset($config["max_html5_upload_size"]));
-	$this->assertTrue(isset($config["upload_chunk_size"]));
-	$this->assertTrue(isset($config["download_chunk_size"]));
-    $this->assertTrue(isset($config["html5_max_uploads"]));
+	$this->assertTrue(set("max_flash_upload_size"));
+	$this->assertTrue(set("max_gears_upload_size"));
+	$this->assertTrue(set("max_html5_upload_size"));
+	$this->assertTrue(set("upload_chunk_size"));
+	$this->assertTrue(set("download_chunk_size"));
+    $this->assertTrue(set("html5_max_uploads"));
 	
 	// update max_flash_upload_size if php.ini post_max_size and upload_max_filesize is set lower
-	$this->assertTrue(isset($config["max_flash_upload_size"]));
+	$this->assertTrue(set("max_flash_upload_size"));
 	
-	$this->assertTrue(isset($config["server_drivespace_warning"]));
+	$this->assertTrue(set("server_drivespace_warning"));
 	
 	// Advanced server settings, do not change unless you have a very good reason.
-	$this->assertTrue(isset($config["postgresdateformat"]));
-	$this->assertTrue(isset($config["db_dateformat"]));
-	$this->assertTrue(isset($config["crlf"]));
-	$this->assertTrue(isset($config["voucherRegEx"]));
-	$this->assertTrue(isset($config["voucherUIDLength"]));
-	$this->assertTrue(isset($config["emailRegEx"]));
+	$this->assertTrue(set("postgresdateformat"));
+	$this->assertTrue(set("db_dateformat"));
+	$this->assertTrue(set("crlf"));
+	$this->assertTrue(set("voucherRegEx"));
+	$this->assertTrue(set("voucherUIDLength"));
+	$this->assertTrue(set("emailRegEx"));
 
 	// site URL settings
-	$this->assertTrue(isset($config["site_url"]));
-	$this->assertTrue(isset($config["site_simplesamlurl"]));
-	$this->assertTrue(isset($config["site_authenticationSource"]));
-	$this->assertTrue(isset($config["site_logouturl"])); 
-	$this->assertTrue(isset($config["site_downloadurl"]));
+	$this->assertTrue(set("site_url"));
+	$this->assertTrue(set("site_simplesamlurl"));
+	$this->assertTrue(set("site_authenticationSource"));
+	$this->assertTrue(set("site_logouturl")); 
+	$this->assertTrue(set("site_downloadurl"));
 	
-	$this->assertTrue(isset($config["forceSSL"]));
-	$this->assertTrue(isset($config["displayerrors"]));
+	$this->assertTrue(set("forceSSL"));
+	$this->assertTrue(set("displayerrors"));
 	// Support links
-	$this->assertTrue(isset($config["aboutURL"]));
-	$this->assertTrue(isset($config["helpURL"]));
-	$this->assertTrue(isset($config["HTML5URL"]));
+	$this->assertTrue(set("aboutURL"));
+	$this->assertTrue(set("helpURL"));
+	$this->assertTrue(set("HTML5URL"));
 	//config['gearsURL'] = 'http://tools.google.com/gears/';
 
 	// (absolute) file locations
-	$this->assertTrue(isset($config["site_filestore"]));
-	$this->assertTrue(isset($config["site_temp_filestore"])); 
-	$this->assertTrue(isset($config["site_simplesamllocation"]));
-	$this->assertTrue(isset($config["log_location"]));	
+	$this->assertTrue(set("site_filestore"));
+	$this->assertTrue(set("site_temp_filestore")); 
+	$this->assertTrue(set("site_simplesamllocation"));
+	$this->assertTrue(set("log_location"));	
 	
-	$this->assertTrue(isset($config["db_type"]));
-	$this->assertTrue(isset($config["db_host"]));
-	$this->assertTrue(isset($config["db_database"]));
-	$this->assertTrue(isset($config["db_port"]));
+	$this->assertTrue(set("db_type"));
+	$this->assertTrue(set("db_host"));
+	$this->assertTrue(set("db_database"));
+	$this->assertTrue(set("db_port"));
 	// database username and password
-	$this->assertTrue(isset($config["db_username"]));
-	$this->assertTrue(isset($config["db_password"]));
+	$this->assertTrue(set("db_username"));
+	$this->assertTrue(set("db_password"));
 	
 	// cron settings
-	$this->assertTrue(isset($config["cron_exclude prefix"]));
-	$this->assertTrue(isset($config["cron_shred"]));
-	$this->assertTrue(isset($config["cron_shred_command"]));
-	$this->assertTrue(isset($config["cron_cleanuptempdays"]));
+	$this->assertTrue(set("cron_exclude prefix"));
+	$this->assertTrue(set("cron_shred"));
+	$this->assertTrue(set("cron_shred_command"));
+	$this->assertTrue(set("cron_cleanuptempdays"));
 	
 	// email templates section
-	$this->assertTrue(isset($config["default_emailsubject"]));
+	$this->assertTrue(set("default_emailsubject"));
+	
 	}
 	
 }
-
+function set($item)
+	{
+			$CFG = new Config();
+	 $config = $CFG->loadConfig();
+		return isset($config[$item]);
+	}
 ?>
--- unittest/utconfig.php
+++ unittest/utconfig.php
@@ -1,14 +1,19 @@
 <?php
 // set default objects for unit testing
 
+		$default = array();
+		$default["version"] = "1.6-multiupload-3";
+		$default["guid"] = "6bf0eeed-e341-a8c8-22a6-00004e587674";
+		
+		// valid object
 		$obj = array();
 		$obj["fileexpirydate"] = "2013-05-17 09:18:27";
-		$obj["fileto"] = "testto@filesender.org";
+		$obj["fileto"] = "chris@ricoshae.com.au";
 		$obj["filesubject"] = "Test Subject";
 		$obj["fileactivitydate"] = "2013-05-17 09:18:26";
 		$obj["filevoucheruid"] = "6bf0eeed-e341-a8c8-22a6-00004e587674";
 		$obj["filemessage"] = "Test Message";
-		$obj["filefrom"] = "testfrom@filesender.org";
+		$obj["filefrom"] = "chrisrichter@crcsmedia.com";
 		$obj["filesize"] = "1000";
 		$obj["fileoriginalname"] = "filename.zip";
 		$obj["filestatus"] = "Available";
@@ -21,6 +26,45 @@
 		$obj["fileauthuseruid"] = "5ea610bbd6d3bd907b04be9acb5cb6e273ef8c96";
 		$obj["fileauthuseremail"] = "";
 		$obj["filecreateddate"] = "2013-05-17 09:18:26";
+		$obj["filetrackingcode"] = "";
+		$obj["filegroupid"] = "";
+		$obj["filedownloadconfirmations"] = "";
+		$obj["fileenabledownloadreceipts"] = "";
+		$obj["filedailysummary"] = "";
 		$obj["fileoptions"] = "";
-
-?>
+		$obj["fileid"] = "";
+		$obj["filegroupuid"] = "";
+		$obj["filesent"] = "";
+		$obj["fileauthurl"] = "";	
+		
+		// invalid object
+		$objInvalid = array();
+		$objInvalid["fileexpirydate"] = "";
+		$objInvalid["fileto"] = "";
+		$objInvalid["filesubject"] = "";
+		$objInvalid["fileactivitydate"] = "";
+		$objInvalid["filevoucheruid"] = "";
+		$objInvalid["filemessage"] = "";
+		$objInvalid["filefrom"] = "";
+		$objInvalid["filesize"] = "";
+		$objInvalid["fileoriginalname"] = "";
+		$objInvalid["filestatus"] = "";
+		$objInvalid["fileip4address"] = "";
+		$objInvalid["fileip6address"] = "";
+		$objInvalid["filesendersname"] = "";
+		$objInvalid["filereceiversname"] = "";
+		$objInvalid["filevouchertype"] = "";
+		$objInvalid["fileuid"] = "";
+		$objInvalid["fileauthuseruid"] = "";
+		$objInvalid["fileauthuseremail"] = "";
+		$objInvalid["filecreateddate"] = "";
+		$objInvalid["filetrackingcode"] = "";
+		$objInvalid["filegroupid"] = "";
+		$objInvalid["filedownloadconfirmations"] = "";
+		$objInvalid["fileenabledownloadreceipts"] = "";
+		$objInvalid["filedailysummary"] = "";
+		$objInvalid["fileoptions"] = "";
+		$objInvalid["fileid"] = "";
+		$objInvalid["filegroupuid"] = "";
+		$objInvalid["filesent"] = "";
+		$objInvalid["fileauthurl"] = "";		
