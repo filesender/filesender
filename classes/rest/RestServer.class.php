43b160b0ed2b006fe616479742b7fe0bb7bf76b3
etiennemeleard <etiennemeleard@8555b757-d854-4b86-925a-82fd84c90ff4>
2014-07-31 15:08:11 +0000

    Introducing rest webservice as it ease up testing auth changes (instead of having to propagate all testing in index.php and such ...).
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/branches/filesender-2.0@2815 8555b757-d854-4b86-925a-82fd84c90ff4

---
+++ classes/rest/RestServer.class.php
@@ -0,0 +1,198 @@
+<?php
+
+/*
+ * FileSender www.filesender.org
+ * 
+ * Copyright (c) 2009-2012, AARNet, Belnet, HEAnet, SURFnet, UNINETT
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * *    Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ * *    Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ * *    Neither the name of AARNet, Belnet, HEAnet, SURFnet and UNINETT nor the
+ *     names of its contributors may be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+// Require environment (fatal)
+if (!defined('FILESENDER_BASE')) 
+    die('Missing environment');
+
+/**
+ * REST server
+ */
+class RestServer {
+    /**
+     * Process the request
+     * 
+     * @throws lots of various exceptions
+     */
+    public static function process() {
+        try {
+            @session_start();
+            
+            // Get request data
+            
+            $path = array();
+            if(array_key_exists('PATH_INFO', $_SERVER)) $path = array_filter(explode('/', $_SERVER['PATH_INFO']));
+            
+            $method = null;
+            foreach(array('X_HTTP_METHOD_OVERRIDE', 'REQUEST_METHOD') as $k) {
+                if(!array_key_exists($k, $_SERVER)) continue;
+                $method = strtolower($_SERVER[$k]);
+            }
+            
+            RestException::setContext('method', $method);
+            if(!in_array($method, array('get', 'post', 'put', 'delete'))) throw new RestException('rest_method_not_allowed', 405);
+            
+            $endpoint = array_shift($path);
+            if(!$endpoint) throw new RestException('rest_endpoint_missing', 400);
+            RestException::setContext('endpoint', $endpoint);
+            
+            $request = new RestRequest();
+            
+            $input = AuthRemoteRequest::body(); // Because php://input can only be read once for PUT requests we rely on a central getter in AuthRemoteRequest
+            
+            $type = array_key_exists('CONTENT_TYPE', $_SERVER) ? $_SERVER['CONTENT_TYPE'] : null;
+            if(!$type && array_key_exists('HTTP_CONTENT_TYPE', $_SERVER)) $type = $_SERVER['HTTP_CONTENT_TYPE'];
+            
+            // Parse body
+            switch($type) {
+                case 'text/plain' :
+                    $request->input = $input;
+                    break;
+                
+                case 'application/x-www-form-urlencoded' :
+                    $data = array();
+                    parse_str($input, $data);
+                    $request->input = (object)$data;
+                    break;
+                
+                case 'application/json' :
+                default :
+                    $request->input = json_decode($input);
+            }
+            
+            //Check authentication
+            try {
+                Auth::isAuthenticated();
+                if(Auth::isRemoteApplication()) {
+                    $application = AuthRemoteRequest::application();
+                    if(array_key_exists($endpoint, $application['acl'])) {
+                        $acl = $application['acl'][$endpoint];
+                    }else if(array_key_exists('*', $application['acl'])) {
+                        $acl = $application['acl']['*'];
+                    }else throw new RestException('rest_access_forbidden', 403);
+                    
+                    if(is_array($acl)) {
+                        $macl = false;
+                        if(array_key_exists($method, $acl)) {
+                            $macl = $acl[$method];
+                        }else if(array_key_exists('*', $acl)) {
+                            $macl = $acl['*'];
+                        }
+                        
+                        if(!$macl) throw new RestException('rest_method_not_allowed', 403);
+                    }else if(!$acl) throw new RestException('rest_access_forbidden', 403);
+                }
+            } catch(Exception $e) {
+                throw new Exception($e->getMessage(), $e->getCode() ? $e->getCode() : 400);
+            }
+            
+            // JSONP specifics
+            if(array_key_exists('callback', $_GET) && ($method != 'get')) throw new RestException('rest_jsonp_get_only', 405);
+            
+            // Get response filters
+            foreach($_GET as $k => $v) switch($k) {
+                case 'count' :
+                case 'startIndex' :
+                    if(preg_match('`^[0-9]+$`', $v)) $request->$k = (int)$v;
+                    break;
+                
+                case 'format' :
+                    break;
+                
+                case 'filterOp' :
+                    if(is_array($v)) {
+                        $request->filterOp = new stdClass();
+                        foreach(array('equals', 'startWith', 'contains', 'present') as $k)
+                            if(array_key_exists($k, $v)) $request->filterOp->$k = $v[$k];
+                    }
+                    break;
+                
+                case 'sortOrder' :
+                    if(in_array($v, array('ascending', 'descending'))) $request->sortOrder = $v;
+                    break;
+                
+                case 'updatedSince' :
+                    // updatedSince takes ISO date, relative N days|weeks|months|years format and epoch timestamp (UTC)
+                    date_default_timezone_set($config->Default_TimeZone);
+                    $updatedSince = null;
+                    if(preg_match('`^[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}(Z|[+-][0-9]{2}:[0-9]{2})$`', $v)) {
+                        // ISO date
+                        $config = Config::getInstance();
+                        $localetz = new DateTimeZone($config->Default_TimeZone);
+                        $offset = $localetz->getOffset(new DateTime($v));
+                        $updatedSince = strtotime($v) + $offset;
+                    }else if(preg_match('`^([0-9]+)\s*(hour|day|week|month|year)s?$`', $v, $m)) {
+                        // Relative N day|days|week|weeks|month|months|year|years format
+                        $updatedSince = strtotime('-'.$m[1].' '.$m[2]);
+                    }else if(preg_match('`^[0-9]+$`', $v)) $updatedSince = (int)$v; // Epoch timestamp
+                    
+                    if(!$updatedSince || !is_numeric($updatedSince))
+                        throw new RestException('rest_updatedsince_bad_format', 400, array('since' => $updatedSince));
+                    
+                    $request->updatedSince = $updatedSince;
+                    break;
+            }
+            
+            // Forward to handler
+            $class = 'RestEndpoint'.ucfirst($endpoint);
+            if(!file_exists(FILESENDER_BASE.'/classes/rest/endpoints/'.$class.'.class.php')) // Avoids FileNotFoundException from Autoloader
+                throw new RestException('endpoint_not_implemented', 501);
+                
+            $handler = new $class($request);
+            if(!method_exists($handler, $method)) throw new RestException('method_not_implemented', 501);
+            
+            $data = call_user_func_array(array($handler, $method), $path);
+            
+            // Output data
+            if(array_key_exists('callback', $_GET)) {
+                header('Content-Type: text/javascript');
+                echo $_GET['callback'].'('.json_encode($data).');';
+                exit;
+            }
+            
+            header('Content-Type: application/json');
+            if(($method == 'post') && $data) {
+                RestUtilities::sendResponseCode(201);
+                header('Location: '.Config::get('site_url').'/rest.php/'.$endpoint.'/'.$data['path']);
+                $data = $data['data'];
+            }
+            echo json_encode($data);
+            
+        } catch(Exception $e) { // Return exceptions as HTTP errors
+            $code = $e->getCode();
+            if($code < 400 || $code >= 600) $code = 500;
+            RestUtilities::sendResponseCode($code);
+            header('Content-Type: text/plain');
+            echo $e->getMessage();
+        }
+    }
+}
---
+++ classes/rest/RestUtilities.class.php
@@ -0,0 +1,115 @@
+<?php
+
+/*
+ * FileSender www.filesender.org
+ * 
+ * Copyright (c) 2009-2012, AARNet, Belnet, HEAnet, SURFnet, UNINETT
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * *    Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ * *    Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ * *    Neither the name of AARNet, Belnet, HEAnet, SURFnet and UNINETT nor the
+ *     names of its contributors may be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+// Require environment (fatal)
+if (!defined('FILESENDER_BASE')) 
+    die('Missing environment');
+
+/**
+ * REST server
+ */
+class RestUtilities {
+    
+    /**
+     * Flatten arguments (recursive)
+     * 
+     * @param $a array multi-dimensionnal array
+     * @param $p string parent key stack
+     * 
+     * @return array single dimension array
+     */
+    public static function flatten($a, $p = null) {
+        $o = array();
+        ksort($a);
+        foreach($a as $k => $v) {
+            if(is_array($v)) {
+                foreach(self::flatten($v, $p ? $p.'['.$k.']' : $k) as $s) $o[] = $s;
+            }else $o[] = ($p ? $p.'['.$k.']' : $k).'='.$v;
+        }
+        return $o;
+    }
+    
+    /**
+     * Send HTTP response code (supports)
+     * 
+     * @param $code int http code
+     */
+    public static function sendResponseCode($code = 200) {
+        if(function_exists('http_response_code'))
+            return call_user_func('http_response_code', $code);
+            
+        $messages = array(
+            100 => 'Continue',
+            101 => 'Switching Protocols',
+            200 => 'OK',
+            201 => 'Created',
+            202 => 'Accepted',
+            203 => 'Non-Authoritative Information',
+            204 => 'No Content',
+            205 => 'Reset Content',
+            206 => 'Partial Content',
+            300 => 'Multiple Choices',
+            301 => 'Moved Permanently',
+            302 => 'Moved Temporarily',
+            303 => 'See Other',
+            304 => 'Not Modified',
+            305 => 'Use Proxy',
+            400 => 'Bad Request',
+            401 => 'Unauthorized',
+            402 => 'Payment Required',
+            403 => 'Forbidden',
+            404 => 'Not Found',
+            405 => 'Method Not Allowed',
+            406 => 'Not Acceptable',
+            407 => 'Proxy Authentication Required',
+            408 => 'Request Time-out',
+            409 => 'Conflict',
+            410 => 'Gone',
+            411 => 'Length Required',
+            412 => 'Precondition Failed',
+            413 => 'Request Entity Too Large',
+            414 => 'Request-URI Too Large',
+            415 => 'Unsupported Media Type',
+            500 => 'Internal Server Error',
+            501 => 'Not Implemented',
+            502 => 'Bad Gateway',
+            503 => 'Service Unavailable',
+            504 => 'Gateway Time-out',
+            505 => 'HTTP Version not supported'
+        );
+        
+        $protocol = array_key_exists('SERVER_PROTOCOL', $_SERVER) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.0';
+        header($protocol.' '.$code.' '.$messages[$code]);
+        
+        return $code;
+    }
+}
