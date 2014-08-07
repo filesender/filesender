cd545bae3dd7bfc50da65d3dc402888b6d66ab25
etiennemeleard <etienne.meleard@renater.fr>
2014-08-07 15:51:23 +0000

    Update storage classes to be fully static and use delegation.
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/branches/filesender-2.0@2879 8555b757-d854-4b86-925a-82fd84c90ff4

--- classes/storage/StorageFileSystem.class.php
+++ classes/storage/StorageFileSystem.class.php
@@ -32,256 +32,219 @@
 if (!defined('FILESENDER_BASE')) die('Missing environment');
 
 /**
- *  Represents a file on the filesystem 
- *
- *  @property string $tempdir: path to local temporary directory
- *  @property string $uploaddir: path to local upload dir
- *  @property FileSystem $instance: holds a single instance of this class
- *  @property uint $chunksize
- *  @property bool $calc_hash:    whether calculating hash for storage type is supported
+ *  Gives access to a file on the filesystem 
  */
-class StorageFileSystem
-{
-    ////////////
-    //Properties
-    ////////////
-    private $uploadfolder = null;
-    private $tempfolder = null;
-    private $chunksize  = null;
-    private static $instance = null;
-    private $calculate_hash = false;
-
-
+class StorageFilesystem {
     /**
-     *  Gets the FileSystem object
-     *  Creates a new one, if none exists (need only one, hence singleton class declaration)
+     * Storage path
      */
-    public static function getInstance()
-    {
-        if(is_null(self::$instance))
-            self::$instance = new self();
-        return self::$instance;
-    }
-
-
+    private static $path = null;
+    
     /**
-     *  Constructor: creates a new instance and sets
-     *  properties from config
+     * Folder hashing
      */
-    private function __construct()
-    {
-        $this->uploadfolder = Config::get('filestorage_filesystem_file_location');
-        $this->tempfolder = Config::get('filestorage_filesystem_temp_location');
-
-        // If chunk size not defined in config then default to 2mb
-        if (is_null($this->chunksize = Config::get('upload_chunk_size')))
-            $this->chunksize = 2*1024*1024;
-
-        $this->calculate_hash = Config::get('filestorage_filesystem_calc_hash');
-    }
-
-
+    private static $hashing = null;
+    
     /**
-     *  Default getter - instance getter could also be added here
-     *  @param string $pname: property name
-     *  @returns property with name = $pname
+     * Storage setup, loads options from config
      */
-    public function __get($pname)
-    {
-        if(in_array($pname, array('uploadfolder', 'tempfolder', 'chunksize',
-            'calculate_hash' )))
-            return $this->$pname;
-        //If property $pname does not exist
-        throw new PropertyAccessException($this, $pname);
+    private static function setup() {
+        if(!is_null(self::$path)) return;
         
+        $path = Config::get('storage_filesystem_path');
+        if(!$path) throw new ConfigMissingParameterException('storage_filesystem_path');
+        
+        if(!is_dir($path) || !is_writable($path))
+            throw new StorageFilesystemCannotWriteException($path);
+        
+        self::$path = $path;
+        
+        $hashing = Config::get('storage_filesystem_hashing');
+        if($hashing) self::$hashing = $hashing;
     }
     
-
     /**
-     *  Default setter: sets property $pname to value $value
-     *  @param string @pname: property name
-     *  @param string @value: value to set to
+     * Checks for free space on the disk where file will be stored
+     * 
+     * @param File $file
+     *
+     * @return float $availablembytes: free space in mbytes
      */
-    public function __set($pname, $value)
-    {
-        if(in_array($pname, array('chunksize')) && !is_null($value))
-            $this->pname = $value;
-        throw new PropertyAccessException($this, $pname);
+    public static function getFreeSpace(File $file) {
+        $location = self::buildPath($file);
+        
+        $free = disk_free_space($location); // TODO move to utilities ? to self ?
+        
+        return $free;
     }
-
-
+    
+    /**
+     * Build file storage path (without file uid)
+     * 
+     * @param object $file
+     * 
+     * @return string path
+     */
+    private static function buildPath(File $file) {
+        self::setup();
+        
+        $path = self::$path;
+        if(substr($path, -1) != '/') $path .= '/';
+        
+        // Is storage path hashing enabled
+        if(self::$hashing) {
+            $subpath = '';
+            
+            if(is_numeric(self::$hashing)) {
+                // Prepend self::$hashing letters from $file->uid as subfolders of $path
+                for($i=0; $i<self::$hashing; $i++)
+                    $subpath .= substr($file->uid, $i, 1).'/';
+                
+            }else if(is_callable(self::$hashing)) {
+                // Call self::$hashing with $file to get sub-path
+                $subpath = trim(trim(self::$hashing($file)), '/');
+            }
+            
+            if($subpath) { // Ensure that subpath exists and is writable
+                $p = $path;
+                foreach(array_filter(explode('/', $subpath)) as $sub) {
+                    $p .= $sub;
+                    
+                    if(!is_dir($p) && !mkdir($p))
+                        throw new StorageFilesystemCannotCreatePathException($p);
+                    
+                    if(!is_writable($p))
+                        throw new StorageFilesystemCannotWriteException($p);
+                    
+                    $p .= '/';
+                }
+            }
+        }
+        
+        return $path;
+    }
+    
     /**
      *  Reads chunk at offset
      *
-     *  @param File $dbfile: The database file object with file info
-     *  @param uint $offset: offset as no. of bytes
-     *  @throws StorageAccessException if file cannot be read
-     *  @throws FileNotFoundException if file cannot be found at location
-     *  @throws NoDataAtOffsetException
-     *  @return string chunk, chunk data enc as string, or false
+     * @param File $file
+     * @param uint $offset offset in bytes
+     * 
+     * @return mixed chunk data encoded as string or null if no chunk remaining
+     * 
+     * @throws StorageFilesystemFileNotFoundException
+     * @throws StorageFilesystemCannotReadException
      */
-    public function readChunk(File $dbfile, $offset)
-    {
-        /* setting execution time limit, sending headers, etc. is delegated to
-         * the webservice (in rest/)
-         * set_time_limit(0); */
-
-        if ($offset)
-            $chunksize = $offset; //in bytes
-        else
-            $chunksize = 1 * 1024 * 1024;  // in bytes
-
-        $chunk = '';
+    public function readChunk(File $file, $offset = 0) {
+        $chunk_size = (int)Config::get('download_chunk_size');
         
-        $path = $this->uploadfolder.$dbfile->name;
-
-        //Locates file, opens it for reading, reads, returns data
-        try {
-            if (!file_exists($path))
-                throw new StorageFileSystemNotFoundException($path);
-
-            if (($file = fopen($this->uploadfolder.$dbfile->name, 'rb')) != true)
-                throw new StorageAccessException(get_class());
-
+        $file_path = self::buildPath($file).$file->uid;
+        
+        if(!file_exists($file_path))
+            throw new StorageFilesystemFileNotFoundException($file_path);
+        
+        // Open file for reading
+        if($fh = fopen($file_path, 'rb')) {
             // Sets position of file pointer
-            if ( $offset != 0 ) {
-                fseek($file, $offset);
-            }
-
-            if (($chunk = fread($file, $chunksize)) == false)
-                // This might not be needed
-                throw new NoDataAtOffsetException();
-
-            fclose($file);
-            return $chunk;
-
-        } catch (StorageAccessException $e) {
-        
-        } catch (StorageFileSystemNotFoundException $e) {
-
-        } catch (NoDataAtOffsetException $e) {
-
-        }
-        
-        //failed opening file
-        return false;
-    }
-
-
-    /**
-     *  Write a chunk of data to file (appends if not empty)
-     *
-     *  @param File $dbfile: object with file info
-     *  @param string $chunk: the chunk of data to write
-     *  @param uint $offset: offset as no. of bytes
-     *  @throws StorageAccessException if file cannot be written to
-     *  @throws OutOfSpaceException if disk doesn't have space for chunk
-     *  @throws CannotWriteToFileException if any other write error
-     *  @return true if written, false if error
-     */
-    public function writeChunk(File $dbfile, $chunk, $offset)
-    {
-        // if file doesn't exist: creates it
-        $path = $this->uploadfolder.$dbfile->name;
-
-        try {
-            if (($file = fopen($path, 'w')) != true)
-                throw new StorageAccessException(get_class());    //failed opening file
+            if($offset) fseek($fh, $offset);
             
-            // set file position pointer to $offset
-            // if postdata was accessed using php://input, fseek cannot be used,
-            // if using php://stdin - yes
-            // should use: $fdata = fopen('php://input', 'r'); while(
-            // fread($fdata, $chunk) { writeChunk() } in calling method
-            if ($offset)
-                fseek($file, $offset);
-
-            if ($this->getFreeSpace() <= strlen($chunk)) {
-                throw new OutOfSpaceException($path);
-            }
-
-            $written = fwrite($file, $chunk); //$written = nr of bytes
-
-            fclose($file);
-
-            if ($written)
-                return $written;
-            else
-                throw new CannotWriteToFileException($path);
-
-        } catch (StorageAccessException $eaccess) {
-
-        } catch (OutOfSpaceException $espace) {
+            // Try to read chunk
+            $chunk_data = fread($fh, $chunk_size);
+            
+            // Close reader
+            fclose($fh);
+            
+            if($chunk_data === false) return null; // No data remaining
+            
+            return $chunk_data;
+            
+        }else throw new StorageFilesystemCannotReadException($file_path);
+    }
+    
+    /**
+     * Write a chunk of data to file at offset
+     * 
+     * @param File $file
+     * @param string $data the chunk data
+     * @param uint $offset offset in bytes
+     * 
+     * @return array with offset and written amount of bytes
+     * 
+     * @throws StorageFilesystemOutOfSpaceException
+     * @throws StorageFilesystemCannotWriteException
+     */
+    public function writeChunk(File $file, $data, $offset = null) {
+        $chunk_size = strlen($data);
         
-        } catch (CannotWriteToFileException $ewrite) {
-
+        $file_path = self::buildPath($file).$file->uid;
+        
+        $free_space = self::getFreeSpace($file);
+        if($free_space <= $chunk_size) {
+            throw new OutOfSpaceException($path);
         }
-
-        //failed to write to file
-        return false;
+        
+        // Open file for writing
+        $mode = file_exists($file_path) ? 'rb+' : 'wb+'; // Create file if it does not exist
+        if($fh = fopen($file_path, $mode)) {
+            // Sets position of file pointer
+            if($offset) {
+                fseek($fh, $offset); // Known offset
+            }else if(is_null($offset)) {
+                fseek($fh, 0, SEEK_END); // End of file if no offset given
+            }
+            
+            // Get offset
+            $offset = ftell($fh);
+            
+            // Try to write chunk
+            $written = fwrite($fh, $data);
+            
+            // Close writer
+            fclose($fh);
+            
+            return array(
+                'offset' => $offset,
+                'written' => $written
+            );
+        }else throw new StorageFilesystemCannotWriteException($file_path);
     }
-
-
+    
     /**
-     *  Deletes the file given as argument
-     *
-     *  @param File $dbfile:    File object
-     *  @throws StorageFileSystemNotFoundException
-     *  @return true if successful, false oterwise
+     * Deletes a file
+     * 
+     * @param File $file
+     * 
+     * @throws StorageFilesystemCannotDeleteException
      */
-    public function delete(File $dbfile)
-    {
-        $path = $this->uploadfolder.$dbfile->name;
-        try {
-            if(!file_exists($path))
-                throw new StorageFileSystemNotFoundException();
-            return unlink($path);
-        } catch (StorageFileSystemNotFoundException $e) {
-
-        }
-
-        return false;
+    public function delete(File $file) {
+        $file_path = self::buildPath($file).$file->uid;
+        
+        if(!file_exists($file_path)) return;
+        
+        if(!unlink($file_path))
+            throw new StorageFilesystemCannotDeleteException($file_path);
     }
-
-
+    
     /**
-     *  Checks for unused space on the disk
-     *  where upload dir is located
-     *
-     *  @return float $availablembytes: free space in mbytes
+     * Tells wether storage support file digests
+     * 
+     * @return bool
      */
-    public function getFreeSpace()
-    {
-        $udir = $this->uploadfolder;
-        $availablebytes = disk_free_space($udir);
-
-        $availablembytes = $availablebytes / 1024 / 1024;
-
-        return $availablembytes;
+    public static function supportsDigest() {
+        return true;
     }
-
+    
     /**
-     *  TODO a method to find how much space the file actually takes on disk
-     *  (as opposed to what the database record says)
+     * Computes the digest of a file
+     * 
+     * @param File $file
+     * 
+     * @return string hex digest
      */
-    public function getUsedSpace()
-    {
-
-    }
-
-    /**
-     *  Get hash
-     *
-     *  @param File $dbfile with file info
-     *  @return string hash code; false if hash calculation not supported or error
-     */
-    public function getHash(File $dbfile)
-    {
-        if (!$this->calculate_hash)
-            return false;
-       
-        // Just a skeleton to try out approaches, so always return false
-        return false;
+    public static function getDigest(File $file) {
+        $file_path = self::buildPath($file).$file->uid;
+        
+        return sha1_file($file_path);
     }
 }
 
