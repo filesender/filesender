3f69fc5d34f5c57d1ae00471d80d8a11d08ded9b
etiennemeleard <etienne.meleard@renater.fr>
2014-08-14 14:55:23 +0000

    Managing cron task for closing expired transfer
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/branches/filesender-2.0@2923 8555b757-d854-4b86-925a-82fd84c90ff4

---
+++ cron/auditLogHandler.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * FileSender www.filesender.org
+ * 
+ * Copyright (c) 2009-2014, AARNet, Belnet, HEAnet, SURFnet, UNINETT
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * *	Redistributions of source code must retain the above copyright
+ * 	notice, this list of conditions and the following disclaimer.
+ * *	Redistributions in binary form must reproduce the above copyright
+ * 	notice, this list of conditions and the following disclaimer in the
+ * 	documentation and/or other materials provided with the distribution.
+ * *	Neither the name of AARNet, Belnet, HEAnet, SURFnet and UNINETT nor the
+ * 	names of its contributors may be used to endorse or promote products
+ * 	derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS'
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+require_once (dirname(__FILE__).'/commonCronConfig.php');
+
+$transfers = Transfer::all(array('where' => "DATE_FORMAT(expires,:dateformat) = DATE_FORMAT(SUBDATE(NOW(), INTERVAL 1 DAY),:dateformat)" ), array('dateformat' => "%y-%d-%m"));
+
+if (sizeof($transfers) > 0){
+    foreach ( $transfers as $transfer){
+        $transfer->close(false);
+    }
+}else{
+    Logger::log(LogLevels::INFO, "[CRON] No transfer expired found.");
+}
