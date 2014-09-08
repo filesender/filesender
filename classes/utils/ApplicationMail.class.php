13d3a90076a953f1f9950b4d8cb3d2b5c6ef7451
etiennemeleard <etienne.meleard@renater.fr>
2014-09-08 15:46:36 +0000

    Refactor mail system (2nd pass), introduce quick ways to send application scoped (reply-to and such) emails from translated emails directly.
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/branches/filesender-2.0@3059 8555b757-d854-4b86-925a-82fd84c90ff4

---
+++ classes/utils/ApplicationMail.class.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+ * FileSender www.filesender.org
+ * 
+ * Copyright (c) 2009-2012, AARNet, Belnet, HEAnet, SURFnet, UNINETT
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * *    Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ * *    Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ * *    Neither the name of AARNet, Belnet, HEAnet, SURFnet and UNINETT nor the
+ *     names of its contributors may be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+// Require environment (fatal)
+if (!defined('FILESENDER_BASE'))
+    die('Missing environment');
+
+class ApplicationMail extends Mail {
+    /**
+     * Constructor
+     * 
+     * @param mixed $content Lang instance or subject as string
+     */
+    public function __construct($content = 'No subject') {
+        $replyto = Config::get('email_reply_to');
+        $replyto_name = Config::get('email_reply_to_name');
+        $use_html = Config::get('email_use_html');
+        
+        $subject = ($content instanceof Lang) ? (string)$content->subject : $content;
+        
+        parent::__construct($subject, $replyto, $replyto_name, $use_html);
+        
+        if($content instanceof Lang) {
+            $this->writePlain($content->plain);
+            
+            if($use_html)
+                $this->writeHTML($content->html);
+        }
+    }
+}
