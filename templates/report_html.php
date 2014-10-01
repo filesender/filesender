df09ed287115b4b14fa4130fd4f4244d8a6be1bf
etiennemeleard <etiennemeleard@8555b757-d854-4b86-925a-82fd84c90ff4>
2014-10-01 16:12:54 +0000

    Add report generation templates.
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/branches/filesender-2.0@3134 8555b757-d854-4b86-925a-82fd84c90ff4

--- templates/report_html.php
+++ templates/report_html.php
@@ -1,6 +1,26 @@
-<ul>
-    <li><?php echo $target->id; ?></li>
-    <li><?php echo $target->subject; ?></li>
-    <li><?php echo $target->message; ?></li>
-</ul>
-<br/>
+<?php
+
+$lines = array();
+foreach($report->logs as $entry) {
+    echo '<tr>';
+    
+    echo '<td>'.Utilities::formatDate($entry->created).'</td>';
+    
+    $lid = 'report_';
+    if($report->target_type == 'Recipient')
+        $lid .= 'recipient_';
+        
+    $lid .= 'event_'.$entry->event;
+    
+    $action = Lang::tr($lid)->r(
+        array(
+            'author' => $entry->author,
+            'log' => $entry
+        ),
+        $entry->target
+    );
+    
+    echo '<td>'.$action.'</td>';
+    
+    echo '</tr>';
+}
