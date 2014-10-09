0e8d38dc40f64381b9c1fa42c93b94646d3325c2
etiennemeleard <etienne.meleard@renater.fr>
2014-10-09 13:02:43 +0000

    Move method where it belongs.
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/branches/filesender-2.0@3165 8555b757-d854-4b86-925a-82fd84c90ff4

--- classes/utils/Utilities.class.php
+++ classes/utils/Utilities.class.php
@@ -191,41 +191,5 @@ class Utilities
             return "";
         }
     }
-    
-    
-    /**
-     * Force HTTPS - redirects HTTP to HTTPS.
-     */
-    public static function forceHTTPS(){
-        if (
-            // Unless force_ssl is false or 0 ...
-            Config::get('force_ssl') !== false && Config::get('force_ssl') !== 0
-            // ... or we're on https ...
-            && !(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')
-        ) {
-            if (session_id() != '') {
-                // Destroy current session to prevent stealing session, because someone may have sniffed it during our HTTP (not HTTPS) request.
-                unset($_SESSION);
-
-                if (ini_get('session.use_cookies')) {
-                    // Unset the PHPSESSID cookie, so that the user will get a new session ID on their next request.
-                    $params = session_get_cookie_params();
-
-                    setcookie(
-                        session_name(), '', time() - 42000,
-                        $params['path'], $params['domain'],
-                        $params['secure'], $params['httponly']
-                    );
-                }
-
-                session_destroy();
-            }
-
-            // ... Redirect the user to HTTPS.
-            $redirect = sprintf('Location: https://%s%s', $_SERVER['HTTP_HOST'], $_SERVER['REQUEST_URI']);
-            header($redirect);
-            exit;
-        }
-    }
 }
 
