d2846dceba1ee5639d9b554fe1dd66a22cc3080a
mdobrx414308 <mdobrx414308@8555b757-d854-4b86-925a-82fd84c90ff4>
2014-12-07 13:01:57 +0000

    Configurable maximum size of file that is accepted when encryption is used
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/qtest/filesender-1.6-crypto@3226 8555b757-d854-4b86-925a-82fd84c90ff4

--- language/nl_NL.php
+++ language/nl_NL.php
@@ -289,5 +289,6 @@ $lang["_MISSING_PASSWORD"] = "Fout: er moet een wachtwoord worden ingevuld.";
 $lang["_UPLOAD_ENCRYPT_PROGRESS_MESSAGE"] = "Het versleutelen en uploaden van het bestand kan even duren.";
 $lang["_UPLOAD_COMPLETE_ENCRYPTED"] = "Het bestand is versleuteld. Vergeet niet om het wachtwoord op een veilige manier aan de ontvanger te geven. Zonder het wachtwoord is de download niet mogelijk";
 $lang["_RANDOM_NOT_READY"] = "Random number is nog niet klaar; :1% voortgang. Probeer het over een paar seconden nogmaals.";
-$lang["_GENERATE_RANDOM"] = "Genereer";		
+$lang["_GENERATE_RANDOM"] = "Genereer";
+$lang["_ENCSIZE_WARNING"] = "Het bestand ':1' is te groot om als versleuteld bestand verwerkt te kunnen worden.";		
 ?>
