f96bd251733f41c4c5474afbe2d0f60f51b67b9e
etiennemeleard <etiennemeleard@8555b757-d854-4b86-925a-82fd84c90ff4>
2014-12-17 14:39:25 +0000

    Add feedback email handling script and bounce handling related default config.
    Script can be called as :
        
        php cron/emailfeedback.php [options] [sources]
        
        options are :
          * --remove_after_processing : remove file after processing
          * --move_after_processing[=target] : move file to target folder after processing, if no absolute target will be considered to be under source folder, if no target given "done" source subfolder is used by default
        
        sources can be (several may be used at the same time) :
          * a path to a file containing a rfc822 message
          * a path to a folder containing rfc822 message files
          * - to get rfc822 message from standard input
        
        if no sources are defined standard input is used by default
    
    New config parameters related to bounce handling :
    
    report_bounces, bounces reporting mode, possible values are :
      - null / false / 0 : bounce reporting disabled (bounces are still saved and displayed in ui, just not reported by email)
      - asap : report every bounce right away (default)
      - daily : compile a daily report for each transfer/guest (handled by cron/cron.php)
      - asap_then_daily : bounces in a time range (see report_bounces_asap_then_daily_range) after transfer/guest creation are reported right away, bounces comming after are reported daily
    
    report_bounces_asap_then_daily_range, time range for asap reporting in asap_then_daily bounce reporting mode, in seconds, defaults to 15 minutes
    
    
    
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/branches/filesender-2.0@3271 8555b757-d854-4b86-925a-82fd84c90ff4

---
+++ cron/emailfeedback.php
@@ -0,0 +1,213 @@
+<?php
+
+/*
+ * FileSender www.filesender.org
+ * 
+ * Copyright (c) 2009-2012, AARNet, Belnet, HEAnet, SURFnet, UNINETT
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * *	Redistributions of source code must retain the above copyright
+ * 	notice, this list of conditions and the following disclaimer.
+ * *	Redistributions in binary form must reproduce the above copyright
+ * 	notice, this list of conditions and the following disclaimer in the
+ * 	documentation and/or other materials provided with the distribution.
+ * *	Neither the name of AARNet, Belnet, HEAnet, SURFnet and UNINETT nor the
+ * 	names of its contributors may be used to endorse or promote products
+ * 	derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+require_once(dirname(dirname(__FILE__)).'/includes/init.php');
+
+Logger::info('Email feedback handler started');
+
+// TODO daemon mode ?
+
+$remove_after_processing = false;
+$move_after_processing = false;
+$inputs = array();
+
+foreach(array_slice($argv, 1) as $arg) {
+    if($arg == '--remove_after_processing') {
+        $remove_after_processing = true;
+    
+    } else if(preg_match('/^--move_after_processing(=(.*))?$/', $arg, $m)) {
+        if($m[1]) {
+            $move_after_processing = preg_replace('`/+$`', '', $m[2]);
+        } else $move_after_processing = 'done';
+    } else {
+        $inputs[] = $arg;
+    }
+}
+
+if(!count($inputs)) $inputs[] = '-';
+
+while($inputs) {
+    $input = array_shift($inputs);
+    
+    if(is_dir($input)) {
+        if(substr($input, -1) != '/') $input .= '/';
+        foreach(scandir($input) as $i) {
+            if(!is_file($input.$i)) continue;
+            $inputs[] = $input.$i;
+        }
+        continue;
+    }
+    
+    try {
+        if($input != '-' && !is_file($input))
+            throw new Exception('not a valid feedback input');
+        
+        $message = null;
+        if($input == '-') {
+            $message = file_get_contents('php://stdin');
+        } else if(is_file($input)) {
+            $message = file_get_contents($input);
+        } else throw new Exception('not a valid feedback input');
+        
+        // Parse and process
+        $email = FeedbackMail::parse($message);
+        $headers = $email->headers;
+        $parts = $email->parts;
+        
+        // Get related object
+        $target_type = null;
+        $target_id = null;
+        $feedback_type = null;
+        $details = array();
+        
+        $return_path = Config::get('email_return_path');
+        if($return_path && preg_match('`^(.+)<verp>(.+)$`i', $return_path, $match)) {
+            $addr = explode('@', (string)$headers->to);
+            $variable = substr($addr[0], strlen($match[1]));
+            
+            if(preg_match('/^(recipient|guest)-([0-9]+)$/', $variable, $m)) {
+                $target_type = $m[1];
+                $target_id = $m[2];
+                
+                Logger::info('Got rfc3464 report for '.$target_type.'#'.$target_id.' (verp)');
+            }
+        }
+        
+        // Try to get context from original message if multipart/report
+        if(
+            $headers->content_type == 'multipart/report' &&
+            $headers->content_type_report_type == 'delivery-status' &&
+            count($parts) >= 3
+        ) {
+            if(preg_match('`^text/`', $parts[0]->headers->content_type)) {
+                $details[] = $parts[0]->content;
+            }
+            
+            if($parts[1]->headers->content_type == 'message/delivery-status') {
+                if(!$feedback_type) {
+                    $data = FeedbackMailHeaders::parse($parts[1]->content);
+                    
+                    if(
+                        preg_match('`failed`', (string)$data->action) ||
+                        preg_match('`^[45]\.?[0-9]\.?[0-9]$`', (string)$data->status)
+                    ) {
+                        $feedback_type = 'bounce';
+                        Logger::info('Got bounce from rfc3464 report');
+                    }
+                }
+                
+                $details[] = $parts[1]->content;
+            }
+                
+            if(!$target_type && $parts[2]->headers->content_type == 'message/rfc822') {
+                $attached_email = FeedbackMail::parse($parts[2]->content);
+                
+                if(preg_match('/^(recipient|guest)-([0-9]+)$/', (string)$attached_email->headers->x_filesender_context, $m)) {
+                    $target_type = $m[1];
+                    $target_id = $m[2];
+                    Logger::info('Got rfc3464 report for '.$target_type.'#'.$target_id.' (matched in custom header)');
+                } else if(preg_match('/^<(recipient|guest)-([0-9]+)-[0-9a-f]+@filesender>$/', (string)$attached_email->headers->message_id, $m)) {
+                    $target_type = $m[1];
+                    $target_id = $m[2];
+                    Logger::info('Got rfc3464 report for '.$target_type.'#'.$target_id.' (matched in message id)');
+                }
+                
+            }
+        }
+        
+        // Here we should have target_type, target_id and feedback_type
+        if(!$target_type)
+            throw new Exception('is missing target_type or anything allowing to discover it');
+        
+        if(!$target_id)
+            throw new Exception('is missing target_id or anything allowing to discover it');
+        
+        if(!$feedback_type)
+            throw new Exception('is missing feedback_type or anything allowing to discover it');
+        
+        $details = implode("\n\n", $details);
+        
+        $target = null;
+        try {
+            if($target_type == 'recipient') $target = Recipient::fromId((int)$m[2]);
+            else if($target_type == 'guest') $target = Guest::fromId((int)$m[2]);
+        } catch(Exception $e) {
+            throw new Exception('refers to an unknown '.$target_type.' #'.$target_id);
+        }
+        
+        // Handle feedback type
+        if($feedback_type == 'bounce') {
+            $bounce = TrackingEvent::create(
+                TrackingEventTypes::BOUNCE,
+                $target,
+                $email->headers->date ? strtotime($email->headers->date) : null,
+                $details
+            );
+            $bounce->save();
+            
+            $report = Config::get('report_bounces');
+            if(!$report) continue;
+            
+            if($report == 'asap_then_daily' && $target_type = 'guest') $report = 'asap';
+            
+            $range = (int)Config::get('report_bounces_asap_then_daily_range');
+            if(!$range) $range = 15 * 60;
+            
+            if(
+                $report == 'asap' ||
+                ($report == 'asap_then_daily' && $bounce->created < $target->transfer->created + $range)
+            )
+                $bounce->report();
+            
+        } else throw new Exception('gives feedback about unknown event : '.$feedback_type);
+        
+        if($input != '-') {
+            if($remove_after_processing) {
+                unlink($input);
+                
+            } else if($move_after_processing) {
+                $target = $move_after_processing;
+                if(substr($target, 0, 1) != '/') $target = dirname($input).'/'.$target;
+                
+                if(!is_dir($target) && !mkdir($target, 0777, true)) {
+                    throw new Exception('target directory "'.$target.'" does not exist and cannot be created');
+                
+                copy($input, $target.'/'.basename($input));
+                unlink($input);
+            }
+        }
+        
+    } catch(Exception $e) {
+        Logger::error($input.' processing failed : '.$e->getMessage());
+        continue;
+    }
+}
