cc7c31dd7f94ac332d83594f71b668565ddc72d3
etiennemeleard <etienne.meleard@renater.fr>
2015-01-08 16:42:44 +0000

    Highlight warning storages in admin section
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/branches/filesender-2.0@3371 8555b757-d854-4b86-925a-82fd84c90ff4

--- templates/admin_statistics_section.php
+++ templates/admin_statistics_section.php
@@ -20,13 +20,25 @@ foreach($storage_usage as $info) {
     $total_space += $info['total_space'];
     $free_space += $info['free_space'];
 }
+
+$level = Config::get('storage_usage_warning');
+$block_warnings = array();
+$global_warning = false;
+if($level) {
+    if($free_space <= $level * $total_space / 100) $global_warning = true;
+    
+    foreach($storage_usage as $block => $info) {
+        if($info['free_space'] > $level * $info['total_space'] / 100) continue;
+        $block_warnings[] = $block;
+    }
+}
 ?>
 <h3>{tr:storage_usage}</h3>
 
-<table class="storage_usage">
-    <tr><th>{tr:storage_total}</th><td><?php echo Utilities::formatBytes($total_space) ?></td></tr>
-    <tr><th>{tr:storage_used}</th><td><?php echo Utilities::formatBytes($total_space - $free_space).' ('.sprintf('%.1d', 100 * ($total_space - $free_space) / $total_space).'%)' ?></td></tr>
-    <tr><th>{tr:storage_available}</th><td><?php echo Utilities::formatBytes($free_space).' ('.sprintf('%.1d', 100 * $free_space / $total_space).'%)' ?></td></tr>
+<table class="storage_usage <?php echo $global_warning ? 'warning' : '' ?>">
+    <tr data-metric="total"><th>{tr:storage_total}</th><td><?php echo Utilities::formatBytes($total_space) ?></td></tr>
+    <tr data-metric="used"><th>{tr:storage_used}</th><td><?php echo Utilities::formatBytes($total_space - $free_space).' ('.sprintf('%.1d', 100 * ($total_space - $free_space) / $total_space).'%)' ?></td></tr>
+    <tr data-metric="available"><th>{tr:storage_available}</th><td><?php echo Utilities::formatBytes($free_space).' ('.sprintf('%.1d', 100 * $free_space / $total_space).'%)' ?></td></tr>
 </table>
 
 <table class="list storage_usage_blocks">
@@ -42,7 +54,7 @@ foreach($storage_usage as $info) {
     
     <tbody>
     <?php foreach($storage_usage as $block => $info) { ?>
-        <tr>
+        <tr class="<?php echo in_array($block, $block_warnings) ? 'warning' : '' ?>">
             <td><?php echo $block ?></td>
             <td><?php echo array_filter($info['paths']) ? implode(', ', $info['paths']) : Lang::tr('storage_main') ?></td>
             <td><?php echo Utilities::formatBytes($info['total_space']) ?></td>
