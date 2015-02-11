67d6d42d5e71f1e17c9b8f525e907c85709af6a6
etiennemeleard <etiennemeleard@8555b757-d854-4b86-925a-82fd84c90ff4>
2015-02-11 16:04:33 +0000

    Remove obsolete parameter
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/branches/filesender-2.0@3477 8555b757-d854-4b86-925a-82fd84c90ff4

--- includes/ConfigDefaults.php
+++ includes/ConfigDefaults.php
@@ -64,7 +64,6 @@ $default = array(
     'mac_unzip_name' => 'The Unarchiver',
     'mac_unzip_link' => 'http://unarchiver.c3.cx/unarchiver',
     'ban_extension' => 'exe,bat',
-    'max_email_recipients' => 50,
     
     'max_transfer_size' => 107374182400,
     'max_transfer_recipients' => 50,
