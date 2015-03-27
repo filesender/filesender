8d33787c056b89c0396b5438c221302afd69b3a1
etiennemeleard <etienne.meleard@renater.fr>
2015-03-27 16:49:40 +0000

    Security fix : set session cookie mode according to env and config
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/branches/filesender-2.0@3569 8555b757-d854-4b86-925a-82fd84c90ff4

--- includes/init/init_web.php
+++ includes/init/init_web.php
@@ -37,9 +37,21 @@ session_cache_limiter('nocache');
 // Start session if necessary
 if(!session_id()) {
     // start new session and mark it as valid because the system is a trusted source
-    session_start();
-    $_SESSION['valid'] = true;
     
+    // Set session cookie options
+    $site_url_parts = parse_url(Config::get('site_url'));
+    session_set_cookie_params(
+        0,                                                      // Cookie lives as long as browser isn't closed
+        $site_url_parts['path'],                                // It is only valid for the filesender app
+        $site_url_parts['host'],                                // and only for the precise domain
+        Config::get('force_ssl') || Utilities::httpsInUse(),    // It uses secure mode if ssl forced or in use
+        true                                                    // and is httpOnly (not reachable through javascript)
+    );
+        
+    session_start();            // Start the session
+    $_SESSION['valid'] = true;  // Set session as valid TODO do we we still need this ?
+    
+    // Generate security token if needed TODO need renewal
     if(!array_key_exists('security_token', $_SESSION))
         $_SESSION['security_token'] = Utilities::generateUID();
 }
@@ -48,13 +60,13 @@ if(!session_id()) {
 // Ensure HTTPS if needed
 GUI::forceHTTPS();
 
-// Handle magic quoting (maybe deprecated now ?)
+// Handle magic quoting (TODO maybe deprecated now ?)
 if(get_magic_quotes_gpc()) {
     $_POST = array_map('stripslashes', $_POST);
     $_GET = array_map('stripslashes', $_GET);
 };
 
-// Sanitize request
+// Sanitize all input variables
 $_GET = Utilities::sanitizeInput($_GET);
 $_POST = Utilities::sanitizeInput($_POST);
 $_COOKIE = Utilities::sanitizeInput($_COOKIE);
@@ -63,4 +75,5 @@ $_REQUEST = Utilities::sanitizeInput($_REQUEST);
 // Output is all UTF8
 header('Content-Type: text/html; charset=UTF-8');
 
+// Validate config on the fly
 require_once(FILESENDER_BASE.'/includes/ConfigValidation.php');
