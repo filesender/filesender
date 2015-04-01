05e94da9917c68e929ae032a65ae95922b1e7482
etiennemeleard <etiennemeleard@8555b757-d854-4b86-925a-82fd84c90ff4>
2015-04-01 10:00:21 +0000

    Add the "translate email" email footer
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/branches/filesender-2.0@3585 8555b757-d854-4b86-925a-82fd84c90ff4

---
+++ language/en_AU/translate_email_footer.mail.php
@@ -0,0 +1,11 @@
+subject: translate email footer, shared by all languages
+
+{alternative:plain}
+
+Translate this email : {raw:translatableemail.link}
+
+{alternative:html}
+
+<p>
+    Translate this email : <a href="{translatableemail.link}">{translatableemail.link}</a>
+</p>
