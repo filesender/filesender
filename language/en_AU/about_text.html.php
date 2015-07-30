ff3b8846b75be7c895b71f27c5bb135c6b2a6e28
etiennemeleard <etiennemeleard@8555b757-d854-4b86-925a-82fd84c90ff4>
2015-07-30 12:30:27 +0000

    Simplify file structure
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/branches/filesender-2.0@3670 8555b757-d854-4b86-925a-82fd84c90ff4

--- language/en_AU/about_text.html.php
+++ language/en_AU/about_text.html.php
@@ -1,4 +1,3 @@
-<h2><i class="fa fa-info-circle "> <span>About</span></i></h2>
 <p>
     {cfg:site_name} is an installation of FileSender (<a rel="nofollow" href="http://www.filesender.org/" target="_blank">www.filesender.org</a>),
     which is developed to the requirements of the higher education and research community.
