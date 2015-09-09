8f932f967317f58eef37b30c5bb508743ce39ba4
etiennemeleard <etiennemeleard@8555b757-d854-4b86-925a-82fd84c90ff4>
2015-09-09 15:31:41 +0000

    Make recipients / guests emails addresses clickable (mailto)
    Test #1196
    
    
    
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/branches/filesender-2.0@3715 8555b757-d854-4b86-925a-82fd84c90ff4

--- templates/guests_table.php
+++ templates/guests_table.php
@@ -21,7 +21,7 @@
         <?php foreach($guests as $guest) { ?>
         <tr class="guest" data-id="<?php echo $guest->id ?>" data-errors="<?php echo count($guest->errors) ? '1' : '' ?>">
             <td class="to">
-                <abbr title="<?php echo Template::sanitizeOutput($guest->email) ?>"><?php echo Template::sanitizeOutput($guest->name) ?></abbr>
+                <a href="mailto:<?php echo Template::sanitizeOutput($guest->email) ?>"><?php echo Template::sanitizeOutput($guest->email) ?></a>
                 
                 <?php if($guest->errors) echo '<br /><span class="errors">'.implode(', ', array_map(function($type) {
                     return Lang::tr('recipient_error_'.$type);
