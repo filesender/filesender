439ee80256e02536751153b66f18522895def590
etiennemeleard <etienne.meleard@renater.fr>
2016-06-20 13:28:47 +0000

    Fix SQL quotes
    git-svn-id: https://subversion.assembla.com/svn/file_sender/filesender/branches/filesender-2.0@3956 8555b757-d854-4b86-925a-82fd84c90ff4

--- classes/data/AuditLog.class.php
+++ classes/data/AuditLog.class.php
@@ -320,7 +320,7 @@ class AuditLog extends DBObject {
     public static function clientRecentlyDownloaded($recipient, $files_ids, $range = 3600) {
         // Get a single download for each file if it exists over the range
         $downloaded = self::all(
-            'target_type=\'File\' AND target_id IN :ids AND author_type="Recipient" AND author_id=:rcptid AND ip = :ip AND event="download_ended" AND created > :since GROUP BY target_id',
+            'target_type=\'File\' AND target_id IN :ids AND author_type=\'Recipient\' AND author_id=:rcptid AND ip = :ip AND event=\'download_ended\' AND created > :since GROUP BY target_id',
             array(
                 ':ids' => $files_ids,
                 ':rcptid' => $recipient->id,
