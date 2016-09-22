6d027f0734d1b4e450040dfc8897e8f7a94c96ff
Emiel Blonk <emiel@codebridge.nl>
2016-09-22 16:25:50 +0200

    Check returned data in dbtest

--- unittests/tests/dbs/DBsTest.php
+++ unittests/tests/dbs/DBsTest.php
@@ -59,6 +59,7 @@ class DBsTest extends CommonUnitTestCase {
             $statement = DBI::prepare('SELECT 1;');
             $statement->execute(['*']);
             $data = $statement->fetch();
+            echo "Database fetch data: " . print_r($data,true) . "\n";
             $this->assertTrue($data[1] == 1);
 
             $this->displayInfo(get_class(), __FUNCTION__, 'Connexion DB OK');
