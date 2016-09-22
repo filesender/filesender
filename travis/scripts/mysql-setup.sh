233abbdcdf227dbe247f02805fa49525f9c7b08b
Emiel Blonk <emiel@codebridge.nl>
2016-09-22 15:48:10 +0200

    Trying travis config multi db

--- travis/scripts/mysql-setup.sh
+++ travis/scripts/mysql-setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -ev
 mysql -e "create database IF NOT EXISTS filesender;" -uroot;
+mysql -e "UPDATE user SET password=PASSWORD('password') WHERE user='travis';" -uroot;
 php ./scripts/upgrade/database.php;
