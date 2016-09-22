d56eafae38a52ec6d792f1d6cff06851953906d1
Emiel Blonk <emiel@codebridge.nl>
2016-09-22 12:46:04 +0200

    changed / from includes and update to script path (wrong file)

--- unittests/tests/common/CommonPHPUnitConfigs.php
+++ unittests/tests/common/CommonPHPUnitConfigs.php
@@ -37,5 +37,5 @@
 
 date_default_timezone_set("Europe/Paris");
 
-require_once(dirname(__FILE__).'\..\..\..\classes\autoload.php');
-//require_once(dirname(__FILE__).'\..\..\..\classes\_includes.php');
+require_once(dirname(__FILE__).'/../../../classes/autoload.php');
+//require_once(dirname(__FILE__).'/../../../classes/_includes.php');
