0bdabbfb8fcf84276c95c424ae5d8384c5dd6067
Emiel Blonk <emiel@codebridge.nl>
2016-09-26 13:24:49 +0200

    Added debug info to simplesamlscript

--- travis/scripts/simplesamlphp-setup.sh
+++ travis/scripts/simplesamlphp-setup.sh
@@ -4,13 +4,17 @@ set -ev
 mkdir downloads
 cd downloads
 wget https://simplesamlphp.org/res/downloads/simplesamlphp-1.14.2.tar.gz --no-check-certificate
+ls
 # Extract it in a suitable directory and create symlink:
 cd ../
 mkdir opt
 cd opt
 tar xvzf ../downloads/simplesamlphp-1.14.2.tar.gz
+ls
 ln -s simplesamlphp-1.14.2/ simplesaml
+ls
 # Copy standard configuration files to the right places:
 cd simplesaml
 cp -r config-templates/*.php config/
 cp -r metadata-templates/*.php metadata/
+ls
