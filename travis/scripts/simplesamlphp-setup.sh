f6aebed6375d5119795faed4bbb826d629329339
Peter <peter@codebridge.nl>
2016-09-27 12:55:37 +0200

    Travis test fix

--- travis/scripts/simplesamlphp-setup.sh
+++ travis/scripts/simplesamlphp-setup.sh
@@ -10,7 +10,7 @@ SHA_DOWNLOAD_HASH=`sha256sum simplesamlphp-1.14.2.tar.gz`
 SHA_CHECK_HASH='19b849065cdc8b96d74570b2ef91a08e72d0a4c0d9c30fa9526163ff6684c83e  simplesamlphp-1.14.2.tar.gz'
 
 
-if [ $SHA_DOWNLOAD_HASH!=$SHA_CHECK_HASH ]; then
+if [ "$SHA_DOWNLOAD_HASH" != "$SHA_CHECK_HASH" ]; then
     echo "Hashes did not match!!"
     echo "Downloaded hash $SHA_DOWNLOAD_HASH"
     echo "Verification hash $SHA_CHECK_HASH"
