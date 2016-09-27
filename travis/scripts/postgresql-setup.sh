5df891d4228612324a152b5394d8aea8f97fb93a
Peter <peter@codebridge.nl>
2016-09-27 14:03:23 +0200

    Travis test even more debug

--- travis/scripts/postgresql-setup.sh
+++ travis/scripts/postgresql-setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -ev
+echo psql -c 'SELECT version();' -U postgres
 psql -c 'create database filesender;' -U postgres
 psql -U postgres -c "alter user postgres with password 'password';"
 php ./scripts/upgrade/database.php;
