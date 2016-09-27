78bfcb2e77cd2d45b9d1ae57e2ebbbe1f0d08389
Peter <peter@codebridge.nl>
2016-09-27 13:03:01 +0200

    Travis test fix

--- travis/scripts/simplesamlphp-setup.sh
+++ travis/scripts/simplesamlphp-setup.sh
@@ -6,7 +6,7 @@ cd /opt/filesender
 
 wget https://simplesamlphp.org/res/downloads/simplesamlphp-1.14.2.tar.gz --no-check-certificate
 
-SHA_DOWNLOAD_HASH=`sha256sum simplesamlphp-1.14.2.tar.gz`
+SHA_DOWNLOAD_HASH=$(sha256sum simplesamlphp-1.14.2.tar.gz)
 SHA_CHECK_HASH='19b849065cdc8b96d74570b2ef91a08e72d0a4c0d9c30fa9526163ff6684c83e  simplesamlphp-1.14.2.tar.gz'
 
 
@@ -15,6 +15,8 @@ if [ "$SHA_DOWNLOAD_HASH" != "$SHA_CHECK_HASH" ]; then
     echo "Downloaded hash $SHA_DOWNLOAD_HASH"
     echo "Verification hash $SHA_CHECK_HASH"
     exit 1
+else
+    echo "Hashes matched"
 fi
 
 tar xvzf simplesamlphp-1.14.2.tar.gz
