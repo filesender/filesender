1a425fbb3f23fe1d34b7d1f6b68634591c5f9811
madsi1m <michael.dsilva@outlook.com>
2016-11-22 10:46:00 +1100

    terrasender update progress bar, wasnt reporting on slow internets

--- www/js/upload_page.js
+++ www/js/upload_page.js
@@ -759,13 +759,13 @@ $(function() {
         $('#encryption_password_container').slideToggle();
         $('#encryption_password_container_generate').slideToggle();
         $('#encryption_password_show_container').slideToggle();
-	$('#encryption_description_container').slideToggle();
+        $('#encryption_description_container').slideToggle();
         return false;
     });
     filesender.ui.nodes.encryption.generate.on('click', function() {
         filesender.ui.nodes.encryption.password.val(Math.random().toString(36).substr(2, 14));
-        filesender.ui.nodes.encryption.show_hide.prop('checked', true);
-        filesender.ui.nodes.encryption.show_hide.trigger('change');
+	filesender.ui.nodes.encryption.show_hide.prop('checked',true);
+	filesender.ui.nodes.encryption.show_hide.trigger('change');
     });
     filesender.ui.nodes.encryption.show_hide.on('change', function() {
         if (filesender.ui.nodes.encryption.show_hide.is(':checked')) {
@@ -773,7 +773,7 @@ $(function() {
         } else {
             filesender.ui.nodes.encryption.password.attr('type','password');
         }
-        filesender.ui.nodes.encryption.password.stop().effect('highlight',{},500);
+	filesender.ui.nodes.encryption.password.stop().effect('highlight',{},500);
         return false;
     });
     
