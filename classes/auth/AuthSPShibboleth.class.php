2244a11bd536c1780e6b619471085d5570727bb8
monkeyiq <monkeyiq@users.sourceforge.net>
2017-09-16 17:08:14 +1000

    sysadmin: Help the sysadmin with missing SAML attributes (#127)
    * Help the sysadmin with missing SAML attributes
    
    * more info

--- classes/auth/AuthSPShibboleth.class.php
+++ classes/auth/AuthSPShibboleth.class.php
@@ -99,9 +99,13 @@ class AuthSPShibboleth {
             if(is_array($attributes['uid'])) $attributes['uid'] = array_shift($attributes['uid']);
             if(is_array($attributes['name'])) $attributes['name'] = array_shift($attributes['name']);
             
-            if(!$attributes['uid']) throw new AuthSPMissingAttributeException('uid');
+            if(!$attributes['uid'])
+                throw new AuthSPMissingAttributeException(
+                    'uid',$attributes,'uid_attribute',self::$config['uid_attribute']);
             
-            if(!$attributes['email']) throw new AuthSPMissingAttributeException('email');
+            if(!$attributes['email'])
+                throw new AuthSPMissingAttributeException(
+                     'email',$attributes,'email_attribute',self::$config['email_attribute']);
             
             foreach($attributes['email'] as $email) {
                 if(!Utilities::validateEmail($email)) throw new AuthSPBadAttributeException('email');
