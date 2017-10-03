cb32c277c8d1d2f90fdc22c4830cd5ba92069168
monkeyiq <monkeyiq@users.sourceforge.net>
2017-10-03 19:28:53 +1000

    SQL: upload graph working on postgresql as well (#149)

--- www/lib/graph/uploadGraph.php
+++ www/lib/graph/uploadGraph.php
@@ -4,28 +4,33 @@ require_once('../../../includes/init.php');
 
 $minSz = Config::get('upload_graph_bulk_min_file_size_to_consider');
 
-// FIXME:MYSQLONLY
-$statement = DBI::prepare(
-    'SELECT Days.Date, Speed.Speed, Speed.EnSpeed '
-  . 'FROM (SELECT (SELECT Date(NOW() - INTERVAL 30 DAY)) + INTERVAL a + b DAY Date '
+$encF = 'additional_attributes LIKE \'%\"encryption\":false%\'';
+$encT = 'additional_attributes LIKE \'%\"encryption\":true%\'';
+
+$sql =
+    'SELECT days.date, speed.speed, speed.enspeed '
+  . 'FROM (SELECT (SELECT Date(NOW() - INTERVAL \'30\' DAY)) + '
+                     . DBLayer::toIntervalDays("a+b") . ' date '
   . '        FROM (SELECT 0 a UNION SELECT 1 a UNION SELECT 2 UNION SELECT 3 UNION SELECT 4 '
   . '                     UNION SELECT 5 UNION SELECT 6 UNION SELECT 7 UNION SELECT 8 UNION SELECT 9 ) d, '
   . '             (SELECT 0 b UNION SELECT 10 UNION SELECT 20 UNION SELECT 30 UNION SELECT 40) m '
-  . '        WHERE (SELECT Date(NOW() - INTERVAL 30 DAY)) + INTERVAL a + b DAY <= (select date(now())) '
-  . '        ORDER BY a + b) as Days LEFT '
-  . ' JOIN (SELECT DATE(created) as Date, '
-  . '              AVG(IF(LOCATE("\"encryption\":true",additional_attributes)=0,size/time_taken,NULL)) as Speed, '
-  . '              AVG(IF(LOCATE("\"encryption\":true",additional_attributes)<>0,size/time_taken,NULL)) as EnSpeed, '
-  . '              COUNT(IF(LOCATE("\"encryption\":true",additional_attributes)=0,id,NULL)) as Count, '
-  . '              COUNT(IF(LOCATE("\"encryption\":true",additional_attributes)<>0,id,NULL)) as EnCount '
+  . '        WHERE (SELECT Date(NOW() - INTERVAL \'30\' DAY)) + '
+                            . DBLayer::toIntervalDays("a+b") . ' <= (select date(now())) '
+  . '        ORDER BY a + b) as days LEFT '
+  . ' JOIN (SELECT DATE(created) as date, '
+  . '   AVG(case WHEN time_taken > 0 AND ' . $encF . ' THEN size/time_taken ELSE null END) as speed, '
+  . '   AVG(case WHEN time_taken > 0 AND ' . $encT . ' THEN size/time_taken ELSE null END) as enspeed, '
+  . '   AVG(case WHEN ' . $encF . ' THEN id ELSE null END) as count, '
+  . '   AVG(case WHEN ' . $encT . ' THEN id ELSE null END) as encount '
   . '       from StatLogs '
-  . '      WHERE event="file_uploaded" '
-  . '            AND created>NOW() - INTERVAL 31 DAY '
+  . '      WHERE event=\'file_uploaded\' '
+  . '            AND created>NOW() - INTERVAL \'31\' DAY '
   . '            AND size > ' . $minSz . ' '
-  . '      GROUP BY Date) as Speed ON Days.Date=Speed.Date '
-  . ' ORDER BY Days.Date');
+  . '      GROUP BY Date) as speed on days.date=speed.date '
+  . ' ORDER BY days.date';
 
-$statement->execute(array(':id' => 'pointless'));
+$statement = DBI::prepare($sql);
+$statement->execute(array());
 $result = $statement->fetchAll();
 
 $data = array(
@@ -76,24 +81,24 @@ $lastEnSpeed=array(0);
 //$yMax = 0;
 foreach($result as $row) {
 
-    $row['Speed']=round($row['Speed']==0?(array_sum($lastSpeed)/count($lastSpeed)):($row['Speed']/1048576),2);
-    $row['EnSpeed']=round($row['EnSpeed']==0?(array_sum($lastEnSpeed)/count($lastEnSpeed)):($row['EnSpeed']/1048576),2);
+    $row['speed']=round($row['speed']==0?(array_sum($lastSpeed)/count($lastSpeed)):($row['speed']/1048576),2);
+    $row['enspeed']=round($row['enspeed']==0?(array_sum($lastEnSpeed)/count($lastEnSpeed)):($row['enspeed']/1048576),2);
 
-    if ($row['Speed']>0) { 
+    if ($row['speed']>0) { 
 	array_shift($lastSpeed);
-	$lastSpeed[]=$row['Speed'];
+	$lastSpeed[]=$row['speed'];
     }
-    if ($row['EnSpeed']>0) {
+    if ($row['enspeed']>0) {
 	array_shift($lastEnSpeed);
-	$lastEnSpeed[]=$row['EnSpeed'];
+	$lastEnSpeed[]=$row['enspeed'];
     }
 
-    if ($row['Speed']<=0) $row['Speed']=null;
-    if ($row['EnSpeed']<=0) $row['EnSpeed']=null;
+    if ($row['speed']<=0) $row['speed']=null;
+    if ($row['enspeed']<=0) $row['enspeed']=null;
 
-    $data['data']['labels'][]=$row['Date'];
-    $data['data']['datasets'][0]['data'][]=$row['EnSpeed'];
-    $data['data']['datasets'][1]['data'][]=$row['Speed'];
+    $data['data']['labels'][]=$row['date'];
+    $data['data']['datasets'][0]['data'][]=$row['enspeed'];
+    $data['data']['datasets'][1]['data'][]=$row['speed'];
     
     //	$yMax=max($yMax,$row['EnSpeed'],$row['Speed']);
 }
