3cbc9caa1540372ef9674966a1eef5aba80d3b2c
Ben Martin <monkeyiq@users.sourceforge.net>
2018-01-12 18:35:19 +1000

    offending, real calc

--- www/js/transfer.js
+++ www/js/transfer.js
@@ -77,7 +77,10 @@ window.filesender.progresstracker = function() {
         
         if( !tooSlow )
             return false;
-        
+
+        if( (new Date()).getTime() - this.stamp > (tooSlow*1000) )
+            return true;
+
         return false;
     };
 
