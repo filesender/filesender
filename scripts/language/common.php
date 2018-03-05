cb79d7754189d5556d9d02009eb571f03b8bc7a4
monkeyiq <monkeyiq@tpg.com.au>
2018-03-05 11:51:02 +1000

    i18n: import script updates (#270)

--- scripts/language/common.php
+++ scripts/language/common.php
@@ -10,13 +10,15 @@ function loadLang( $code ) {
     foreach(explode("\n", file_get_contents("$BASE/language/$code/lang.php")) as $line) {
         $n++;
         
-        if(!preg_match('`\$lang\[[\'"]([^\'"]+)[\'"]\]\s*=`', $line, $m)) continue;
+        if(!preg_match('`\$lang\[[\'"]([^\'"]+)[\'"]\]\s*=\s*[\'"](.*)[\'"];`', $line, $m)) continue;
         $kn++;
-        
+        $k = $m[1];
+        $v = $m[2];
+        echo "$k \n";
         if(array_key_exists($m[1], $keys)) {
             echo $m[1].' ('.$n.') already found at '.$keys[$m[1]]."\n";
         } else {
-            $keys[$m[1]] = $n;
+            $keys[$m[1]] = $v;
         }
     }
     return $keys;
@@ -40,4 +42,40 @@ function loadLangDirectory( $code ) {
         }
     }
     return $keys;
+}
+
+
+function squote( $s ) {
+    $s = str_replace( '\\', '\\\\', $s );
+    $s = str_replace( "'", "\'", $s );
+    return $s;
+}
+
+function write_translation_lang_file( $code, $lang ) {
+    global $BASE;
+    $fn = "$BASE/language/$code/lang.php";
+    $data = '';
+
+    ksort( $lang );
+    foreach( $lang as $k => $v ) {
+        $data .= "\$lang['" . $k . "'] = '" . squote($v) . "';\n";
+    }
+
+    echo "writing general translations to file at $fn \n";
+    file_put_contents( $fn, "<?php\n" . $data );
+    
+}
+
+
+function write_translation_term_file( $code, $term, $data ) {
+    global $BASE;
+    $p = "$BASE/language/$code/";
+    $fn = '';
+    foreach( array('html','mail','text') as $type ) {
+        if( file_exists( "$BASE/language/master/$term.$type.php" )) {
+            $fn = "$p/$term.$type.php";
+        }
+    }
+    echo "translation file for $term at $fn \n";
+    file_put_contents( $fn, $data );
 }
