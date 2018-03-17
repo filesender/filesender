96c6a7a46aee22619ff64fd3bb8fbf021e9276df
Ben Martin <monkeyiq@users.sourceforge.net>
2018-03-17 12:54:02 +1000

    cleanup prints

---
+++ scripts/cloud/azure/azure-simpleblob-benchmark.php
@@ -0,0 +1,84 @@
+<?php
+
+/*
+ * FileSender www.filesender.org
+ * 
+ * Copyright (c) 2009-2014, AARNet, Belnet, HEAnet, SURFnet, UNINETT
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * *	Redistributions of source code must retain the above copyright
+ * 	notice, this list of conditions and the following disclaimer.
+ * *	Redistributions in binary form must reproduce the above copyright
+ * 	notice, this list of conditions and the following disclaimer in the
+ * 	documentation and/or other materials provided with the distribution.
+ * *	Neither the name of AARNet, Belnet, HEAnet, SURFnet and UNINETT nor the
+ * 	names of its contributors may be used to endorse or promote products
+ * 	derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS'
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+require_once dirname(__FILE__).'/../../../includes/init.php';
+require_once dirname(__FILE__).'/../../../optional-dependencies/azure/vendor/autoload.php';
+
+use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
+use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
+use MicrosoftAzure\Storage\Blob\Models\CreateBlobOptions;
+use MicrosoftAzure\Storage\Blob\Models\GetBlobOptions;
+use MicrosoftAzure\Storage\Common\Exceptions\ServiceException;
+use MicrosoftAzure\Storage\Common\Exceptions\InvalidArgumentTypeException;
+use MicrosoftAzure\Storage\Common\Internal\Resources;
+use MicrosoftAzure\Storage\Common\Internal\StorageServiceSettings;
+use MicrosoftAzure\Storage\Common\Models\RetentionPolicy;
+use MicrosoftAzure\Storage\Common\Models\ServiceProperties;
+use MicrosoftAzure\Storage\Common\SharedAccessSignatureHelper;
+use MicrosoftAzure\Storage\Common\ServicesBuilder;
+
+
+$connectionString = Config::get('cloud_azure_connection_string');
+$containerName    = 'filesender-test-container';
+
+$s = microtime(true);
+
+$blobClient = ServicesBuilder::getInstance()->createBlobService($connectionString);
+
+$e = microtime(true);
+echo "Connecting to Azure server took " . round($e-$s,2) . " seconds\n";
+
+
+
+echo "performance testing...\n";
+
+ini_set("memory_limit","1024M");
+foreach(  array(1,5,10,50,100,200) as $mb ) {
+
+    $fn = uniqid();
+
+    $str = str_repeat('0',$mb*1024*1024);
+    $s = microtime(true);
+    {
+        $blobClient->createBlockBlob( $containerName, $fn, $str );
+    }
+    $e = microtime(true);
+    $tt = $e-$s;
+    printf("%10.1f mb/s to write %5.1f mb to server, total time needed was %3.1f seconds\n",
+           $mb/$tt, $mb, $tt);
+
+    
+}
+
+
+?>
+
