e6b2214cf6ddc47f753a54ccbffed13a40f659d0
Ben Martin <monkeyiq@users.sourceforge.net>
2018-03-18 14:33:45 +1000

    i38n: sort all the remaining lang.php files

--- language/it_IT/lang.php
+++ language/it_IT/lang.php
@@ -1,93 +1,43 @@
 <?php
-
-/*
- * FileSender www.filesender.org
- * 
- * Copyright (c) 2009-2012, AARNet, Belnet, HEAnet, SURFnet, UNINETT
- * All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- * 
- * *	Redistributions of source code must retain the above copyright
- * 	notice, this list of conditions and the following disclaimer.
- * *	Redistributions in binary form must reproduce the above copyright
- * 	notice, this list of conditions and the following disclaimer in the
- * 	documentation and/or other materials provided with the distribution.
- * *	Neither the name of AARNet, Belnet, HEAnet, SURFnet and UNINETT nor the
- * 	names of its contributors may be used to endorse or promote products
- * 	derived from this software without specific prior written permission.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-/* --------------------------------------------------------------
- * it_IT Language File
- * Contributed by 
- * 		Fabio Spelta - Sistemi Informativi, Università degli Studi di Milano-Bicocca
- * 		Riccardo Valzorio - CILEA
- * --------------------------------------------------------------
- */
-$lang['logon'] = 'Login';
-
 $lang['about'] = 'Informazioni';
-$lang['help'] = 'Aiuto';
-$lang['download'] = 'Download';
-
-$lang['files'] = 'File';
-
-$lang['downloads'] = 'File caricati';
-
-$lang['to'] = 'A';
-$lang['from'] = 'Da';
-$lang['size'] = 'Dimensione';
-$lang['created'] = 'Data di creazione';
-$lang['subject'] = 'Oggetto';
-$lang['message'] = 'Messaggio';
-
-$lang['showhide'] = 'Mostra/Nascondi';
-
 $lang['cancel'] = 'Elimina';
 $lang['close'] = 'Chiudi';
-$lang['ok'] = 'OK';
-$lang['send'] = 'Invia';
+$lang['created'] = 'Data di creazione';
 $lang['delete'] = 'Elimina';
-$lang['yes'] = 'Sì';
-$lang['no'] = 'No';
-$lang['invalid_file'] = 'File non valido';
-$lang['select_file'] = 'Seleziona il tuo file.';
-
-
-$lang['email_separator_msg'] = 'Indirizzi di posta elettronica separati da , o ;';
-$lang['email_sent'] = 'Messaggio Inviato';
-$lang['expiry_date'] = 'Data di scadenza';
-$lang['send_voucher'] = 'Invia voucher';
-
-
-$lang['optional'] = 'opzionale';
-
-
+$lang['download'] = 'Download';
+$lang['downloads'] = 'File caricati';
 $lang['dp_close_text'] = 'Fatto';
-$lang['dp_prev_text'] = 'Prec';
-$lang['dp_next_text'] = 'Prox';
 $lang['dp_current_text'] = 'Oggi';
-$lang['dp_month_names'] = 'gennaio,febbraio,marzo,aprile,maggio,giugno,luglio,agosto,settembre,ottobre,novembre,dicembre';
-$lang['dp_month_names_short'] = 'gen,feb,mar,apr,mag,giu,lug,ago,set,ott,nov,dic';
-$lang['dp_day_names'] = 'domenica,lunedì,martedì,mercoledì,giovedì,venerdì,sabato';
-$lang['dp_day_names_short'] = 'dom,lun,mar,mer,gio,ven,sab';
-$lang['dp_day_names_min'] = 'do,lu,ma,me,gi,ve,sa';
-$lang['dp_week_header'] = 'Wk';
 $lang['dp_date_format'] = 'dd/mm/yy';
+$lang['dp_day_names'] = 'domenica,lunedì,martedì,mercoledì,giovedì,venerdì,sabato';
+$lang['dp_day_names_min'] = 'do,lu,ma,me,gi,ve,sa';
+$lang['dp_day_names_short'] = 'dom,lun,mar,mer,gio,ven,sab';
 $lang['dp_first_day'] = '1';
 $lang['dp_is_rtl'] = 'false';
+$lang['dp_month_names'] = 'gennaio,febbraio,marzo,aprile,maggio,giugno,luglio,agosto,settembre,ottobre,novembre,dicembre';
+$lang['dp_month_names_short'] = 'gen,feb,mar,apr,mag,giu,lug,ago,set,ott,nov,dic';
+$lang['dp_next_text'] = 'Prox';
+$lang['dp_prev_text'] = 'Prec';
 $lang['dp_show_month_after_year'] = 'false';
+$lang['dp_week_header'] = 'Wk';
 $lang['dp_year_suffix'] = '';
+$lang['email_sent'] = 'Messaggio Inviato';
+$lang['email_separator_msg'] = 'Indirizzi di posta elettronica separati da , o ;';
+$lang['expiry_date'] = 'Data di scadenza';
+$lang['files'] = 'File';
+$lang['from'] = 'Da';
+$lang['help'] = 'Aiuto';
+$lang['invalid_file'] = 'File non valido';
+$lang['logon'] = 'Login';
+$lang['message'] = 'Messaggio';
+$lang['no'] = 'No';
+$lang['ok'] = 'OK';
+$lang['optional'] = 'opzionale';
+$lang['select_file'] = 'Seleziona il tuo file.';
+$lang['send'] = 'Invia';
+$lang['send_voucher'] = 'Invia voucher';
+$lang['showhide'] = 'Mostra/Nascondi';
+$lang['size'] = 'Dimensione';
+$lang['subject'] = 'Oggetto';
+$lang['to'] = 'A';
+$lang['yes'] = 'Sì';
