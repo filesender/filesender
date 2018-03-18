5f332d9841f20d13cbe2db489a7c0e9b3880084d
monkeyiq <monkeyiq@tpg.com.au>
2018-03-18 14:51:27 +1000

    i38n: sort all the remaining lang.php files (#278)

--- language/nl_NL/lang.php
+++ language/nl_NL/lang.php
@@ -1,101 +1,49 @@
 <?php
-
-/*
- * FileSender www.filesender.org
- * 
- * Copyright (c) 2009-2012, AARNet, Belnet, HEAnet, SURFnet, UNINETT
- * All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- * 
- * *	Redistributions of source code must retain the above copyright
- * 	notice, this list of conditions and the following disclaimer.
- * *	Redistributions in binary form must reproduce the above copyright
- * 	notice, this list of conditions and the following disclaimer in the
- * 	documentation and/or other materials provided with the distribution.
- * *	Neither the name of AARNet, Belnet, HEAnet, SURFnet and UNINETT nor the
- * 	names of its contributors may be used to endorse or promote products
- * 	derived from this software without specific prior written permission.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-/* ---------------------------------
- * nl_NL Language File
- * Maintained by the FileSender Core Team
- * ---------------------------------
- * 
- */
-$lang['logon'] = 'Aanmelden';
-
 $lang['about'] = 'Over';
-$lang['help'] = 'Help';
-$lang['download'] = 'Download';
-
-
-$lang['files'] = 'Bestanden';
-
-$lang['downloads'] = 'Downloads';
-
-$lang['to'] = 'Aan';
-$lang['from'] = 'Van';
-$lang['size'] = 'Grootte';
+$lang['cancel'] = 'Annuleer';
+$lang['close'] = 'Sluit';
 $lang['created'] = 'Aangemaakt';
-$lang['subject'] = 'Onderwerp';
+$lang['delete'] = 'Verwijder';
+$lang['download'] = 'Download';
+$lang['downloads'] = 'Downloads';
+$lang['dp_close_text'] = 'Sluiten';
+$lang['dp_current_text'] = 'Vandaag';
+$lang['dp_date_format'] = 'dd-mm-yy';
+$lang['dp_day_names'] = 'zondag,maandag,dinsdag,woensdag,donderdag,vrijdag,zaterdag';
+$lang['dp_day_names_min'] = 'zo,ma,di,wo,do,vr,za';
+$lang['dp_day_names_short'] = 'zon,maa,din,woe,don,vri,zat';
+$lang['dp_first_day'] = '1';
+$lang['dp_is_rtl'] = 'false';
+$lang['dp_month_names'] = 'januari,februari,maart,april,mei,juni,juli,augustus,september,oktober,november,december';
+$lang['dp_month_names_short'] = 'jan,feb,mrt,apr,mei,jun,jul,aug,sep,okt,nov,dec';
+$lang['dp_next_text'] = '→';
+$lang['dp_prev_text'] = '←';
+$lang['dp_show_month_after_year'] = 'false';
+$lang['dp_week_header'] = 'Wk';
+$lang['dp_year_suffix'] = '';
+$lang['email_sent'] = 'E-mail verstuurd';
+$lang['email_separator_msg'] = 'Meerdere e-mailadressen gescheiden door, of ;';
+$lang['expiry_date'] = 'Vervaldatum';
 $lang['file_encryption'] = 'Bestands encryptie';
+$lang['file_encryption_enter_password'] = 'Geef een wachtwoord op';
+$lang['file_encryption_generate_password'] = 'Genereer wachtwoord';
+$lang['file_encryption_need_password'] = 'Zonder wachtwoord kan dit bestand niet worden geopend';
 $lang['file_encryption_password'] = 'Wachtwoord';
 $lang['file_encryption_show_password'] = 'Toon / Verberg wachtwoord';
 $lang['file_encryption_wrong_password'] = 'Foutief wachtwoord';
-$lang['file_encryption_enter_password'] = 'Geef een wachtwoord op';
-$lang['file_encryption_need_password'] = 'Zonder wachtwoord kan dit bestand niet worden geopend';
-$lang['file_encryption_generate_password'] = 'Genereer wachtwoord';
-$lang['optional'] = 'Optioneel';
-$lang['expiry_date'] = 'Vervaldatum';
-
-$lang['showhide'] = 'Toon/Verberg';
-$lang['select_file'] = 'Selecteer een bestand om te uploaden';
-$lang['email_separator_msg'] = 'Meerdere e-mailadressen gescheiden door, of ;';
-
-
-
-$lang['send_voucher'] = 'Stuur uitnodiging';
-
-$lang['cancel'] = 'Annuleer';
-$lang['close'] = 'Sluit';
-$lang['ok'] = 'OK';
-$lang['send'] = 'Verzend';
-$lang['delete'] = 'Verwijder';
-$lang['yes'] = 'Ja';
-$lang['no'] = 'Nee';
-
+$lang['files'] = 'Bestanden';
+$lang['from'] = 'Van';
+$lang['help'] = 'Help';
 $lang['invalid_file'] = 'Ongeldig bestand';
-
-$lang['email_sent'] = 'E-mail verstuurd';
-
-
-
-$lang['dp_close_text'] = 'Sluiten';
-$lang['dp_prev_text'] = '←';
-$lang['dp_next_text'] = '→';
-$lang['dp_current_text'] = 'Vandaag';
-$lang['dp_month_names'] = 'januari,februari,maart,april,mei,juni,juli,augustus,september,oktober,november,december';
-$lang['dp_month_names_short'] = 'jan,feb,mrt,apr,mei,jun,jul,aug,sep,okt,nov,dec';
-$lang['dp_day_names'] = 'zondag,maandag,dinsdag,woensdag,donderdag,vrijdag,zaterdag';
-$lang['dp_day_names_short'] = 'zon,maa,din,woe,don,vri,zat';
-$lang['dp_day_names_min'] = 'zo,ma,di,wo,do,vr,za';
-$lang['dp_week_header'] = 'Wk';
-$lang['dp_date_format'] = 'dd-mm-yy';
-$lang['dp_first_day'] = '1';
-$lang['dp_is_rtl'] = 'false';
-$lang['dp_show_month_after_year'] = 'false';
-$lang['dp_year_suffix'] = '';
+$lang['logon'] = 'Aanmelden';
+$lang['no'] = 'Nee';
+$lang['ok'] = 'OK';
+$lang['optional'] = 'Optioneel';
+$lang['select_file'] = 'Selecteer een bestand om te uploaden';
+$lang['send'] = 'Verzend';
+$lang['send_voucher'] = 'Stuur uitnodiging';
+$lang['showhide'] = 'Toon/Verberg';
+$lang['size'] = 'Grootte';
+$lang['subject'] = 'Onderwerp';
+$lang['to'] = 'Aan';
+$lang['yes'] = 'Ja';
--- language/sl_SI/lang.php
+++ language/sl_SI/lang.php
@@ -1,92 +1,43 @@
 <?php
-
-/*
- * FileSender www.filesender.org
- * 
- * Copyright (c) 2009-2012, AARNet, Belnet, HEAnet, SURFnet, UNINETT
- * All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- * 
- * *	Redistributions of source code must retain the above copyright
- * 	notice, this list of conditions and the following disclaimer.
- * *	Redistributions in binary form must reproduce the above copyright
- * 	notice, this list of conditions and the following disclaimer in the
- * 	documentation and/or other materials provided with the distribution.
- * *	Neither the name of AARNet, Belnet, HEAnet, SURFnet and UNINETT nor the
- * 	names of its contributors may be used to endorse or promote products
- * 	derived from this software without specific prior written permission.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-/* ------------------------------------------------
- * sl_SI Language File
- * Contributed and maintained by Andrej Bagon/Arnes
- * ------------------------------------------------
- * 
- */
-$lang['logon'] = 'Prijava';
-
 $lang['about'] = 'O storitvi';
-$lang['help'] = 'Pomoč';
-$lang['download'] = 'Prenos';
-
-$lang['files'] = 'Dokumenti';
-
-$lang['downloads'] = 'Prenosi';
-
-$lang['to'] = 'Za';
-$lang['from'] = 'Od';
-$lang['size'] = 'Velikost';
-$lang['created'] = 'Datum kreiranja';
-$lang['subject'] = 'Zadeva';
-$lang['message'] = 'Sporočilo';
-
-$lang['showhide'] = 'Prikaži/Skrij';
-
 $lang['cancel'] = 'Prekliči';
 $lang['close'] = 'Zapri';
-$lang['ok'] = 'V redu';
-$lang['send'] = 'Pošlji';
+$lang['created'] = 'Datum kreiranja';
 $lang['delete'] = 'Izbriši';
-$lang['yes'] = 'Da';
-$lang['no'] = 'Ne';
-$lang['invalid_file'] = 'Neveljaven dokument';
-$lang['select_file'] = 'Izberite dokument';
-
-
-$lang['email_separator_msg'] = 'Ob navedbi več naslovnikov, elektronske naslove ločite z , ali ;';
-$lang['email_sent'] = 'Sporočilo je poslano';
-$lang['expiry_date'] = 'Datum poteka veljavnosti';
-$lang['send_voucher'] = 'Pošlji vavčer';
-
-
-$lang['optional'] = 'izbirno';
-
-
+$lang['download'] = 'Prenos';
+$lang['downloads'] = 'Prenosi';
 $lang['dp_close_text'] = 'Dokončano';
-$lang['dp_prev_text'] = 'Nazaj';
-$lang['dp_next_text'] = 'Naprej';
 $lang['dp_current_text'] = 'Danes';
-$lang['dp_month_names'] = 'Januar,Februar,Marec,April,Maj,Junij,Julij,Avgust,September,Oktober,November,December';
-$lang['dp_month_names_short'] = 'Jan,Feb,Mar,Apr,Maj,Jun,Jul,Aug,Sep,Okt,Nov,Dec';
-$lang['dp_day_names'] = 'Nedelja,Ponedeljek,Torek,Sreda,Četrtek,Petek,Sobota';
-$lang['dp_day_names_short'] = 'Ned,Pon,Tor,Sre,Čet,Pet,Sob';
-$lang['dp_day_names_min'] = 'Ne,Po,To,Sr,Če,Pe,So';
-$lang['dp_week_header'] = 'Wk';
 $lang['dp_date_format'] = 'dd/mm/yy';
+$lang['dp_day_names'] = 'Nedelja,Ponedeljek,Torek,Sreda,Četrtek,Petek,Sobota';
+$lang['dp_day_names_min'] = 'Ne,Po,To,Sr,Če,Pe,So';
+$lang['dp_day_names_short'] = 'Ned,Pon,Tor,Sre,Čet,Pet,Sob';
 $lang['dp_first_day'] = '1';
 $lang['dp_is_rtl'] = 'false';
+$lang['dp_month_names'] = 'Januar,Februar,Marec,April,Maj,Junij,Julij,Avgust,September,Oktober,November,December';
+$lang['dp_month_names_short'] = 'Jan,Feb,Mar,Apr,Maj,Jun,Jul,Aug,Sep,Okt,Nov,Dec';
+$lang['dp_next_text'] = 'Naprej';
+$lang['dp_prev_text'] = 'Nazaj';
 $lang['dp_show_month_after_year'] = 'false';
+$lang['dp_week_header'] = 'Wk';
 $lang['dp_year_suffix'] = '';
+$lang['email_sent'] = 'Sporočilo je poslano';
+$lang['email_separator_msg'] = 'Ob navedbi več naslovnikov, elektronske naslove ločite z , ali ;';
+$lang['expiry_date'] = 'Datum poteka veljavnosti';
+$lang['files'] = 'Dokumenti';
+$lang['from'] = 'Od';
+$lang['help'] = 'Pomoč';
+$lang['invalid_file'] = 'Neveljaven dokument';
+$lang['logon'] = 'Prijava';
+$lang['message'] = 'Sporočilo';
+$lang['no'] = 'Ne';
+$lang['ok'] = 'V redu';
+$lang['optional'] = 'izbirno';
+$lang['select_file'] = 'Izberite dokument';
+$lang['send'] = 'Pošlji';
+$lang['send_voucher'] = 'Pošlji vavčer';
+$lang['showhide'] = 'Prikaži/Skrij';
+$lang['size'] = 'Velikost';
+$lang['subject'] = 'Zadeva';
+$lang['to'] = 'Za';
+$lang['yes'] = 'Da';
