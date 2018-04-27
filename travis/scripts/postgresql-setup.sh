ffc6dd632f01eae45519f407320d82349def27e3
Geoffrey L. Brimhall <glbrimhall@email.arizona.edu>
2018-04-27 14:04:09 -0700

    Rolled back accidental checkin of postgresql-setup.sh

--- travis/scripts/postgresql-setup.sh
+++ travis/scripts/postgresql-setup.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-set -x
+set -ev
 # psql -c 'SELECT version();' -U postgres
-export PGPASSWORD=password
-
-psql -c 'create database filesender;'     -h db-host   -U postgres
-psql -U postgres -h db-host -c "alter user postgres with password 'password';"
+psql -c 'create database filesender;'        -U postgres
+psql -U postgres -c "alter user postgres with password 'password';"
 php ./scripts/upgrade/database.php;
 
-psql -c 'create database filesenderdataset;' -h db-host -U postgres
-bzcat ./scripts/dataset/dumps/filesender-2.0beta1.pg.bz2 | psql -h db-host -U postgres -d filesenderdataset 
+psql -c 'create database filesenderdataset;' -U postgres
+bzcat ./scripts/dataset/dumps/filesender-2.0beta1.pg.bz2 | psql -d filesenderdataset 
