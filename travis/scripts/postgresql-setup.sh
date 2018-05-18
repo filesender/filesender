c64427b81d26818771c07b94b6c21073ee6801d7
Ben Martin <monkeyiq@users.sourceforge.net>
2018-05-19 08:24:33 +1000

    ci: move the database migration generic code calls up

--- travis/scripts/postgresql-setup.sh
+++ travis/scripts/postgresql-setup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+#
+# These are only the commands that are specific to postgresql which need
+# to be executed before the scripts/upgrade/database.php can be used
+#
 set -ev
 # psql -c 'SELECT version();' -U postgres
 psql -c 'create database filesender;'        -U postgres
 psql -U postgres -c "alter user postgres with password 'password';"
-php ./scripts/upgrade/database.php;
 
 psql -c 'create database filesenderdataset;' -U postgres
 bzcat ./scripts/dataset/dumps/filesender-2.0beta1.pg.bz2 | psql -d filesenderdataset 
+
