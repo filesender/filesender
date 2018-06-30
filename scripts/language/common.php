115d3e0f5188e159a3508863e7cd17ac6e2566b3
monkeyiq <monkeyiq@tpg.com.au>
2018-06-30 12:13:19 +1000

    lang: sorted new terms (#352)
    * lang: sorted new terms
    
    * lang: make that comment a comment

--- scripts/language/common.php
+++ scripts/language/common.php
@@ -2,15 +2,15 @@
 
 $BASE = __DIR__ . '/../../';
 
-$warningAboutChangingFile = "\n";
-$warningAboutChangingFile .= " WARNING, this is a read only file created by import scripts\n";
-$warningAboutChangingFile .= " WARNING\n";
-$warningAboutChangingFile .= " WARNING,  Changes made to this file will be clobbered\n";
-$warningAboutChangingFile .= " WARNING\n";
-$warningAboutChangingFile .= " WARNING,  Please make changes on poeditor instead of here\n";
-$warningAboutChangingFile .= "\n";
-$warningAboutChangingFile .= "\n";
-$warningAboutChangingFile .= "\n";
+$warningAboutChangingFile = "<?php \n";
+$warningAboutChangingFile .= "// WARNING, this is a read only file created by import scripts\n";
+$warningAboutChangingFile .= "// WARNING\n";
+$warningAboutChangingFile .= "// WARNING,  Changes made to this file will be clobbered\n";
+$warningAboutChangingFile .= "// WARNING\n";
+$warningAboutChangingFile .= "// WARNING,  Please make changes on poeditor instead of here\n";
+$warningAboutChangingFile .= "// \n";
+$warningAboutChangingFile .= "// \n";
+$warningAboutChangingFile .= "?>\n";
 
 function loadLang( $code ) {
     global $BASE;
@@ -56,13 +56,15 @@ function loadLangDirectory( $code ) {
 
 
 function squote( $s ) {
-    $s = str_replace( '\\', '\\\\', $s );
-    $s = str_replace( "'", "\'", $s );
+
+    $s = preg_replace( '![^\\\\]\\\\[^\\\\\']!' , '\\\\' , $s );
+    $s = preg_replace( "![^\\\\]\'!", "\\\'", $s );
     return $s;
 }
 
 function write_translation_lang_file( $code, $lang ) {
     global $BASE;
+    global $warningAboutChangingFile;
     $fn = "$BASE/language/$code/lang.php";
     $data = '';
 
@@ -79,6 +81,7 @@ function write_translation_lang_file( $code, $lang ) {
 
 function write_translation_term_file( $code, $term, $data ) {
     global $BASE;
+    global $warningAboutChangingFile;
     $p = "$BASE/language/$code/";
     $fn = '';
     foreach( array('html','mail','text') as $type ) {
