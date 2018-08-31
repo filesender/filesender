18a0cbb4383c30c21232e3251b90be179b387af0
Ben Martin <monkeyiq@users.sourceforge.net>
2018-08-31 20:00:08 +1000

    quotes

--- travis/scripts/mariadb-setup.sh
+++ travis/scripts/mariadb-setup.sh
@@ -5,13 +5,13 @@
 #
 set -ev
 
-mysql -u root -e "CREATE DATABASE `filesender` DEFAULT CHARACTER SET utf8mb4;"
+mysql -u root -e "CREATE DATABASE filesender DEFAULT CHARACTER SET utf8mb4;"
 mysql -u root -e "GRANT USAGE ON *.* TO 'filesender'@'localhost' IDENTIFIED BY 'password';"
-mysql -u root -e "GRANT DROP, CREATE, CREATE VIEW, ALTER, SELECT, INSERT, INDEX, UPDATE, DELETE ON `filesender`.* TO 'filesender'@'localhost';"
+mysql -u root -e "GRANT DROP, CREATE, CREATE VIEW, ALTER, SELECT, INSERT, INDEX, UPDATE, DELETE ON filesender.* TO 'filesender'@'localhost';"
 
 
-mysql -u root -e "CREATE DATABASE `filesenderdataset` DEFAULT CHARACTER SET utf8mb4;"
-mysql -u root -e "GRANT DROP, CREATE, CREATE VIEW, ALTER, SELECT, INSERT, INDEX, UPDATE, DELETE ON `filesenderdataset`.* TO 'filesender'@'localhost';"
+mysql -u root -e "CREATE DATABASE filesenderdataset DEFAULT CHARACTER SET utf8mb4;"
+mysql -u root -e "GRANT DROP, CREATE, CREATE VIEW, ALTER, SELECT, INSERT, INDEX, UPDATE, DELETE ON filesenderdataset.* TO 'filesender'@'localhost';"
 mysql -u root -e "FLUSH PRIVILEGES;"
 
 
