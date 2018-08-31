edf7d06b79a7bd8ea3b12f40bacf52fc8c73445a
monkeyiq <monkeyiq@tpg.com.au>
2018-08-31 22:14:12 +1000

    ci: do the tests against mariadb as well as postgresql (#407)
    * ci: do the tests against mariadb as well as postgresql
    
    * move to plain mysql cmds
    
    * quotes
    
    * matching
    
    * sauce seems to give 500 errors, try the rest for now
    
    * sauce is back john, so lets add that again
    
    * ok, do both mariadb and postgresql before merge

--- travis/scripts/mysql-setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-# These are only the commands that are specific to mysql/mariadb which need
-# to be executed before the scripts/upgrade/database.php can be used
-#
-set -ev
-mysql -e "create database IF NOT EXISTS filesender;" -uroot;
-mysql -e "UPDATE user SET password=PASSWORD('password') WHERE user='travis';" -uroot;
