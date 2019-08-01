682e571f307d23d8864499562dfd15dc612ee3fc
Ben Martin <monkeyiq@users.sourceforge.net>
2019-08-01 14:28:51 +1000

    hush

--- unittests/selenium/SeleniumTest.php
+++ unittests/selenium/SeleniumTest.php
@@ -110,12 +110,14 @@ class SeleniumTest extends Sauce\Sausage\WebDriverTestCase
         if (!defined('SAUCE_ACCESS_KEY')) {
             define('SAUCE_ACCESS_KEY', getenv('SAUCE_ACCESS_KEY'));
         }
-                
-        if(getenv('SAUCE_DONT_VERIFY_CERTS')) {
-            $env_sauce_dont_verify_certify = getenv('SAUCE_DONT_VERIFY_CERTS');
-            define('SAUCE_VERIFY_CERTS', empty($env_sauce_dont_verify_certify));
-        } else {
-            define('SAUCE_VERIFY_CERTS', true);
+
+        if (!defined('SAUCE_VERIFY_CERTS')) {
+            if(getenv('SAUCE_DONT_VERIFY_CERTS')) {
+                $env_sauce_dont_verify_certify = getenv('SAUCE_DONT_VERIFY_CERTS');
+                define('SAUCE_VERIFY_CERTS', empty($env_sauce_dont_verify_certify));
+            } else {
+                define('SAUCE_VERIFY_CERTS', true);
+            }
         }
 
         $this->start_url = Config::get('site_url');
