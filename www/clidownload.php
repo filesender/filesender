e29d19c6568f3a390dc298e82d56577f04bd33be
peter <peter-@users.noreply.github.com>
2019-11-06 18:08:26 +0100

    Replace days valid w/ configured value
    without making assumptions about existing/default values (hence the regex).

--- www/clidownload.php
+++ www/clidownload.php
@@ -36,8 +36,10 @@ $one=1;
 $script=file_get_contents('../scripts/client/filesender.py');
 
 $apipath=Config::get('site_url').'rest.php';
+$daysvalid=Config::get('default_transfer_days_valid');
 
 $script=str_replace('[base_url]',$apipath,$script,$one);
+$script=preg_replace('/(default_transfer_days_valid).*/', '$1 = '.$daysvalid,$script);
 
 header('Content-Description: File Transfer');
 header('Content-Type: application/octet-stream');
