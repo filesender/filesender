01013f8a9b81f3fbc5f3e7152d841d40f2950ee1
Phaze-III <xj.dropbox+github@gmail.com>
2019-11-08 15:38:20 +0100

    i18n: Put Estonian in et_EE directory (#675)

--- scripts/language/import-langs-directory.sh
+++ scripts/language/import-langs-directory.sh
@@ -31,7 +31,7 @@ importfile cs_CZ FileSender_2.0_Czech.php
 importfile da_DK FileSender_2.0_Danish.php
 importfile nl_NL FileSender_2.0_Dutch.php
 importfile en_AU FileSender_2.0_English_AU.php
-importfile es_ES FileSender_2.0_Estonian.php
+importfile et_EE FileSender_2.0_Estonian.php
 importfile fi_FI FileSender_2.0_Finnish.php
 importfile de_DE FileSender_2.0_German.php
 importfile it_IT FileSender_2.0_Italian.php
