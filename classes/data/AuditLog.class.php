d47960757bc0132074cfe27b1bb99eeff967be26
Michael D'Silva <michael.dsilva@aarnet.edu.au>
2020-03-03 11:50:14 +1100

    Strip out ::ffff: from ipv4 addresses when in ipv6 mode

--- classes/data/AuditLog.class.php
+++ classes/data/AuditLog.class.php
@@ -232,9 +232,13 @@ class AuditLog extends DBObject
             'author_type',
             'author_id',
             'created',
-            'ip',
-        ))) {
+         ))) {
             return $this->$property;
+        }
+ 
+        if ($property == 'ip') {
+            //Strip out ::ffff: from ipv4 addresses when in ipv6 mode
+            return preg_replace('/^::ffff:([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)$/', '$1', $this->ip);
         }
         
         if ($property == 'target') {
