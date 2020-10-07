1d2a8286fe9f26fb0897228c8579c324805827f8
Ben Martin <monkeyiq@users.sourceforge.net>
2020-10-07 14:28:35 +1000

    add these too

---
+++ language/de_DE/local_authdb_password_reminder.mail.php
@@ -0,0 +1,40 @@
+<?php 
+// WARNING, this is a read only file created by import scripts
+// WARNING
+// WARNING,  Changes made to this file will be clobbered
+// WARNING
+// WARNING,  Please make changes on poeditor instead of here
+// 
+// 
+?>
+Betreff: Einladung zur Verwendung von FileSender
+
+{alternative:plain}
+
+Hallo,
+
+Sie wurden eingeladen, die FileSender-Instanz {cfg:site_name} unter {cfg:site_url} zu verwenden.
+Ihr Passwort für die Anmeldung lautet zunächst {Passwort}.
+
+Sie werden aufgefordert, die Seite Mein Profil zu besuchen und Ihr Passwort zu ändern, wenn Sie die Website nutzen.
+
+Freundliche Grüße,
+{cfg:site_name}
+
+{alternative:html}
+
+<p>
+    Hallo,
+</ p>
+
+<p>
+Sie wurden eingeladen, die FileSender-Instanz {cfg:site_name} unter {cfg:site_url} zu verwenden.
+Ihr Passwort für die Anmeldung lautet zunächst {Passwort}.</ p>
+<p>
+Sie werden aufgefordert, die Seite Mein Profil zu besuchen und Ihr Passwort zu ändern, wenn Sie die Website nutzen.
+</ p>
+
+<p>
+    Viele Grüße,<br />
+    {cfg:site_name}
+</ p>
