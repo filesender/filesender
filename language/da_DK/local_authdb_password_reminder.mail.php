ec9cdfdb26ef6877b11b352de6960573a0cab81c
monkeyiq <monkeyiq@tpg.com.au>
2020-10-07 14:29:44 +1000

    add these too (#848)

---
+++ language/da_DK/local_authdb_password_reminder.mail.php
@@ -0,0 +1,40 @@
+<?php 
+// WARNING, this is a read only file created by import scripts
+// WARNING
+// WARNING,  Changes made to this file will be clobbered
+// WARNING
+// WARNING,  Please make changes on poeditor instead of here
+// 
+// 
+?>
+subject: Invitation til at bruge FileSender
+
+{alternative:plain}
+
+Hej!
+
+Du er blevet inviteret til at bruge FileSender-instansen {cfg:site_name} på {cfg:site_url}.
+Din adgangskode er i første omgang {password}
+
+Du bør besøge siden Min profil og ændre din adgangskode når du begynder at bruge sitet.
+
+Mange venlige hilsner
+{cfg:site_name}
+
+{alternative:html}
+
+<p>
+Hej!
+</p>
+
+<p>
+Du er blevet inviteret til at bruge FileSender-instansen {cfg:site_name} på {cfg:site_url}.
+Din adgangskode er i første omgang {password}</p>
+<p>
+Du bør besøge siden Min profil og ændre din adgangskode når du begynder at bruge sitet.
+</p>
+
+<p>
+    Mange venlige hilsner<br />
+    {cfg:site_name}
+</p>
