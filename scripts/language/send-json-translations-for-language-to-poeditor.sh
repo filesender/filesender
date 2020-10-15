c296eccab99a691dc81aa0b8d334e68cbc218bcb
monkeyiq <monkeyiq@tpg.com.au>
2020-10-15 14:46:52 +1000

    i18n: add new terms before trying to update translations (#856)

--- scripts/language/send-json-translations-for-language-to-poeditor.sh
+++ scripts/language/send-json-translations-for-language-to-poeditor.sh
@@ -8,6 +8,11 @@ projectid=${1:?supply poeditor project id as arg1. Main project is 48000 test pr
 langcode=${2:?supply poeditor language code as arg2};
 jsonfile=${3:?supply path to json file created with convert-php-to-poeditor-json.php as arg3};
 
+curl -X POST https://api.poeditor.com/v2/terms/add \
+     -d api_token="$API_TOKEN" \
+     -d id="$projectid" \
+     --data-binary @$jsonfile
+
 curl -X POST https://api.poeditor.com/v2/languages/update \
      -d api_token="$API_TOKEN" \
      -d id="$projectid" \
