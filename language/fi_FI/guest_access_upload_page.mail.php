c906a5150423ab5dea87b9b52e13c9230d79e35f
Ben Martin <monkeyiq@users.sourceforge.net>
2020-12-16 19:43:41 +1000

    add these new files from auto i18n import

---
+++ language/fi_FI/guest_access_upload_page.mail.php
@@ -0,0 +1,35 @@
+<?php 
+// WARNING, this is a read only file created by import scripts
+// WARNING
+// WARNING,  Changes made to this file will be clobbered
+// WARNING
+// WARNING,  Please make changes on poeditor instead of here
+// 
+// 
+?>
+subject: Vierailija avasi lataussivun
+
+{alternative:plain}
+
+Hei,
+
+tiedoksesi, että vierailija {guest.email} on avannut lataussivun lähettämääsi voucheria käyttäen.
+
+Terveisin,
+{cfg:site_name}
+
+{alternative:html}
+
+<p>
+    Hei,
+</p>
+
+<p>
+    tiedoksesi, että vierailija <a href="mailto:{guest.email}">{guest.email}</a> on avannut lataussivun lähettämääsi voucheria käyttäen.
+</p>
+
+<p>
+    Terveisin,<br />
+    {cfg:site_name}
+</p>
+
---
+++ language/fi_FI/guest_cancelled.mail.php
@@ -0,0 +1,35 @@
+<?php 
+// WARNING, this is a read only file created by import scripts
+// WARNING
+// WARNING,  Changes made to this file will be clobbered
+// WARNING
+// WARNING,  Please make changes on poeditor instead of here
+// 
+// 
+?>
+subject: Voucher peruttu
+
+{alternative:plain}
+
+Hei,
+
+voucher käyttäjältä {guest.user_email} on peruttu.
+
+Terveisin,
+{cfg:site_name}
+
+{alternative:html}
+
+<p>
+    Hei,
+</p>
+
+<p>
+    voucher käyttäjältä <a href="mailto:{guest.user_email}">{guest.user_email}</a> on peruttu.
+</p>
+
+<p>
+    Terveisin,<br />
+    {cfg:site_name}
+</p>
+
