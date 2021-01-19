bbb171d482bbe820a4298277717cb9c02e01b5ef
Ben Martin <monkeyiq@users.sourceforge.net>
2021-01-19 10:51:22 +1000

    mariadb testing

--- ci/scripts/mariadb-setup.sh
+++ ci/scripts/mariadb-setup.sh
@@ -3,16 +3,33 @@
 # These are only the commands that are specific to mariadb which need
 # to be executed before the scripts/upgrade/database.php can be used
 #
+
+echo "----------------------------"
+echo "mariadb-setup.sh DB:$DB "
+echo "----------------------------"
 set -ev
 
-mysql -u root -e "CREATE DATABASE filesender DEFAULT CHARACTER SET utf8mb4;"
-mysql -u root -e "GRANT USAGE ON *.* TO 'filesender'@'localhost' IDENTIFIED BY 'password';"
-mysql -u root -e "GRANT DROP, CREATE, CREATE VIEW, ALTER, SELECT, INSERT, INDEX, UPDATE, DELETE, DROP, REFERENCES ON filesender.* TO 'filesender'@'localhost';"
+MYSQL="mysql --host=localhost -u root "
+
+echo "[client]"             > ~/.my.cnf
+echo "host=localhost"      >> ~/.my.cnf
+echo "password=password"   >> ~/.my.cnf
+chmod 600 ~/.my.cnf
+echo "my.cnf is "
+cat ~/.my.cnf
+
+echo "database listing is..."
+$MYSQL --execute="show databases;"
 
 
-mysql -u root -e "CREATE DATABASE filesenderdataset DEFAULT CHARACTER SET utf8mb4;"
-mysql -u root -e "GRANT DROP, CREATE, CREATE VIEW, ALTER, SELECT, INSERT, INDEX, UPDATE, DELETE ON filesenderdataset.* TO 'filesender'@'localhost';"
-mysql -u root -e "FLUSH PRIVILEGES;"
+$MYSQL  -e "CREATE DATABASE filesender DEFAULT CHARACTER SET utf8mb4;"
+$MYSQL  -e "GRANT USAGE ON *.* TO 'filesender'@'localhost' IDENTIFIED BY 'password';"
+$MYSQL  -e "GRANT DROP, CREATE, CREATE VIEW, ALTER, SELECT, INSERT, INDEX, UPDATE, DELETE, DROP, REFERENCES ON filesender.* TO 'filesender'@'localhost';"
+
+
+$MYSQL  -e "CREATE DATABASE filesenderdataset DEFAULT CHARACTER SET utf8mb4;"
+$MYSQL  -e "GRANT DROP, CREATE, CREATE VIEW, ALTER, SELECT, INSERT, INDEX, UPDATE, DELETE ON filesenderdataset.* TO 'filesender'@'localhost';"
+$MYSQL  -e "FLUSH PRIVILEGES;"
 
 
 
