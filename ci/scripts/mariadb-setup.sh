c31f9cea32a9ce933b3c6cd42edab057dc1afa3d
Ben Martin <monkeyiq@users.sourceforge.net>
2021-01-19 12:22:16 +1000

    localhost

--- ci/scripts/mariadb-setup.sh
+++ ci/scripts/mariadb-setup.sh
@@ -28,14 +28,15 @@ $MYSQL --execute="show databases;"
 
 echo "making db..."
 #$MYSQL  -e "CREATE DATABASE filesender DEFAULT CHARACTER SET utf8mb4;"
-echo "created db..."
 $MYSQL  -e "GRANT USAGE ON *.* TO 'filesender'@'127.0.0.1' IDENTIFIED BY 'password';"
-echo "grant1 done..."
+$MYSQL  -e "GRANT USAGE ON *.* TO 'filesender'@'localhost' IDENTIFIED BY 'password';"
 $MYSQL  -e "GRANT DROP, CREATE, CREATE VIEW, ALTER, SELECT, INSERT, INDEX, UPDATE, DELETE, DROP, REFERENCES ON filesender.* TO 'filesender'@'127.0.0.1';"
+$MYSQL  -e "GRANT DROP, CREATE, CREATE VIEW, ALTER, SELECT, INSERT, INDEX, UPDATE, DELETE, DROP, REFERENCES ON filesender.* TO 'filesender'@'localhost';"
 
 
-#$MYSQL  -e "CREATE DATABASE filesenderdataset DEFAULT CHARACTER SET utf8mb4;"
-#$MYSQL  -e "GRANT DROP, CREATE, CREATE VIEW, ALTER, SELECT, INSERT, INDEX, UPDATE, DELETE ON filesenderdataset.* TO 'filesender'@'127.0.0.1';"
+$MYSQL  -e "CREATE DATABASE filesenderdataset DEFAULT CHARACTER SET utf8mb4;"
+$MYSQL  -e "GRANT DROP, CREATE, CREATE VIEW, ALTER, SELECT, INSERT, INDEX, UPDATE, DELETE ON filesenderdataset.* TO 'filesender'@'127.0.0.1';"
+$MYSQL  -e "GRANT DROP, CREATE, CREATE VIEW, ALTER, SELECT, INSERT, INDEX, UPDATE, DELETE ON filesenderdataset.* TO 'filesender'@'localhost';"
 $MYSQL  -e "FLUSH PRIVILEGES;"
 
 
