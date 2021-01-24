d7909eacd7ad9cec850a604bd7bf582cfda79bc4
monkeyiq <monkeyiq@tpg.com.au>
2021-01-24 11:22:41 +1000

    Some updates to the console CI in preparation for Sauce/Selenium (#907)
    * rest api aup add the terms template page
    
    * clientlogs: move from console.log to clientlogs.log
    
    * test the test secret
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * updates for moving toward selenium
    
    * getting ready for pi
    
    * getting ready for pr

--- ci/setup-machine.sh
+++ ci/setup-machine.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+set -v
+
 PWD=$(pwd)
 echo "----------------------------------------------------------------"
 echo "This is the ci/setup-machine.sh script running on database $DB "
 echo "----------------------------------------------------------------"
 
+echo "current directory"
+pwd
+FILESENDERROOT=$(pwd);
 echo "your php modules installed..."
 php -m
 echo "----------------------------------------------------------------"
+
 
 
 export POSTGRES_DB
@@ -17,17 +23,13 @@ export POSTGRES_PASSWORD
 
 mkdir -p ./log ./tmp ./files
 
-#phpenv config-add ./ci/php-config.ini
-
 cp ./ci/filesender-config.php ./config/config.php
 if [ "$DB" = "mysql" ]; then
     sed -e "s?usemysql=0?usemysql=1?g" --in-place ./config/config.php  ;
 fi
 
-sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)?g" --in-place ./config/config.php
+sed -e "s?%TRAVIS_BUILD_DIR%?${FILESENDERROOT}?g" --in-place ./config/config.php
 chmod -R a+x ./ci/scripts
-
-####miq#./ci/scripts/simplesamlphp-setup.sh
 
 ####
 #
@@ -55,8 +57,7 @@ fi
 # Packages
 #
 sudo apt-get update
-sudo apt-get install curl
-sudo apt-get install apache2 #libapache2-mod-fastcgi
+sudo apt-get install curl wget apache2 
 
 ####
 # 
@@ -67,26 +68,28 @@ sudo sed -i "s/NameVirtualHost \*:80/# NameVirtualHost \*:80/" /etc/apache2/port
 sudo sed -i "s/Listen 80/# Listen 80/" /etc/apache2/ports.conf
 sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/certs/filesender_test.key -out /etc/ssl/certs/filesender_test.crt -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=file_sender.app"
 sudo cp -f ci/apache2.conf /etc/apache2/sites-enabled/000-default.conf
-sudo sed -e "s?%TRAVIS_BUILD_DIR%?$(pwd)?g" --in-place /etc/apache2/sites-enabled/000-default.conf
+sudo sed -e "s?%TRAVIS_BUILD_DIR%?${FILESENDERROOT}?g" --in-place /etc/apache2/sites-enabled/000-default.conf
 
-# enable php-fpm
-#sudo sed -i "s/error_reporting = E_ALL/error_reporting = E_ALL \& ~E_DEPRECATED \& ~E_STRICT/" ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
-#sudo cp ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf.default ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf
-#sudo a2enmod rewrite actions fastcgi alias
-#echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
-#sudo sed -i -e "s,www-data,travis,g" /etc/apache2/envvars
-#sudo chown -R travis:travis /var/lib/apache2/fastcgi
-#sudo cp ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.d/www.conf.default ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.d/www.conf
-# #- sudo sed -i -e "s,nobody,travis,g " ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.d/www.conf
-# - ~/.phpenv/versions/$(phpenv version-name)/sbin/php-fpm
-# - sudo cat /etc/apache2/sites-enabled/000-default.conf
+sudo apt-get install php${php_version}-fpm
+sudo cp /usr/sbin/php-fpm{php_version} ${php_version} /usr/bin/php-fpm # copy to /usr/bin
+sudo ls -l /var/lib/apache2/fastcgi
+sudo chown -R runner:docker /var/lib/apache2/fastcgi /usr/sbin/php-fpm${php_version} /usr/bin/php-fpm
 
+phpini=$(php -r "echo get_cfg_var('cfg_file_path');")
+echo "cgi.fix_pathinfo = 1" >> $phpini
 
-# - echo "... making sure sauce connect doesn't redefine ..."
-# - sudo  sed -i -e "s,define('SAUCE_HOST',if(\!defined('SAUCE_HOST')) define('SAUCE_HOST',g" /home/travis/build/filesender/filesender/vendor/sauce/sausage/src/Sauce/Sausage/SauceAPI.php
-# - sudo  sed -i -e "s,define('SAUCE_HOST',if(\!defined('SAUCE_HOST')) define('SAUCE_HOST',g" /home/travis/build/filesender/filesender/vendor/sauce/sausage/src/Sauce/Sausage/SauceConfig.php
+echo "___ changing group to www-data for $FILESENDERROOT "
+sudo chgrp -R www-data ${FILESENDERROOT}
+sudo chown -R www-data:docker   ${FILESENDERROOT}/log
+sudo chown -R www-data:docker   ${FILESENDERROOT}/files
+sudo chown -R www-data:www-data ${FILESENDERROOT}/tmp
+# recommended by install script for php${php_version}-fpm
+sudo a2enmod proxy_fcgi setenvif
+sudo a2enconf php7.2-fpm
 
-echo "restarting apache2..."
+echo "restarting apache2 and php-fpm..."
+sudo service php${php_version}-fpm start
+sudo service php${php_version}-fpm status
 sudo service apache2 restart
 
 # stop the database we are not planning to use
@@ -98,7 +101,15 @@ if [ "$DB" = "mysql" ]; then
     sudo service postgresql stop
 fi 
 
-# after_failure:
-# - sudo cat /var/log/apache2/error.log
-# - sudo cat /var/log/apache2/access.log
-# - find ./log -type f -exec cat {} +
+echo "... making sure sauce connect doesn't redefine SAUCE_HOST ..."
+grep -l -R SAUCE_HOST ${FILESENDERROOT}/vendor | \
+    xargs -I {} -n 1 sed -i -e "s,define('SAUCE_HOST',if(\!defined('SAUCE_HOST')) define('SAUCE_HOST',g" "{}"
+
+echo "... trying to get index page to verify ..."
+curl -k https://localhost/filesender/
+
+echo "--------------------"
+echo "Apache logs....     "
+echo "--------------------"
+cat /var/log/apache2/access.log
+cat /var/log/apache2/error.log
