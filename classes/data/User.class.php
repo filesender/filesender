f29b94313311a5e58d829333db3c0c22a5f228e4
Ben Martin <monkeyiq@users.sourceforge.net>
2022-01-08 14:04:35 +1000

    no real change

--- classes/data/User.class.php
+++ classes/data/User.class.php
@@ -30,6 +30,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+
 // Require environment (fatal)
 if (!defined('FILESENDER_BASE')) {
     die('Missing environment');
