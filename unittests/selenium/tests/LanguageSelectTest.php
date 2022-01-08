9ee096f66bd74a825b62e40dd984694359af2bd7
Ben Martin <monkeyiq@users.sourceforge.net>
2022-01-08 15:40:42 +1000

    .

--- unittests/selenium/tests/LanguageSelectTest.php
+++ unittests/selenium/tests/LanguageSelectTest.php
@@ -8,15 +8,10 @@ class LanguageSelectTest extends SeleniumTest
     
     private function selectLangauge( $lang )
     {
-        sleep(10);
-        
         $this->waitForCSS(".language-dropdown-toggle")->click();
         sleep(2);
         $this->scrollIntoView( "#toplangdropdown > a[data-id='".$lang."']" );
-//        $this->execute(array('script' => "document.querySelector(\"#toplangdropdown > a[data-id='".$lang."']\").scrollIntoView(true);",
-//                             'args' => array()));
-        sleep(5);
-        
+        sleep(2);
         $this->waitForCSS("#toplangdropdown > a[data-id='".$lang."']")->click();
     }
 
