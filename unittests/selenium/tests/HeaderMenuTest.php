1030bc8ef2932b1145ffd593c0ee2aca6ab5fdcc
Ben Martin <monkeyiq@users.sourceforge.net>
2022-01-08 10:13:45 +1000

    .

--- unittests/selenium/tests/HeaderMenuTest.php
+++ unittests/selenium/tests/HeaderMenuTest.php
@@ -8,41 +8,63 @@ class HeaderMenuTest extends SeleniumTest
     protected $start_url_path = '';
     
 
-    public function testHeaderMenu()
+    public function testHeaderMenuUnauthenticated()
     {
         extract($this->getKeyBindings());
-
         $this->setupUnauthenticated();
         sleep(1);
         $this->assertEquals(4, $this->getHeaderMenuSize());
 
         $this->setupAuthenticated();
         sleep(1);
+    }
+
+    public function testHeaderMenuAuthenticated()
+    {
+        extract($this->getKeyBindings());
+        $this->setupAuthenticated();
         $this->assertGreaterThanOrEqual( 3, $this->getHeaderMenuSize(),
                                          'authenticated user menu item count' );
 
         $this->setUserPage();
-
+    }
+    
+    public function testHeaderMenuAuthenticatedUserPage()
+    {
+        extract($this->getKeyBindings());
         $this->assertGreaterThanOrEqual(4, $this->getHeaderMenuSize(),
                                         'user page' );
 
         $this->setAdmin();
+        sleep(1);
+    }
 
+    
+    public function testHeaderMenuAdminPage()
+    {
+        extract($this->getKeyBindings());
         $this->assertEquals(10, $this->getHeaderMenuSize());
-
         $this->unsetAdmin();
-
+    }
+    
+    public function testHeaderMenuNoAdminPage()
+    {
+        extract($this->getKeyBindings());
         $this->assertGreaterThanOrEqual( 4, $this->getHeaderMenuSize());
-
         $this->unsetUserPage();
-        
+    }
+    
+    public function testNormalHeader()
+    {
+        extract($this->getKeyBindings());
         $this->assertEquals(8, $this->getHeaderMenuSize());
-
-        
         $this->setupUnauthenticated();
+    }
+    
+    public function testNormalUnauthenticated()
+    {
+        extract($this->getKeyBindings());
         $this->assertEquals(4, $this->getHeaderMenuSize());
-
-        
         $this->setupAuthenticated();
         sleep(1);
     }
