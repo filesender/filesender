6bc946c2b8cb697b66822cb269f9026a93cd6a80
Ben Martin <monkeyiq@users.sourceforge.net>
2023-02-01 06:26:03 +1000

    crypto-ui: when a password has worked offer it for other files on the same download page

--- language/en_AU/lang.php
+++ language/en_AU/lang.php
@@ -689,3 +689,4 @@ $lang['you_can_report_exception'] = 'When reporting this error please give the f
 $lang['you_can_report_exception_by_email'] = 'You can report this error by email';
 $lang['you_can_send_client_logs'] = 'In order to help your support team to find out what happened you can send the last log entries from your user interface by clicking this button :';
 $lang['you_generated_this_auth_secret_at'] = 'You generated this auth secret at: {datetime}';
+$lang['previous_password_shown_for_next_action'] = 'Your previously entered password was correct. It is shown again to allow you to reuse it.';
