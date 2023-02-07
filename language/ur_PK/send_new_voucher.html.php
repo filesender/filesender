75391e1e3c95b374afcb3eb863f659658c0c26f8
monkeyiq <monkeyiq@tpg.com.au>
2023-02-07 21:06:57 +1000

    i18n: auto import from poedtor on 2023-02-07-1675767614 (#1369)

---
+++ language/ur_PK/send_new_voucher.html.php
@@ -0,0 +1,15 @@
+<?php 
+// WARNING, this is a read only file created by import scripts
+// WARNING
+// WARNING,  Changes made to this file will be clobbered
+// WARNING
+// WARNING,  Please make changes on poeditor instead of here
+// 
+// 
+?>
+<h2>مہمان صارف کو مدعو کرنا</h2>
+<p>آپ کسی صارف کو بغیر اکاؤنٹ کے، دعوت نامے کے ساتھ فائلیں بھیج سکتے ہیں:
+<li>مہمان صارف کا ای میل پتہ درج کریں۔</li>
+<li>اگر ضروری ہو تو ایک مضمون اور پیغام ٹائپ کریں۔</li>
+<li>'دعوت نامہ بھیجیں' پر کلک کریں۔</li></p>
+<p>مہمان صارف کو دعوتی لنک پر مشتمل ایک ای میل موصول ہوتا ہے۔</p>
