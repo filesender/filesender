1ca92caec5b158a9c9850a44b938bfe581ba84db
monkeyiq <monkeyiq@tpg.com.au>
2023-03-02 08:29:59 +1000

    i18n: auto import from poedtor on 2023-03-01-1677613593 (#1402)

--- language/da_DK/privacy_text.html.php
+++ language/da_DK/privacy_text.html.php
@@ -7,17 +7,12 @@
 // 
 // 
 ?>
-<h1>Velkommen til {cfg:site_name}</h1>
+<h1>Privatlivsbeskyttelse på {cfg:site_name}</h1>
 <p>
-For at tjenesten her kan fungere, må den gemme visse oplysninger om filerne, hvem der kan tilgå dem, og hvad der er sket. Filer slettes automatisk fra systemet når de udløber; og andre gemte oplysninger vil blive fjernet fra systemet og databasen efter et stykke tid. Her på siden kan man se hvor længe de forskellige typer oplysninger og data gemmes i den her installation af FileSender. 
+Tjenesten her gemmer oplysninger om de overførte filer i logfilerne, herunder oplysninger om hvem der må gøre noge med filerne, og hvad der sker med dem.
 </p>
 <p>
-Læg mærke til at sletning af en overførsel medfører samtidig sletning af alle tilhørende filer, inklusive kopier af e-mails som er blevet afsendt i forbindelse med overførslen.
+Vær klar over at udløbne filer slettes automatisk. Hvis du sletter en overførsel, slettes også alle hermed forbundne filer og mails.
 </p>
-<?php
-if( ShredFile::shouldUseShredFile()) {
-    echo "<p>Sitet her er sat op til at makulere uploadede filer når de slettes. ";
-    echo "Makulering af en fil indebærer at der skrives data et stort antal gange ind over det område på disken hvor filen lå, for at man kan være sikker på at brugerdata forsvinder fra systemet.";
-    echo "Det giver øget privatlivsbeskyttelse for folk som bruger tjenesten her.</p>";
-}
-?>
+<p>{cfg:site_name} "makulerer" slettede filer. Makulering indebærer at filernes fysiske placering overskrives. De egentlige data forsvinder. Det giver dig ekstra privtalivsbeskyttelse. Her på siden finder du oplysninger om hvor længe logfiler beholdes.
+</p>
