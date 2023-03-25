5d73a83ad040d092c209845fc132f0092c60bd7b
JohannesNicholas <goanna7007@gmail.com>
2023-03-25 17:51:13 +1100

    Adds example file and example recipient

--- www/js/cli.js
+++ www/js/cli.js
@@ -18,16 +18,45 @@ const request = http.get(base_url+"/filesender-config.js.php", function(response
 
    // after download completed close filestream
    file.on("finish", () => {
-       file.close();
-       console.log("Config downloaded");
+        file.close();
+        console.log("Config downloaded");
+
+        //get all the required files
+        XRegExp = require('../lib/xregexp/xregexp-all.js');
+        require('./filesender-config.js');
+        require('./client.js');
+        require('./filesender.js');
+        require('./transfer.js');
+
+        //add some required functions
+        window.filesender.ui = {};
+        window.filesender.ui.error = function(error,callback) {
+            console.log('[error] ' + error.message);
+            console.log(error);
+        }
+        window.filesender.ui.log = function(message) {
+            console.log('[log] ' + message);
+        }
+        window.filesender.ui.validators = {};
+        window.filesender.ui.validators.email = /^[a-z0-9!#$%&'*+\/=?^_\`\{|\}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_\`\{|\}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+(?:[a-z]{2,})$/i
+
+       
+       
+        //create a new transfer
+        var transfer = new window.filesender.transfer()
+
+        //add a file to the transfer
+        const blob = new Blob(['This file was generated as a test.']);
+        transfer.addFile('test.txt', blob, undefined);
+
+        //set the recipient
+        transfer.addRecipient('joey@joeyn.dev', undefined);
+    
+
+
    });
 });
 
 
-require('./filesender-config.js');
-require('./filesender.js');
-require('./transfer.js');
 
-//delete the config file
-fs.unlinkSync("./filesender-config.js");
 
