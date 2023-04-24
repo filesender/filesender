32dc16cb3614c6b0bf42844e9450ce392323ecd7
monkeyiq <monkeyiq@tpg.com.au>
2023-04-25 08:37:52 +1000

    Update language/en_AU/lang.php
    Co-authored-by: Veredior <Veredior@users.noreply.github.com>

--- language/en_AU/lang.php
+++ language/en_AU/lang.php
@@ -693,5 +693,5 @@ $lang['you_can_report_exception'] = 'When reporting this error please give the f
 $lang['you_can_report_exception_by_email'] = 'You can report this error by email';
 $lang['you_can_send_client_logs'] = 'In order to help your support team to find out what happened you can send the last log entries from your user interface by clicking this button :';
 $lang['you_generated_this_auth_secret_at'] = 'You generated this auth secret at: {datetime}';
-$lang['guest_uploads_expire_when_guest_expires'] = 'Guest uploads expire at or before when the guest invitation expires';
+$lang['guest_upload_default_expire_is_guest_expire'] = 'Guest default upload expiry date is the same as the guest invitation one';
 $lang['guest_upload_expire_read_only'] = 'Guest can not change the expire time for uploads';
