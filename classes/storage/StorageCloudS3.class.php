b44311e8ff8b36b28bf9d363be9677b3816594d6
juhassi <129836175+juhassi@users.noreply.github.com>
2023-04-26 09:16:05 +0300

    StorageCloudS3: When using a custom bucket name, deleting single file empties out the whole bucket.

--- classes/storage/StorageCloudS3.class.php
+++ classes/storage/StorageCloudS3.class.php
@@ -222,7 +222,11 @@ class StorageCloudS3 extends StorageFilesystem
         try {
             $client = self::getClient();
 
-            $objects = $client->getIterator('ListObjects', array('Bucket' => $bucket_name));
+            if( !self::usingCustomBucketName( $file ) ) {
+                $objects = $client->getIterator('ListObjects', array('Bucket' => $bucket_name));
+            } else {
+                $objects = $client->getIterator('ListObjects', array('Bucket' => $bucket_name, 'Prefix' => $file->uid));    
+            }
 
             foreach ($objects as $object) {
                 $result = $client->deleteObject(array(
