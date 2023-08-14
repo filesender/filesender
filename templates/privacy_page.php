f449e3c7cc408b98339a13c8796520c6a254a129
Wander Rocha Gomes <wander.rg@gmail.com>
2023-08-14 19:06:12 -0300

    feat(new-ui): privacy page i18n terms

--- templates/privacy_page.php
+++ templates/privacy_page.php
@@ -26,33 +26,43 @@ function config_VisString( $k, $specialv, $specialret ) {
 
                 <div class="fs-base-page__content">
                     {tr:privacy_text}
-
+                    <br/>
                     <strong>{tr:privacy_page_days_old_table_text}</strong>
-                    <table columns="2">
-                        <tr><th><?php echo Lang::tr('value')?></th>
+                    <table class="fs-table fs-table--thin" columns="2">
+                        <tbody>
+                        <tr>
+                            <th><?php echo Lang::tr('value')?></th>
                             <th><?php echo Lang::tr('description')?></th>
                         </tr>
-                        <tr><td><?php echo Config::get('max_transfer_days_valid') ?></td>
+                        <tr>
+                            <td><?php echo Config::get('max_transfer_days_valid') ?></td>
                             <td><?php echo Lang::tr('privacy_page_max_transfer_days_valid')?></td>
                         </tr>
-                        <tr><td><?php echo Config::get('clientlogs_lifetime') ?></td>
+                        <tr>
+                            <td><?php echo Config::get('clientlogs_lifetime') ?></td>
                             <td><?php echo Lang::tr('privacy_page_clientlogs_lifetime')?> </td>
                         </tr>
-                        <tr><td><?php echo Config::get('translatable_emails_lifetime') ?></td>
+                        <tr>
+                            <td><?php echo Config::get('translatable_emails_lifetime') ?></td>
                             <td><?php echo Lang::tr('privacy_page_translatable_emails_lifetime')?> </td>
                         </tr>
-                        <tr><td><?php echo config_VisString('guests_expired_lifetime',-1,Lang::tr('never')) ?></td>
+                        <tr>
+                            <td><?php echo config_VisString('guests_expired_lifetime',-1,Lang::tr('never')) ?></td>
                             <td><?php echo Lang::tr('privacy_page_guests_expired_lifetime')?> </td>
                         </tr>
-                        <tr><td><?php echo Config::get('auditlog_lifetime') ?></td>
+                        <tr>
+                            <td><?php echo Config::get('auditlog_lifetime') ?></td>
                             <td><?php echo Lang::tr('privacy_page_auditlog_lifetime')?> </td>
                         </tr>
-                        <tr><td><?php echo Config::get('trackingevents_lifetime') ?></td>
+                        <tr>
+                            <td><?php echo Config::get('trackingevents_lifetime') ?></td>
                             <td><?php echo Lang::tr('privacy_page_trackingevents_lifetime')?> </td>
                         </tr>
-                        <tr><td></td>
+                        <tr>
+                            <td></td>
                             <td><?php echo Lang::tr('privacy_page_frequent_recipients_text')?> </td>
                         </tr>
+                        </tbody>
                     </table>
                 </div>
             </div>
