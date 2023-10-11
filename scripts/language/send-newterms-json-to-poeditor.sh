264d0e253b061ce2495331b2e6d6f6ab77ed3f49
Nils Vogels <n.vogels@aves-it.nl>
2023-10-11 13:39:54 +0200

    Switch poeditor project id

--- scripts/language/send-newterms-json-to-poeditor.sh
+++ scripts/language/send-newterms-json-to-poeditor.sh
@@ -4,7 +4,7 @@ set -eou pipefail
 . ~/.filesender/poeditor-apikey
 
 
-projectid=${1:?supply poeditor project id as arg1. Main project is 48000 test project is 380345 };
+projectid=${1:?supply poeditor project id as arg1. Main project is 633591 test project is 380345 };
 jsonfile=${2:?supply path to json file created with export-terms-to-new-and-deleted-lists.sh as arg2};
 
 curl -X POST https://api.poeditor.com/v2/terms/add \
