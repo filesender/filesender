89fb39e7cb6dbe0520cb7f4c6e53cabc35b53233
Ben Martin <monkeyiq@users.sourceforge.net>
2024-02-05 15:31:18 +1000

    bring back day buckets

--- includes/ConfigDefaults.php
+++ includes/ConfigDefaults.php
@@ -167,7 +167,7 @@ $default = array(
     'storage_filesystem_external_script' => FILESENDER_BASE.'/scripts/StorageFilesystemExternal/external.py',
 
     'storage_filesystem_shred_path' => FILESENDER_BASE.'/shredfiles',
-    'storage_filesystem_per_day_buckets' => false,
+    'storage_filesystem_per_day_buckets' => true,
     'storage_filesystem_per_hour_buckets' => false,
     
     'email_from' => 'sender',
