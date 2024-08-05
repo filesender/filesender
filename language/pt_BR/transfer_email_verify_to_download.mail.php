ab0cfafb9707fffaa5a98dcf1ff6a351a845581a
monkeyiq <monkeyiq@users.sourceforge.net>
2024-08-05 14:31:27 +1000

    i18n: ui3: auto import from poedtor on 2024-08-05-1722832022 (#1964)

---
+++ language/pt_BR/transfer_email_verify_to_download.mail.php
@@ -0,0 +1,51 @@
+<?php 
+// WARNING, this is a read only file created by import scripts
+// WARNING
+// WARNING,  Changes made to this file will be clobbered
+// WARNING
+// WARNING,  Please make changes on poeditor instead of here
+// 
+// 
+?>
+
+<?php 
+// ATENÇÃO, Arquivo somente leitura criado pelos scripts de importação.
+// ATENÇÃO
+// ATENÇÃO,  Alterações realizadas nele serão perdidas.
+// ATENÇÃO
+// ATENÇÃO,  Favor realizar as alterações no poeditor.
+// 
+// 
+?>
+subject: {cfg:site_name}: Código de verificação única para download
+
+{alternative:plain}
+
+Olá,
+
+Alguém (provavelmente você) solicitou um código de verificação único para download do arquivo que foi disponibilizado em {cfg:site_name}.
+Este é o código de verificação:
+
+{verificationCode}
+
+Atenciosamente,
+{cfg:site_name}
+
+{alternative:html}
+
+<p>
+    Olá,
+</p>
+
+<p>
+Alguém (provavelmente você) solicitou um código de verificação único para download do arquivo que foi disponibilizado em {cfg:site_name}.
+Este é o código de verificação:
+</p>
+<p>
+{verificationCode}
+</p>
+
+<p>
+    Atenciosamente,<br />
+    {cfg:site_name}
+</p>
