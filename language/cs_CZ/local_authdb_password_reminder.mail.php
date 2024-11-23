3a6bda4e4639c780f88993ff54ba6fe260a06e14
monkeyiq <monkeyiq@users.sourceforge.net>
2024-11-23 10:15:05 +1000

    i18n: auto import from poedtor on 2024-11-23-1732320503 (#2079)

---
+++ language/cs_CZ/local_authdb_password_reminder.mail.php
@@ -0,0 +1,41 @@
+<?php 
+// WARNING, this is a read only file created by import scripts
+// WARNING
+// WARNING,  Changes made to this file will be clobbered
+// WARNING
+// WARNING,  Please make changes on poeditor instead of here
+// 
+// 
+?>
+subject: Pozvánka k používání {cfg:site_name}
+
+{alternative:plain}
+
+Ahoj,
+
+Byli jste pozváni k použití {cfg:site_name} na {cfg:site_url}.
+Vaše heslo pro prvotní přihlášení je {password}.
+
+Doporučujeme vám, abyste navštívili stránku Můj profil a změnili si heslo, jakmile začnete stránky používat.
+
+S pozdravem,
+{cfg:site_name}
+
+{alternative:html}
+
+<p>
+    Hello,
+</p>
+
+<p>
+Byli jste pozváni k používání {cfg:site_name} na {cfg:site_url}.
+Vaše heslo pro prvotní přihlášení je {password}.</p>
+<p>
+Doporučujeme vám, abyste navštívili stránku Můj profil a změnili si heslo, jakmile začnete stránky používat.
+</p>
+
+<p>
+    S pozdravem,<br />
+    {cfg:site_name}
+</p>
+
---
+++ language/cs_CZ/transfer_email_verify_to_download.mail.php
@@ -0,0 +1,49 @@
+<?php 
+// WARNING, this is a read only file created by import scripts
+// WARNING
+// WARNING,  Changes made to this file will be clobbered
+// WARNING
+// WARNING,  Please make changes on poeditor instead of here
+// 
+// 
+?>
+?php 
+// WARNING, this is a read only file created by import scripts
+// WARNING
+// WARNING,  Changes made to this file will be clobbered
+// WARNING
+// WARNING,  Please make changes on poeditor instead of here
+// 
+// 
+?>
+subject: {cfg:site_name}: Jednorázový ověřovací kód pro stahování
+
+{alternative:plain}
+
+Ahoj,
+
+Někdo (nejspíše vy) si vyžádal o jednostranné ověřovací heslo ke stahován souborů, které jsou dostupné pomocí služby {cfg:site_name}.
+Toto je váš e-mailový ověřovací kód pro stažení tohoto přenosu:
+
+{verificationCode}
+
+
+{alternative:html}
+
+<p>
+    Ahoj,
+</p>
+
+<p>
+Někdo (nejspíše vy) si vyžádal o jednostranné ověřovací heslo ke stahován souborů, které jsou dostupné pomocí služby {cfg:site_name}.
+Toto je váš e-mailový ověřovací kód pro stažení tohoto přenosu na {cfg:site_name} service.
+Toto je váš e-mailový ověřovací kód pro stažení tohoto přenosu:
+</p>
+<p>
+{verificationCode}
+</p>
+
+<p>
+    S pozdravem,<br />
+    {cfg:site_name}
+</p>
