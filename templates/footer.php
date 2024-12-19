91b5f63b2b16b21005b6a46930bb820870abd077
Wander Rocha Gomes <wander.rg@gmail.com>
2024-12-19 12:21:38 -0300

    feat: general theme improvements to support the use of logos in the negative version

--- templates/footer.php
+++ templates/footer.php
@@ -2,8 +2,11 @@
             <div class="container">
                 <div class="fs-footer-container">
                     <small>{tr:copyright}</small>
-                    <img class="fs-footer-container__brand fs-footer-container__brand--desktop" src="{cfg:site_url}/images/filesender-initiative.png" alt="Brand">
-                    <img class="fs-footer-container__brand fs-footer-container__brand--mobile" src="{cfg:site_url}/images/filesender-initiative_mobile.png" alt="Brand">
+                    <img class="fs-footer-container__brand fs-footer-container__brand--desktop" src="{cfg:site_url}/images/filesender-initiative.png" alt="FileSender initiative">
+                    <img class="fs-footer-container__brand fs-footer-container__brand--mobile" src="{cfg:site_url}/images/filesender-initiative_mobile.png" alt="FileSender initiative">
+
+                    <img class="fs-footer-container__brand fs-footer-container__brand--negative fs-footer-container__brand--desktop" src="{cfg:site_url}/images/filesender-initiative_negative.png" alt="FileSender initiative">
+                    <img class="fs-footer-container__brand fs-footer-container__brand--negative fs-footer-container__brand--mobile" src="{cfg:site_url}/images/filesender-initiative_mobile-negative.png" alt="FileSender initiative">
                 </div>
             </div>
 
