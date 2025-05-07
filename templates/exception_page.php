777f025331cc42b7598028fa2a7ec3db3d23855c
yent <etienne.meleard@renater.fr>
2025-05-07 11:40:26 +0200

    Use array of exceptions
    Update template according to array-based exception storage.

--- templates/exception_page.php
+++ templates/exception_page.php
@@ -9,15 +9,13 @@ if (!preg_match('`^[0-9a-f]{13}$`', $sid)) {
     throw new DetailedException('not_an_exception_identifier', $sid);
 }
 
-if (!array_key_exists('exception_'.$sid, $_SESSION)) {
+if (!array_key_exists('exception', $_SESSION) || !is_array($_SESSION['exception']) || !array_key_exists($sid, $_SESSION['exception'])) {
     throw new DetailedException('unknown_exception_identifier', $sid);
 }
 
-$exception = $_SESSION['exception_'.$sid];
+$exception = $_SESSION['exception'][$sid];
 if (!($exception instanceof Exception)) {
     throw new DetailedException('not_an_exception', $sid);
 }
-
-$exception = $_SESSION['exception_'.$sid];
 
 Template::display('exception', array('exception' => $exception));
