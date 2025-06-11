3c7e66f22ee9d23e242c5344523b12198cac4b89
Ben Martin <monkeyiq@users.sourceforge.net>
2025-06-11 10:12:13 +1000

    i18n: auto import from poedtor on 2025-06-11-1749600568

--- language/es_ES/site_splash.html.php
+++ language/es_ES/site_splash.html.php
@@ -7,9 +7,11 @@
 // 
 // 
 ?>
-<h1> Bienvenido a {cfg: site_name} </h1>
+<h1>Bienvenido a {cfg:site_name}</h1>
 
 <p>
-     ¡{cfg: site_name} es una forma segura de compartir archivos grandes con cualquier persona!
-     Inicie sesión para cargar sus archivos o invite a personas a enviarle un archivo.
+¡{cfg:site_name} es una forma segura de compartir archivos grandes con cualquier persona!
+</p>
+<p>
+Inicie sesión para cargar sus archivos o invite a personas a enviarle un archivo.
 </p>
