728be6f8e606751ca784f1395620f18339a3f5b4
Ben Martin <monkeyiq@users.sourceforge.net>
2025-07-17 09:44:25 +1000

    i18n: ui3: auto import from poedtor on 2025-07-17-1752709295

--- language/nl_NL/privacy_text.html.php
+++ language/nl_NL/privacy_text.html.php
@@ -12,7 +12,7 @@
 Deze dienst slaat informatie van bestanden op in logbestanden. Denk dan aan gegevens over autorisatie en wat er met bestanden gebeurt. 
 </p>
 <p>
-Verlopen bestanden worden automatisch verwijderd. Gooi je zelf een overdracht weg, dan verwijder je ook alle gerelateerde bestanden en bijbehorende e-mails. {cfg:site_name} 'shred' verwijderde bestanden. Bij 'shredden' wordt de fysieke plek van bestanden overschreven. De daadwerkelijke data verdwijnt dan. Zo krijg je extra privacy.
+Verlopen bestanden worden automatisch verwijderd. Gooi je zelf een transfer weg, dan verwijder je ook alle gerelateerde bestanden en bijbehorende e-mails. {cfg:site_name} 'shred' verwijderde bestanden. Bij 'shredden' wordt de fysieke plek van bestanden overschreven. De daadwerkelijke data verdwijnt dan. Zo krijg je extra privacy.
 </p>
 <p>
 Op deze pagina vind je informatie over de bewaartermijn van logbestanden.
