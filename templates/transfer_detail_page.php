d52fda9430367fb4de563504e350f3a5b901dc54
Ben Martin <monkeyiq@users.sourceforge.net>
2026-01-29 09:01:15 +1000

    make them empty rather than null

--- templates/transfer_detail_page.php
+++ templates/transfer_detail_page.php
@@ -110,8 +110,8 @@ if(empty($transfer->options['encryption'])) {
 
     // forget this here to limit scope.
     $token = '';
-    $linkParams = null;
-    $archiveParams = null;
+    $linkParams = array();
+    $archiveParams = array();
 }
 
 ?>
